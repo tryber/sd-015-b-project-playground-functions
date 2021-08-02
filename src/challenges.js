f2ac80aec8bde9b50f553b547bf7ca4f7a43dced
caabeatriz <contatocarolinebs@gmail.com>
2021-08-02 14:23:18 -0300

    corrige erros do lint

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,7 @@
 // Desafio 1
-function compareTrue(value1,value2) {
+function compareTrue(value1, value2) {
   // seu código aqui
-  return value1 && value2
+  return value1 && value2;
 }
 
 // Desafio 2
