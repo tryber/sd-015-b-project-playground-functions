99758da028656585adae462d8c83ec5fb001de28
Nicolas Franzolin <nicolas.franzolin@gmail.com>
2021-08-03 15:54:19 -0300

    Adiciona Requisito 5

--- src/challenges.js
+++ src/challenges.js
@@ -26,8 +26,9 @@ function concatName(stringArray) {
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  return (3 * wins) + (ties);
 }
 
 // Desafio 6
