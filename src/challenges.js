1a38b9c0139b4d04ab915a732b43b67611a8cc9d
douglas <douglasmonteiro.m@gmail.com>
2021-08-03 18:45:24 -0300

    Desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -48,8 +48,17 @@ function highestCount(array) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distanciaCat1 = mouse - cat1;
+  let distanciaCat2 = mouse - cat2;
+
+  if(distanciaCat1 > distanciaCat2){
+    return "cat2"
+  }else if (distanciaCat1 === distanciaCat2){
+    return "os gatos trombam e o rato foge"
+  }else{
+    return "cat1"
+  }
 }
 
 // Desafio 8
