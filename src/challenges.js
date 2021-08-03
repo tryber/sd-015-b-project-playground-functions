3752c806d7246abd5c0367d7566a6256c4e3fd82
DanielHott <daniel_hott@outlook.com>
2021-08-03 14:56:21 -0300

    Verificando erros no desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,9 +1,9 @@
 // Desafio 1
 function compareTrue(boolean1, boolean2) { 
-  if(boolean1 === true && boolean2 === true){
-    return 'true'
-  } else if (boolean1 === false && boolean2 === false){
-    return 'false'
+  if(boolean1 === 'true' && boolean2 === 'true'){
+    return `true`
+  } else if (boolean1 === 'false' && boolean2 === 'false'){
+    return `false`
   }
 }
 // Desafio 2
