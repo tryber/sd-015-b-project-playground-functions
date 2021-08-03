28ba28ad1df624fdf25a6368bc46016f9b50b2f1
davi <davisoarescoura@gmail.com>
2021-08-03 23:39:36 +0200

    finaliza os exercicios de 1 a 9

--- src/challenges.js
+++ src/challenges.js
@@ -6,6 +6,7 @@ function compareTrue(a, b) {
     return false;
   }
 }
+console.log(compareTrue(true, true));
 
 // Desafio 2
 function calcArea(base, height) {
@@ -22,14 +23,17 @@ function splitSentence(string) {
 console.log(splitSentence('go Trybe'));
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(matrizString) {
+  return `${matrizString[matrizString.length - 1]}, ${matrizString[0]}`;
 }
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let multiplicacao = (wins * 3) + (ties * 1);
+  return multiplicacao
 }
+console.log(footballPoints(14, 8));
 
 // Desafio 6
 function highestCount(numeros) {
@@ -40,6 +44,7 @@ function highestCount(numeros) {
       contador++;
     }
   }
+  return contador
 }
 
 // Desafio 7
@@ -57,16 +62,70 @@ function catAndMouse(mouse, cat1, cat2) {
 console.log(catAndMouse(1, 0, 2));
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let resultado = [];
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] % 3 === 0 && array[index] % 5 === 0) {
+      resultado.push('fizzBuzz');
+    } else if (array[index] % 3 === 0) {
+      resultado.push('fizz');
+    } else if (array[index] % 5 === 0) {resultado.push('buzz');
+    } else {resultado.push('bug!');}
+  }return resultado;}
+
+  // Desafio 9
+function encode(string) {
+  let b = []
+  for (let i = 0; i < string.length; i++) {
+    switch (string[i]) {
+      case "a":
+        b[i] = 1
+        break;
+        case "e":
+          b[i] = 2
+          break;
+          case "i":
+            b[i] = 3
+            break;
+            case "o":
+              b[i] = 4
+              break;
+              case "u":
+                b[i] = 5
+                break;
+                default:
+                  b[i] = string[i]
+                  break;
+    }
+  }
+  return b.join('')
 }
 
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
+function decode(string) {
+  let b = []
+  for (let i = 0; i < string.length; i++) {
+    switch (string[i]) {
+      case "1":
+        b[i] = "a"
+        break;
+        case "2":
+          b[i] = "e"
+          break;
+          case "3":
+            b[i] = "i"
+            break;
+            case "4":
+              b[i] = "o"
+              break;
+              case "5":
+                b[i] = "u"
+                break;
+                default:
+                  b[i] = string[i]
+                  break;
+    }
+  }
+  return b.join('')
 }
 
 module.exports = {
