2b3258254e6d2a31accbd6cdd3dcf41779bc8fdc
aaguero96 <andre.luiz_1996@hotmail.com>
2021-08-03 19:57:29 -0300

    Fix Challenge 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -31,7 +31,7 @@ function generatePhoneNumber(arr) {
     for (let i of conf) {
       if ( i >= 3 ) { ver = false; }
     }
-    let number = '(' + arr[0] + arr[1] + ')' + arr[2] + arr[3] + arr[4] + arr[5] + arr[6] + '-' + arr[7] + arr[8] + arr[9]+ arr[10];
+    let number = '(' + arr[0] + arr[1] + ') ' + arr[2] + arr[3] + arr[4] + arr[5] + arr[6] + '-' + arr[7] + arr[8] + arr[9]+ arr[10];
     if (ver === false) {
       return 'não é possível gerar um número de telefone com esses valores';
     } else {
