0d4c82a4c52c1b639e73f421d25170eceef30ee4
Lucas <theluks21@live.com>
2021-08-03 17:49:32 -0300

    adiciona quarto, quinto e sexto requisito

--- src/challenges.js
+++ src/challenges.js
@@ -3,16 +3,15 @@ function compareTrue(a, b) {
   // seu código aqui
   if (a === true && b === true) {
     return true;
-  } else {
-    return false;
   }
+    return false;
 }
 
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui
-  let triangleArea = (base * height) / 2
-  return triangleArea
+  let triangleArea = (base * height) / 2;
+  return triangleArea;
 }
 
 // Desafio 3
@@ -22,21 +21,39 @@ function splitSentence(palavra) {
   return array;
 }
 
-
 // Desafio 4
-function concatName() {
+function concatName(arrayDeStrings) {
   // seu código aqui
+ return arrayDeStrings[arrayDeStrings.length - 1] + ', ' + arrayDeStrings[0];
 }
 
+
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  let totalPoints = (wins * 3) + ties
+  return totalPoints;
 }
 
 // Desafio 6
-function highestCount() {
+function highestCount(numbers) {
   // seu código aqui
+  let resultado = 0;
+  let maiorNumero = -100;
+  for (let index = 0; index < numbers.length; index += 1) {
+    let numeroAtual = numbers[index];
+      if (numeroAtual > maiorNumero) {
+        maiorNumero = numeroAtual;
+      }
+  }
+  for (let index2 = 0; index2 < numbers.length; index2 += 1) {
+    if (numbers[index2] === maiorNumero) {
+      resultado += 1;
+    }
+  }
+  return resultado;
 }
+
 
 // Desafio 7
 function catAndMouse() {
