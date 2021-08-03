2c39c12de7e27ac6b77b2f0ce4d34566112db8a4
ZaQ5 <caique_moreira_sz@hotmail.com>
2021-08-03 15:01:56 -0300

    Finaliza desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,12 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(param1, param2) {
+  if(param1 && param2 == true){
+    return true
+  } 
+  else {
+    return false
+  }
 }
-
 // Desafio 2
 function calcArea() {
   // seu código aqui
