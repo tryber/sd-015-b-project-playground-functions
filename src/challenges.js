e6feb13e94e44c8db98994c6deaf227e291708d4
Marco <marquinco@outlook.com>
2021-08-03 17:41:53 -0300

    linter

--- src/challenges.js
+++ src/challenges.js
@@ -1,169 +1,145 @@
 // Desafio 1
-function compareTrue(par1,par2) {
-  if (par1 === true && par2 === true){
-    return true
-  }
-  else{
-    return false
+function compareTrue(par1, par2) {
+  if (par1 === true && par2 === true) {
+    return true;
   }
 }
 
 // Desafio 2
-function calcArea(base,height) {
-  area = base*height/2
-  return area
+function calcArea(base, height) {
+  let area = base * height;
+  return area / 2;
 }
 
 // Desafio 3
 function splitSentence(str) {
-  separaString =  str.split(" ")
-  return separaString
+  let separaString = str.split(' ');
+  return separaString;
 }
 
 // Desafio 4
 function concatName(vetor) {
   let resultado = [];
-  vector = vetor.reverse()
-  qun = vetor.length-1;
-  for (let index = 0 ; index<=vetor.length-1;index+=1){
-    if (index === 0){
-      resultado.push(vetor[index])
-    }
-    else if (index === vetor.length-1 ){
-      resultado.push(vetor[index])
+  for (let index = 0; index <= vetor.length - 1; index += 1) {
+    if (index === 0) {
+      resultado.push(vetor[index]);
+    } else if (index === vetor.length - 1) {
+      resultado.push(vetor[index]);
     }
   }
-  resultado = resultado.join(', ')
-  return resultado
+  resultado = resultado.join(', ');
+  return resultado;
 }
 
 // Desafio 5
-function footballPoints(wins,ties) {
-  pontos = wins*3 + ties
-  return pontos
+function footballPoints(wins, ties) {
+  let pontos = wins * 3 + ties;
+  return pontos;
 }
 
 // Desafio 6
 function highestCount(vetor) {
-  let count = 0
+  let count = 0;
   let maior = -10000;
-  for (number of vetor){
-    if (number>maior) {
-      maior = number
+  for (number of vetor) {
+    if (number > maior) {
+      maior = number;
     }
   }
-  
+
   for (number of vetor) {
     if (number === maior) {
-      count +=1
+      count += 1;
     }
   }
-  return count
+  return count;
 }
 
 // Desafio 7
-function catAndMouse(mouse,cat1,cat2) {
-  if (cat1> mouse) {
-    distCat1 = cat1-mouse
-    if (cat2>mouse) {
-      distCat2 = cat2-mouse
+function catAndMouse(mouse, cat1, cat2) {
+  if (cat1 > mouse) {
+    let distCat1 = cat1 - mouse;
+    if (cat2 > mouse) {
+      let distCat2 = cat2 - mouse;
+    } else if (cat2 < mouse) {
+      let distCat2 = mouse - cat2;
     }
-    else if (cat2<mouse) {
-      distCat2 = mouse-cat2
-    }
-  }
-  else if (cat1<mouse) {
-    distCat1 = mouse-cat1
-    if (cat2>mouse) {
-      distCat2 = cat2-mouse
-    }
-    else if (cat2<mouse) {
-      distCat2 = mouse-cat2
+  } else if (cat1 < mouse) {
+    let distCat1 = mouse - cat1;
+    if (cat2 > mouse) {
+      let distCat2 = cat2 - mouse;
+    } else if (cat2 < mouse) {
+      let distCat2 = mouse - cat2;
     }
   }
-  if (distCat1>distCat2) {
-    return 'cat2'
+  if (distCat1 > distCat2) {
+    return 'cat2';
   }
-  else if (distCat1<distCat2){
-    return 'cat1'
+  if (distCat1 < distCat2) {
+    return 'cat1';
   }
-  else {
-    return 'os gatos trombam e o rato foge'
-  }
+
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
 function fizzBuzz(vetor) {
-  let vec = []
-  for (number of vetor) {
-    if (number % 3 ===0 && number % 5 ===0) {
-      vec.push("fizzBuzz")
-    }
-    else if (number % 3 ===0) {
-      vec.push("fizz")
-    }
-    else if (number % 5 ===0) {
-      vec.push("buzz")
-    }
-    
-    else {
-      vec.push("bug!")
+  let vec = [];
+  for (let number of vetor) {
+    if (number % 3 === 0 && number % 5 === 0) {
+      vec.push('fizzBuzz');
+    } else if (number % 3 === 0) {
+      vec.push('fizz');
+    } else if (number % 5 === 0) {
+      vec.push('buzz');
+    } else {
+      vec.push('bug!');
     }
   }
-  return vec
+  return vec;
 }
 
 // Desafio 9
 function encode(str) {
-  string = str.split('')
-  let strings = []
-  for (letter of string) {
+  let string = str.split('');
+  let strings = [];
+  for (let letter of string) {
     if (letter === 'a') {
-      strings.push('1')
-    }
-    else if (letter === 'e') {
-      strings.push('2')
-    }
-    else if (letter === 'i') {
-      strings.push('3')
-    }
-    else if (letter === 'o') {
-      strings.push('4')
-    }
-    else if (letter === 'u') {
-      strings.push('5')
-    }
-    else{
-      strings.push(letter)
+      strings.push('1');
+    } else if (letter === 'e') {
+      strings.push('2');
+    } else if (letter === 'i') {
+      strings.push('3');
+    } else if (letter === 'o') {
+      strings.push('4');
+    } else if (letter === 'u') {
+      strings.push('5');
+    } else {
+      strings.push(letter);
     }
   }
-  return strings.join('')
+  return strings.join('');
 }
 
 function decode(str) {
-  string = str.split('')
-  let strings = []
+  string = str.split('');
+  let strings = [];
   for (letter of string) {
     if (letter === '1') {
-      strings.push('a')
-    }
-    else if (letter === '2') {
-      strings.push('e')
-    }
-    else if (letter === '3') {
-      strings.push('i')
-    }
-    else if (letter === '4') {
-      strings.push('o')
-    }
-    else if (letter === '5') {
-      strings.push('u')
-    }
-    else{
-      strings.push(letter)
+      strings.push('a');
+    } else if (letter === '2') {
+      strings.push('e');
+    } else if (letter === '3') {
+      strings.push('i');
+    } else if (letter === '4') {
+      strings.push('o');
+    } else if (letter === '5') {
+      strings.push('u');
+    } else {
+      strings.push(letter);
     }
   }
-  return strings.join('')
+  return strings.join('');
 }
 
 module.exports = {
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,66 +1,63 @@
 // Desafio 10
-function techList(vetor,name) {
-  vector = vetor.sort()
-  let obj = {}
-  let resp = []
-  for (index in vector) {
+function techList(vetor, name) {
+  let vector = vetor.sort();
+  let obj = {};
+  let resp = [];
+  for (let index in vector) {
     obj[index] = {
       tech: vetor[index],
-      name: name
-    }
-    resp.push(obj[index])
+      name,
+    };
+    resp.push(obj[index]);
+  }
+  if (vetor.length > 0) {
+    return resp;
+  }
 
-  }
-  if (vetor.length>0) {
-    return resp
-  }
-  else{
-    return "Vazio!"
-  }
+  return 'Vazio!';
 }
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],"Lucas"));
+console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 // Desafio 11
 function generatePhoneNumber(vetor) {
-  if (vetor.length!=11) {
-    return "Array com tamanho incorreto."
+  if (vetor.length !== 11) {
+    return 'Array com tamanho incorreto.';
   }
-  ramal = '(' + vetor[0] + vetor[1] + ')';
-  numFirst = ' '+vetor[2] + vetor[3] + vetor[4] + vetor [5] + vetor[6];
-  numLast = '-'+vetor[7]+vetor[8]+vetor[9]+vetor[10];
-  for (numero of vetor) {
-    let count = 0
+  const erro = 'não é possível gerar um número de telefone com esses valores';
+  let ramal = `(${vetor[0]}${vetor[1]})`;
+  let numFirst = ` ${vetor[2]}${vetor[3]}${vetor[4]}${vetor[5]}${vetor[6]}`;
+  let numLast = `-${vetor[7]}${vetor[8]}${vetor[9]}${vetor[10]}`;
+  for (let numero of vetor) {
+    let count = 0;
     if (numero < 0) {
-      return "não é possível gerar um número de telefone com esses valores"
+      return erro;
     }
-    else if (numero>9) {
-      return "não é possível gerar um número de telefone com esses valores"
+    if (numero > 9) {
+      return erro;
     }
-    for (index in vetor) {
-      if (vetor[index] === numero){
-        count +=1
-        if (count ===3) {
-          return "não é possível gerar um número de telefone com esses valores"
+    for (let index in vetor) {
+      if (vetor[index] === numero) {
+        count += 1;
+        if (count === 3) {
+          return erro;
         }
       }
     }
   }
-  return ramal+numFirst+numLast
+  return ramal + numFirst + numLast;
 }
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 // Desafio 12
-function triangleCheck(lineA,lineB,lineC) {
-  if (lineA < lineB+lineC && lineA > Math.abs(lineB-lineC)) {
-    return true
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA < lineB + lineC && lineA > Math.abs(lineB - lineC)) {
+    return true;
   }
-  else if (lineB < lineA+lineC && lineB > Math.abs(lineA-lineC)) {
-    return true
+  if (lineB < lineA + lineC && lineB > Math.abs(lineA - lineC)) {
+    return true;
   }
-  else if (lineC < lineB+lineA && lineC > Math.abs(lineB-lineA)) {
-    return true
+  if (lineC < lineB + lineA && lineC > Math.abs(lineB - lineA)) {
+    return true;
   }
-  else {
-    return false
-  }
+
+  return false;
 }
 
 // Desafio 13
