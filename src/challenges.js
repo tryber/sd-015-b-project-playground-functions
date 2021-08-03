e0e86dfc323f4f25c4937db9a6c344822ced6654
leandro oliveira <leandrooliveira35@gmail.com>
2021-08-03 15:09:19 -0300

    requisitos obrigatorios concluidos

--- src/challenges.js
+++ src/challenges.js
@@ -46,21 +46,83 @@ function highestCount(array) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (Math.abs(mouse - cat1) < Math.abs(mouse - cat2)) {
+    return 'cat1';
+  } if (Math.abs(mouse - cat2) < Math.abs(mouse - cat1)) {
+    return 'cat2';
+  }
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let resultado = [];
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] % 3 === 0 && array[index] % 5 === 0) {
+      resultado.push('fizzBuzz');
+    } else if (array[index] % 3 === 0) {
+      resultado.push('fizz');
+    } else if (array[index] % 5 === 0) {
+      resultado.push('buzz');
+    } else { resultado.push('bug!'); }
+  }
+  return resultado;
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let code = [];
+  for (let index = 0; index < string.length; index += 1) {
+    switch (string[index]) {
+    case 'a':
+      code[index] = 1;
+      break;
+    case 'e':
+      code[index] = 2;
+      break;
+    case 'i':
+      code[index] = 3;
+      break;
+    case 'o':
+      code[index] = 4;
+      break;
+    case 'u':
+      code[index] = 5;
+      break;
+    default:
+      code[index] = string[index];
+      break;
+    }
+  }
+  return code.join('');
 }
-function decode() {
-  // seu código aqui
+
+function decode(string) {
+  let code = [];
+  for (let index = 0; index < string.length; index += 1) {
+    switch (string[index]) {
+    case '1':
+      code[index] = 'a';
+      break;
+    case '2':
+      code[index] = 'e';
+      break;
+    case '3':
+      code[index] = 'i';
+      break;
+    case '4':
+      code[index] = 'o';
+      break;
+    case '5':
+      code[index] = 'u';
+      break;
+    default:
+      code[index] = string[index];
+      break;
+    }
+  }
+  return code.join('');
 }
 
 module.exports = {
