71fb1d2d75025929c0216cbc8d869cd08f62e10f
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-08-03 18:45:39 -0400

    add challenge 8

--- src/challenges.js
+++ src/challenges.js
@@ -75,7 +75,21 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(array) {
+  let  num = [];
+  for (i = 0 ; i < array.length ; i += 1 )
+  if (array[i] % 3 === 0 && array[i] % 5 === 0) {
+    num.push("fizzBuzz");
+  }
+  else if (array[i] % 5 === 0) {
+    num.push("buzz");
+  }
+  else if (array[i] % 3 === 0)
+    num.push("fizz")
+  else {
+    num.push("bug!");
+  }
+  return num
   // seu código aqui
 }
 
