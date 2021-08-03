94743836a378f2713647e8a4a5b31f012e3cfd8f
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-08-03 20:22:33 -0300

    Realizando o desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -124,11 +124,25 @@ function fizzBuzz(numbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(phrase) {
+  
+  let code = phrase.replace(/a/gi, "1");
+  code = code.replace(/e/gi, "2");
+  code = code.replace(/i/gi, "3");
+  code = code.replace(/o/gi, "4");
+  code = code.replace(/u/gi, "5");
+
+  return code;
 }
-function decode() {
-  // seu código aqui
+
+function decode(codedPhrase) {
+  let code = codedPhrase.replace(/1/gi, "a");
+  code = code.replace(/2/gi, "e");
+  code = code.replace(/3/gi, "i");
+  code = code.replace(/4/gi, "o");
+  code = code.replace(/5/gi, "u");
+
+  return code;
 }
 
 module.exports = {
