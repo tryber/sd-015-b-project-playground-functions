802c122498f5d384c20207fc50cb3138bf715001
Flávia Labanca <flavialabanca@gmail.com>
2021-08-03 19:15:11 -0300

    requisito 4 e 5

--- src/challenges.js
+++ src/challenges.js
@@ -6,9 +6,9 @@ function compareTrue(parmUm, parmDois) {
       return false;
     }
 }
-console.log(compareTrue(false,true))
-console.log(compareTrue(false,false))
-console.log(compareTrue(true,true))
+//console.log(compareTrue(false,true))
+//console.log(compareTrue(false,false))
+//console.log(compareTrue(true,true))
 
 
 // Desafio 2
@@ -16,9 +16,9 @@ function calcArea(base,height) {
   let areaTriangulo = (base * height) / 2;
   return areaTriangulo;
 }
-console.log(calcArea(10,50));
-console.log(calcArea(5,2));
-console.log(calcArea(51,1));
+//console.log(calcArea(10,50));
+//console.log(calcArea(5,2));
+//console.log(calcArea(51,1));
 
 
 // Desafio 3
@@ -27,25 +27,86 @@ function splitSentence(frase) {
   fraseSeparada = frase.split(" ");
   return fraseSeparada;
 }
-console.log(splitSentence('go Trybe'));
-console.log(splitSentence('vamo que vamo'));
-console.log(splitSentence('foguete'));
+//console.log(splitSentence('go Trybe'));
+//console.log(splitSentence('vamo que vamo'));
+//console.log(splitSentence('foguete'));
 
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(palavras) {
+  let primeiraPalavra = '';
+  let ultimaPalavra = '';
+  let fraseJunta = '';
+  primeiraPalavra = palavras[0];
+  for (let index = 0; index < palavras.length; index++){
+    if (index === (palavras.length - 1)) {
+      ultimaPalavra = palavras[index];
+    }
+  }
+  fraseJunta = (ultimaPalavra + ', ' + primeiraPalavra)
+  return fraseJunta;
 }
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
+console.log(concatName(['foguete', 'não', 'tem', 'ré']));
+console.log(concatName(['captain', 'my', 'captain']));
+
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let totalVitorias = 0;
+  let totalEmpates = 0;
+  let totalPontos = 0;
+
+  totalVitorias = wins * 3;
+  totalEmpates = ties * 1;
+  totalPontos = totalVitorias + totalEmpates
+  return totalPontos;
 }
+console.log(footballPoints(14,8)); // retornar 50
+console.log(footballPoints(1,2)); // retornar 5
+console.log(footballPoints(0,0)); // retornar 0
+
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(arrayNum) {
+  let maiorNumero = 0;
+  let cont = [];
+
+  for (let indexUm = 0; indexUm < arrayNum.lenght; indexUm++){
+    let number = arrayNum[indexUm];
+    if (number >= maiorNumero){
+      maiorNumero = number;
+    }
+  }
+
+  for (let indexDois = 0; indexDois < arrayNum.lenght; indexDois++){
+    if (maiorNumero === arrayNum[indexDois]){
+      cont.push(maiorNumero);
+    }
+  }
+
+  return cont.lenght;
+
 }
+
+console.log(highestCount([9, 1, 2, 3, 9, 5, 7])); // retornar 2
+console.log(highestCount([0, 4, 4, 4, 9, 2, 1])); // retornar 1
+console.log(highestCount([0, 0, 0])); // retornar 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Desafio 7
 function catAndMouse() {
