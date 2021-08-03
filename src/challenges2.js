f48128bbee52936da86fdfcd379783680802e1d6
Gustavo Gracio <gustavogracio@hotmail.com>
2021-08-03 19:32:49 -0300

    Requisito 10 completo

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,20 @@
 // Desafio 10
-function techList() {
+function techList(array, name) {
   // seu código aqui
+  let objeto = [];
+  if (array.length <= 0) {
+    return 'Vazio!';
+  }
+  for (let key = 0; key < array.length; key += 1) {
+    objeto.push({
+      tech: array[key],
+      name,
+    });
+  }
+
+  return objeto.sort(function (a, b) {
+    return a.tech > b.tech;
+  });
 }
 
 // Desafio 11
