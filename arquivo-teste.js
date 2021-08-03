baa333741e0e3a2e261291ae8fb0486763d72d15
Murilo <murilocarrijoadm@gmail.com>
2021-08-03 18:08:05 -0300

    adiciona exericio 1, 2, 3

--- arquivo-teste.js
+++ arquivo-teste.js
@@ -1,21 +1,15 @@
-
-// 1 - Crie uma função usando o operador &&
-
 function compareTrue(valor1, valor2){ 
     return valor1 && valor2;
 };
 
-//2 - Crie uma função que calcule a área de um triângulo
 
 function calcArea (base, height) {
     return ((base * height) / 2);
     };
 
-//3 - Crie uma função que divida a frase
 
 function splitSentence(text) {
     let separa = text.split(' ');
     return separa;
     };
 
-    
---
+++ playground-functions.js
@@ -0,0 +1,14 @@
+function compareTrue(valor1, valor2){ 
+    return valor1 && valor2;
+};
+
+
+function calcArea (base, height) {
+    return ((base * height) / 2);
+    };
+
+
+function splitSentence(text) {
+    let separa = text.split(' ');
+    return separa;
+    };
