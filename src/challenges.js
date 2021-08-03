3648b2b56625c43dc59e08c8f2364bfe94132a45
Erik Lopes <erik.lopesdev@outlook.com>
2021-08-03 17:20:54 -0300

    Nono exercicio

--- src/challenges.js
+++ src/challenges.js
@@ -1,14 +1,14 @@
 // Desafio 1
 function compareTrue(a, b) {
-  if(a == true && b == true){
+  if (a == true && b == true) {
     return true
-  }else{
+  } else {
     return false
   }
 }
 // Desafio 2
 function calcArea(base, height) {
-    return (base * height) / 2
+  return (base * height) / 2
 }
 // Desafio 3
 function splitSentence(string) {
@@ -30,8 +30,8 @@ function footballPoints(wins, ties) {
 function highestCount(numeros) {
   let a = Math.max.apply(null, numeros)
   let contador = 0;
-  for(let i = 0; i <= numeros.length; i++){
-    if(a == numeros[i]){
+  for (let i = 0; i <= numeros.length; i++) {
+    if (a == numeros[i]) {
       contador++
     }
   }
@@ -39,39 +39,86 @@ function highestCount(numeros) {
 }
 
 // Desafio 7
-function catAndMouse( mouse, cat1, cat2, ) {
-    let distanciaCat1 = Math.abs(cat1 - mouse)
-    let distanciaCat2 =  Math.abs(cat2 - mouse)
-    if(distanciaCat1 < distanciaCat2){
-      return "cat1"
-    }else if(distanciaCat2 < distanciaCat1){
-      return "cat2"
-    }else{
-      return "os gatos trombam e o rato foge"
-    }
+function catAndMouse(mouse, cat1, cat2, ) {
+  let distanciaCat1 = Math.abs(cat1 - mouse)
+  let distanciaCat2 = Math.abs(cat2 - mouse)
+  if (distanciaCat1 < distanciaCat2) {
+    return "cat1"
+  } else if (distanciaCat2 < distanciaCat1) {
+    return "cat2"
+  } else {
+    return "os gatos trombam e o rato foge"
+  }
 }
 // Desafio 8
 function fizzBuzz(numeros) {
   let array = []
-    for(let i = 0; i < numeros.length; i++){
-      if(numeros[i] % 3 === 0 && numeros[i] % 5 === 0){
-        array.push('fizzBuzz') 
-      }else if(numeros[i] % 3 === 0){
-        array.push('fizz')
-      }else if(numeros[i] % 5 === 0){
-        array.push('buzz')
-      }else{
-        array.push('bug!')
-      }
+  for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] % 3 === 0 && numeros[i] % 5 === 0) {
+      array.push('fizzBuzz')
+    } else if (numeros[i] % 3 === 0) {
+      array.push('fizz')
+    } else if (numeros[i] % 5 === 0) {
+      array.push('buzz')
+    } else {
+      array.push('bug!')
     }
-    return array
   }
-// Desafio 9
-function encode() {
-  // seu código aqui
+  return array
 }
-function decode() {
-  // seu código aqui
+// Desafio 9
+function encode(string) {
+  let b = []
+  for (let i = 0; i < string.length; i++) {
+    switch (string[i]) {
+      case 'a':
+        b[i] = 1
+        break;
+      case 'e':
+        b[i] = 2
+        break;
+      case 'i':
+        b[i] = 3
+        break;
+      case 'o':
+        b[i] = 4
+        break;
+      case 'u':
+        b[i] = 5
+        break;
+      default:
+        b[i] = string[i]
+        break;
+    }
+  }
+  return b.join('')
+}
+
+function decode(string) {
+  let b = []
+  for (let i = 0; i < string.length; i++) {
+    switch (string[i]) {
+      case '1':
+        b[i] = 'a'
+        break;
+      case '2':
+        b[i] = 'e'
+        break;
+      case '3':
+        b[i] = 'i'
+        break;
+      case '4':
+        b[i] = 'o'
+        break;
+      case '5':
+        b[i] = 'u'
+        break;
+      default:
+        b[i] = string[i]
+        break;
+    }
+  }
+  return b.join('')
 }
 
 module.exports = {
