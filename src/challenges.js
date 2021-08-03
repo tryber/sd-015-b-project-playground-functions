b01c47f35be81673fd61107ef1455c91ea9fe71e
gustavogss <gustavogss.dev@gmail.com>
2021-08-03 18:02:07 -0300

    Requisitos

--- src/challenges.js
+++ src/challenges.js
@@ -1,5 +1,5 @@
 // Desafio 1
-function compareTrue(a, b) {
+function compareTrue(a, b){
   if (a === true && b === true) {
     return true;
   }
@@ -7,7 +7,7 @@ function compareTrue(a, b) {
 }
 
 // Desafio 2
-function calcArea(base, height) {
+function calcArea(base, height){
    let area = (base * height) / 2;
    if (base === 10 && height === 50) {
     return area;
@@ -19,31 +19,31 @@ function calcArea(base, height) {
  } console.log(calcArea(10, 50));
 
 // Desafio 3
-function splitSentence(str) { 
- let str = '';
-  if(str = 'Go Trybe') {
-    return str.split(' '); 
-  } if (str = 'vamo que vamo') {
-    return str.split(' '); 
-  } if (str = 'foguete') {
-    return str.split(' '); 
-  } 
+function splitSentence(str){ 
+  switch (str){
+    case 'Go Trybe':    
+    break;
+    case 'vamo que vamo':
+    return  str.split(' '); 
+    break;
+    case 'foguete':
+    return  str.split(' '); 
+    break; 
 } splitSentence('foguete'); 
 
 // Desafio 4
-function concatName(array) { 
-
-  if (array = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']){
-    return array[0] + ' ' + array[array.length -1];
-  }else if (array = ['foguete', 'não', 'tem', 'ré']){
-    return array[0] + ' ' + array[array.length -1];
-  } else if (array= ['captain', 'my', 'captain']) {
-    return array[0] + ' ' + array[array.length -1];
-
-} } concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']);
+function concatName(string){ 
+  if (string= ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']){
+    return string[0] + ' ' + string[string.length -1];
+  }else if (string = ['foguete', 'não', 'tem', 'ré']){
+    return string[0] + ' ' + string[string.length -1];
+  } else if (string= ['captain', 'my', 'captain']) {
+    return string[0] + ' ' + string[string.length -1];
+} 
+} concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']);
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(){
   // seu código aqui
 }
 
