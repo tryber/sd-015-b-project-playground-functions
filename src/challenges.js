96f359160a3e388e6e28baeef58ebe3a9276ca74
alvesgf16 <alvesgabriel1994@gmail.com>
2021-08-03 15:26:27 -0300

    Adiciona desafio 8

--- src/challenges.js
+++ src/challenges.js
@@ -54,8 +54,24 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let fizzBuzzArray = [];
+
+  for (index = 0; index < array.length; index += 1) {
+    if (array[index] % 3 === 0 || array[index] % 5 === 0) {
+      if (array[index] % 3 === 0 && array[index] % 5 === 0) {
+        fizzBuzzArray.push("fizzBuzz");
+      } else if (array[index] % 3 === 0 && array[index] % 5 !== 0) {
+        fizzBuzzArray.push("fizz");
+      } else if (array[index] % 3 !== 0 && array[index] % 5 === 0) {
+        fizzBuzzArray.push("buzz");
+      }
+    } else {
+      fizzBuzzArray.push("bug!");
+    }
+  }
+
+  return fizzBuzzArray;
 }
 
 // Desafio 9
