0655190d365752d76d7b01edc80a9ebc85261252
Jesse Benevides <jessebenevides@gmail.com>
2021-08-03 17:32:46 -0300

    Adiciona requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -60,8 +60,17 @@ function highestCount(numbers) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distanceCat1 = Math.abs(cat1 - mouse);
+  let distanceCat2 = Math.abs(cat2 - mouse);
+
+  if (distanceCat1 < distanceCat2) {
+    return 'cat1';
+  } else if (distanceCat2 < distanceCat1) {
+    return 'cat2';
+  } else {
+    return 'os gatos trombam e o rato foge';
+  }
 }
 
 // Desafio 8
