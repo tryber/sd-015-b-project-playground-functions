ee61ce87d06ec6f4304afcd98e6d9582e04da694
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-08-03 16:13:50 -0300

    Finaliza o desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -47,9 +47,15 @@ function highestCount(arrayOfNumbers) {
 };
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+function catAndMouse(mouse, cat1, cat2) {
+  if (mouse - cat1 < mouse - cat2) {
+    return "cat1";
+  } else if (mouse - cat2 < mouse - cat1) {
+    return "cat2";
+  } else {
+    return "os gatos trombam e o rato foge"
+  }
+};
 
 // Desafio 8
 function fizzBuzz() {
