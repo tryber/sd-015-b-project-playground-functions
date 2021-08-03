3b0432dcf7af4d14a8ce9832f2369fe22edd0eba
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-08-03 18:25:43 -0300

    Finaliza desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -23,13 +23,17 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA < lineB + lineC && lineA > lineB - lineC && lineB < lineA + lineC && lineB > lineA - lineC && lineC < lineB + lineA && lineC > lineB - lineA) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 // Desafio 13
 function hydrate() {
-  // seu código aqui
+  
 }
 
 module.exports = {
