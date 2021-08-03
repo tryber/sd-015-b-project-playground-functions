819272925e6b0219a2f57a2d5eb1f671ad8f5f4a
Kdulima <carlosedu151@gmail.com>
2021-08-03 17:00:50 -0300

    Resolução requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -6,7 +6,20 @@ console.log(compareTrue(true, true));
 
 // Desafio 2
 function calcArea() {
-  // seu código aqui
+  function calcArea (base, height) {
+    return base * height /2;
+} 
+console.log(calcArea (10, 50));
+
+function calcArea (base, height) {
+    return base * height /2;
+} 
+console.log(calcArea (5, 2));
+
+function calcArea (base, height) {
+    return base * height /2;
+} 
+console.log(calcArea (51, 1));
 }
 
 // Desafio 3
