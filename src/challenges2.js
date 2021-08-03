df7474c2f8c774b358e40bb41dc00f9ed7a0d0c3
Huijiro <huijirohankei@gmail.com>
2021-08-03 16:51:03 -0300

    Feito Exercicios 1-10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,37 @@
 // Desafio 10
-function techList() {
+function techList(array, name) {
   // seu código aqui
+  // Sort feito com ajuda de "https://flaviocopes.com/how-to-sort-array-of-objects-by-property-javascript/"
+
+  let list = [];
+  if (array.length !== 0) {
+    array.forEach((element) => {
+      let obj = {
+        tech: '',
+        name: '',
+      };
+      obj.tech = element;
+      obj.name = name;
+      list.push(obj);
+    });
+    return list.sort((a, b) => ((a.tech > b.tech) ? 1 : -1));
+  }
+  return 'Vazio!';
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(array) {
   // seu código aqui
+  let arraySort = array.sort;
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index = 0; index < arraySort.length; index += 1) {
+    if (arraySort[index] === arraySort[index + 1] && arraySort[index] === arraySort[index + 2]) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  return `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
 }
 
 // Desafio 12
