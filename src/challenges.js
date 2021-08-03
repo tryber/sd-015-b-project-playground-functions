bd69ecf51d037db3366ffef043f4b067eadddcf7
jpcp0614 <jpcp0614@gmail.com>
2021-08-03 15:19:41 -0300

    fix: Desafio 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,5 +1,6 @@
 // Desafio 1
 function compareTrue(valor1, valor2) {
+
   if (valor1 === true && valor2 === true) {
     return true;
   }
@@ -7,8 +8,9 @@ function compareTrue(valor1, valor2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+
+  return ((base * height) / 2);
 }
 
 // Desafio 3
