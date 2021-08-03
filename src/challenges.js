6e61f021c3f190c5ff8f636525fe38272a55f232
Gabriel Peralta <prates_peralta@hotmail.com>
2021-08-03 18:43:56 -0300

    adiciona a oitava função

--- src/challenges.js
+++ src/challenges.js
@@ -56,8 +56,20 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numbers) {
+  let result = []
+  for(let i = 0; i < numbers.length; i += 1) {
+    if(numbers[i] % 3 === 0 && numbers[i] % 5 === 0) {
+      result.push('fizzBuzz');
+    } else if(numbers[i] % 3 === 0) {
+      result.push('fizz');
+    } else if(numbers[i] % 5 === 0) {
+      result.push('buzz')
+    } else {
+      result.push('bug!')
+    }
+  }
+  return result;
 }
 
 // Desafio 9
