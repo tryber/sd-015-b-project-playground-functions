26c945fd4070f325b659f443f22ecec21f839901
alvesgf16 <alvesgabriel1994@gmail.com>
2021-08-03 14:52:46 -0300

    Adiciona desafios 1 a 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,26 +1,25 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(param1, param2) {
+  return param1 && param2;
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return base * height / 2;
 }
-
-// Desafio 3
-function splitSentence() {
-  // seu código aqui
+ // Desafio 3
+function splitSentence(string) {
+  return string.split(" ");
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  return array[array.length - 1] + ", " + array[0];
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return wins * 3 + ties;
 }
 
 // Desafio 6
