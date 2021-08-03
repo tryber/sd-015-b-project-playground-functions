9ef7bea70fdba0444ff60be4b6e8ef4d3855d380
joaocla <jc.claudio.bento@gmail.com>
2021-08-03 20:24:59 -0300

    Corrige fizzBuzz utilizando while para o loop

--- src/challenges.js
+++ src/challenges.js
@@ -7,20 +7,20 @@ function compareTrue(a, b) {
   else {
     return false;
   }
-}
+}// Fiz uma funcao simples... apenas comparando valores booleanos
 
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui
   return (base * height) / 2;
-}
+}// Fiz calculo ja no return, para diminuir o tamanho da funcao
 
 // Desafio 3
 function splitSentence(frase) {
   // seu código aqui
   let worlds = frase.split(' ');
   return worlds;
-}
+}// Utilizei o split() para separar a string em substrings, passando um parametro para separar a cada espaco ' '.
 
 // Desafio 4
 function concatName(lista) {
@@ -32,7 +32,7 @@ function concatName(lista) {
 function footballPoints(wins, ties) {
   // seu código aqui
   return (wins * 3) + ties;
-}
+}// outra funcao simples, passando o calculo ja no return
 
 // Desafio 6
 function highestCount(lista) {
@@ -47,7 +47,7 @@ function highestCount(lista) {
     }
   }
   return repete;
-}// Aqui utilizei um metodo que econtrei na documentacao, que compara cada valor para retornar o maior (dentro de uma array). Econtrei outros como o getMaxOfArray mas achei ele complexo... creio que o array.reduce() fiquei mais simplificado de entender no codigo.
+}// Aqui utilizei um metodo que econtrei na documentacao, que compara cada valor para retornar o maior (dentro de uma array). Econtrei outros como o getMaxOfArray mas achei ele complexo... creio que o array.reduce() fique mais simplificado de entender no codigo.
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
@@ -67,9 +67,26 @@ function catAndMouse(mouse, cat1, cat2) {
 }// Econtrei a funcao Math.abs no documento, estava procurando uma forma de "anular" o numero negativo e consegui por meio dela
 
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(lista) {
   // seu código aqui
-}
+  let index = 0;
+  while (index < lista.length) {
+    if ((lista[index] % 3 == 0) && (lista[index] % 5 != 0)) {
+      lista[index] = "fizz";
+    }
+    else if ((lista[index] % 3 != 0) && (lista[index] % 5 == 0)) {
+      lista[index] = "buzz";
+    }
+    else if ((lista[index] % 3 == 0) && (lista[index] % 5 == 0)) {
+      lista[index] = "fizzBuzz";
+    }
+    else if ((lista[index] % 3 != 0) && (lista[index] % 5 != 0)) {
+      lista[index] = "bug!";
+    }
+    index ++;
+  }
+  return lista;
+}// Aqui eu utilizei o while, pois o meu loop com o for estava infinito... bom demorei um pouco para entender as condicoes, mas utilizei uma calculadora para encontrar um padrao, e este padrao foi o numero 0.
 
 // Desafio 9
 function encode() {
