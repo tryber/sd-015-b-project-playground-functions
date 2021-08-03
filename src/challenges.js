d28ae92f6e3cee7ad1394229dcd0387c02e10312
Antônio Campos <aclcampos@live.com>
2021-08-03 17:59:22 -0300

    Realizado Desafio 4

--- src/challenges.js
+++ src/challenges.js
@@ -21,8 +21,9 @@ function splitSentence(string) {
 }
 
 // Desafio 4
-function concatName() {
+function concatName(array) {
   // seu código aqui
+    return array[array.length-1] + ', ' + array[0]
 }
 
 // Desafio 5
