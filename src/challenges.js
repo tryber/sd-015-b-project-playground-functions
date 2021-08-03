a9a7d452d35af3eeb01499fd5ac2f1171d8ec943
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-08-03 15:32:09 -0300

    Adiciona exercicios 1 a 6

--- src/challenges.js
+++ src/challenges.js
@@ -1,31 +1,64 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(valor1, valor2) {
   // seu código aqui
+  if (valor1 === true && valor2 === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  let area = (base * height) / 2;
+  return area;
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(str) {
   // seu código aqui
+  strSplit = str.split(' ');
+  return strSplit;
 }
 
 // Desafio 4
-function concatName() {
+function concatName(arr) {
   // seu código aqui
+  let lastIndex = arr.length - 1;
+  let newOrder = arr[lastIndex] + ', ' + arr[0];
+  return newOrder;
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  let pointsWins = wins * 3;
+  let pointsTies = ties * 1;
+
+  let pointsTotal = pointsWins + pointsTies;
+
+  return pointsTotal;
 }
 
 // Desafio 6
-function highestCount() {
+function highestCount(arr) {
   // seu código aqui
+  let counter = 0;
+  let largestNumber = arr[0];
+
+  for (index in arr) {
+    if (arr[index] > largestNumber) {
+      largestNumber = arr[index];
+    }
+  }
+
+  for (index in arr) {
+    if (arr[index] === largestNumber) {
+      counter += 1;
+    }
+  }
+  return counter;
 }
 
 // Desafio 7
