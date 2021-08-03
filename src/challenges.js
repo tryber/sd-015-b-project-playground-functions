dd538ed509375cd66683cb216a711ea926235c35
Arthur Teixeira <tuzinf5@gmail.com>
2021-08-03 15:07:42 -0300

    iniciando o projeto. Adiciona requisito 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,5 +1,10 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(valor1, valor2) {
+  if (valor1 && valor2 === true) {
+    return true;
+  } else {
+    return false;
+  }
   // seu código aqui
 }
 
