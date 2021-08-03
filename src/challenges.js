5f8567e67cfe538dfc8d62c45d4d19669a39f598
douglas <douglasmonteiro.m@gmail.com>
2021-08-03 18:21:23 -0300

    Desafio 4

--- src/challenges.js
+++ src/challenges.js
@@ -19,9 +19,10 @@ function splitSentence(string) {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  return array[array.length -1] +', '+ array[0]
 }
+
 
 // Desafio 5
 function footballPoints() {
