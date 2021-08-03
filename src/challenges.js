7de3f1e1ea6e39855154a4dd76a78981c2e67276
Thirki <cicero.santos62@etec.sp.gov.br>
2021-08-03 19:18:03 -0300

    feat: resolvendo o desafio 9 do challenges.js

--- src/challenges.js
+++ src/challenges.js
@@ -80,11 +80,53 @@ function fizzBuzz(numberArr) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(str) {
+  let separaLetras = str.split('');
+  let vogalCodificada = [];
+  console.log(separaLetras.length);
+
+  for (let i = 0; i < separaLetras.length; i++) {
+    if (separaLetras[i] === 'a') {
+      vogalCodificada.push(1);
+    } else if (separaLetras[i] === 'e') {
+      vogalCodificada.push(2);
+    } else if (separaLetras[i] === 'i') {
+      vogalCodificada.push(3);
+    } else if (separaLetras[i] === 'o') {
+      vogalCodificada.push(4);
+    } else if (separaLetras[i] === 'u') {
+      vogalCodificada.push(5);
+    } else {
+      vogalCodificada.push(separaLetras[i]);
+    }
+  }
+
+  let vogalParaNumero = vogalCodificada.join('');
+  return vogalParaNumero;
 }
-function decode() {
-  // seu código aqui
+function decode(str) {
+  let separaLetras = str.split('');
+  let vogalCodificada = [];
+  console.log(separaLetras.length);
+
+  for (let i = 0; i < separaLetras.length; i++) {
+    if (separaLetras[i] === '1') {
+      vogalCodificada.push('a');
+    } else if (separaLetras[i] === '2') {
+      vogalCodificada.push('e');
+    } else if (separaLetras[i] === '3') {
+      vogalCodificada.push('i');
+    } else if (separaLetras[i] === '4') {
+      vogalCodificada.push('o');
+    } else if (separaLetras[i] === '5') {
+      vogalCodificada.push('u');
+    } else {
+      vogalCodificada.push(separaLetras[i]);
+    }
+  }
+
+  let vogalParaNumero = vogalCodificada.join('');
+  return vogalParaNumero;
 }
 
 module.exports = {
