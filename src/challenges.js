99072e367e2267e1d7fb076372a5208ddd478c1f
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-08-03 15:48:13 -0300

    Desafio 5

--- src/challenges.js
+++ src/challenges.js
@@ -29,8 +29,9 @@ function concatName(valor) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let points = (wins * 3) + ties;
+  return points;
 }
 
 // Desafio 6
