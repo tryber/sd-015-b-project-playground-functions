f2fd52e1ecbd0e8259eab165d9e8fec5fcb449da
LaizaBertelli <laizabertelli@hotmail.com>
2021-08-03 15:35:37 -0300

    Resolução dos requisitos 3, 4, 5 e 8

--- src/challenges.js
+++ src/challenges.js
@@ -1,4 +1,4 @@
-// Desafio 1
+// Desafio 1 - CHECK
 function compareTrue(bool1, bool2) {
   // seu código aqui
   if(bool1 && bool2){
@@ -7,45 +7,84 @@ function compareTrue(bool1, bool2) {
     return false;
   }
 }
-// console.log(compareTrue(false, false));
 
-// Desafio 2
+// Desafio 2 - CHECK
 function calcArea(base, height) {
   // seu código aqui
   return (base * height)/2;
 }
-// console.log(calcArea(51, 1));
 
-// Desafio 3
-function splitSentence() {
+
+// Desafio 3 - CHECK
+function splitSentence(orignalString) {
   // seu código aqui
-  
+  let splited = [];
+  splited = orignalString.split(" ");
+  return splited;
 }
 
-// Desafio 4
-function concatName() {
+// Desafio 4 - CHECK
+function concatName(stringsArray) {
   // seu código aqui
+  let concat = stringsArray.pop() +", "+ stringsArray.shift(); //
+  return concat;
 }
 
-// Desafio 5
-function footballPoints() {
+// Desafio 5 - CHECK
+function footballPoints(wins, ties) {
   // seu código aqui
+  let score = (wins*3) + ties;
+  return score;
 }
 
 // Desafio 6
-function highestCount() {
+function highestCount(numbers) {
   // seu código aqui
+  //1 - descobrir qual o maior número;
+ //2 - contar quantas vezes ele se repete;
 }
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+  let distanceCat1 = mouse - cat1;
+  let distanceCat2 = mouse - cat2;
+
+  if(distanceCat1 <0){
+    distanceCat1 = distanceCat1*(-1);
+  }
+  if(distanceCat2 < 0){
+    distanceCat2 = distanceCat2*(-1);
+  }
+  if(distanceCat2 === distanceCat1){
+    return 'os gatos trombam e o rato foge';
+  }else if(distanceCat1 > distanceCat2){
+    return 'cat1';
+  }else{
+    return 'cat2';
+  }
+
 }
 
-// Desafio 8
-function fizzBuzz() {
+// Desafio 8 - CHECK
+function fizzBuzz(numbersArray) {
   // seu código aqui
+  let fizzBuzzBug = [];
+  
+  for(var i = 0; i < numbersArray.length; i ++){
+    if(numbersArray[i] % 3 === 0 && numbersArray[i]%5 === 0 ){
+      fizzBuzzBug.push('fizzBuzz');
+    }else if(numbersArray[i]%3 === 0){
+      fizzBuzzBug.push('fizz');
+    } else if( numbersArray[i]%5 === 0){
+      fizzBuzzBug.push('buzz');
+    } else{
+      fizzBuzzBug.push('bug!');
+    }
+  }
+  return fizzBuzzBug;
 }
+
 
 // Desafio 9
 function encode() {
