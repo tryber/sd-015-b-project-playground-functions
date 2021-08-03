6661a41668370913c33d37de92e58f8745914067
find_CarolinaCosta <carolinadacosta1997@gmail.com>
2021-08-03 15:20:00 -0300

    Resolve requisito 3

--- src/challenges.js
+++ src/challenges.js
@@ -13,8 +13,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(stringValor) {
+  let split = stringValor.split(' ');
+  return split;
 }
 
 // Desafio 4
