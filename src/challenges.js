58be19f59bcd966be7dbe38afe08ad8d50b2d1c2
Wilian Pereira <wilian_pereira92@outlook.com>
2021-08-03 17:14:05 -0300

    Desafio 5: Pontos no futebol

--- src/challenges.js
+++ src/challenges.js
@@ -15,20 +15,22 @@ function calcArea(base, height) {
 // Desafio 3
 function splitSentence(str) {
   // seu código aqui
-  let array = str.split (" ");
+  let array = str.split(' ');
   return array;
 }
 
 // Desafio 4
 function concatName(array) {
   // seu código aqui
-  let resultado = (array[array.length -1] + ', ' + array[0]);
+  let resultado = (array[array.length - 1] + ' , ' + array[0]);
   return resultado;
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  let pontos = (wins * 3) + (ties * 1);
+  return pontos;
 }
 
 // Desafio 6
