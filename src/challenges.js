f02d3d254d3692f2ec93c3f003f7bcf408bf87dc
Rafael Toledo <rafael.toledo@engenharia.ufjf.br>
2021-08-03 20:01:07 -0300

    Exercício 9 refeito

--- src/challenges.js
+++ src/challenges.js
@@ -96,7 +96,8 @@ return array;
 }
 
 // Desafio 9
-function encode(string) {
+function encode(string) 
+{
   // seu código aqui
   let newString = "";
   for (let index = 0; index < string.length; index += 1)
@@ -137,21 +138,15 @@ function decode(string)
   {
     if (string[index] == 1)
     {
-      newString = newString + 'a';// Desafio 11
-      function generatePhoneNumber(numeroDeTelefone) {
-        // seu código aqui
-        let cont = 0;
-      if(numeroDeTelefone.length !== 11){
-        return "Array com tamanho incorreto."
-      }
-      for(let index = 0; index < numeroDeTelefone.length; index += 1){
-        if(numeroDeTelefone[index] < 0 | numeroDeTelefone[index] > 9){
-          return "não é possível gerar um número de telefone com esses valores"
-        }
-      }
-      return "(" + numeroDeTelefone[0] + numeroDeTelefone[1] + ") " + numeroDeTelefone[2] + numeroDeTelefone[3] + numeroDeTelefone[4] + numeroDeTelefone[5] + numeroDeTelefone[6] + "-"  + numeroDeTelefone[7] + numeroDeTelefone[8] + numeroDeTelefone[9] + numeroDeTelefone[10];
-      }
-      
+      newString = newString + 'a';
+    }
+    else if (string[index] == 2)
+    {
+      newString = newString + 'e';
+    }
+    else if (string[index] == 3)
+    {
+      newString = newString + 'i';
     }
     else if (string[index] == 4)
     {
@@ -169,10 +164,6 @@ function decode(string)
   return newString;
 }
 
-console.log(decode("h2ll4"));
-
-
- 
 module.exports = {
   calcArea,
   catAndMouse,
