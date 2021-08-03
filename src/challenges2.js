3c8aa8e5b07c166c62ab14b97440c7095fd853bf
leandro oliveira <leandrooliveira35@gmail.com>
2021-08-03 17:30:38 -0300

    mais alguns requisitos terminados

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,21 +1,49 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, name) {
+  let object = {};
+  let saida = [];
+
+  if (tech.length === 0) {
+    return 'Vazio!';
+  }
+  for (let index = 0; index < tech.length; index += 1) {
+    object.tech = tech[index];
+    object.name = name;
+    saida.push(object);
+  }
+  return saida;
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  if (array.length !== 11) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  for (let index = 0; index < 11; index += 1) {
+    if (array[index] > 0 || array[index]) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(a, b, c) {
+  if (((a > b + c) || (a < Math.abs(b - c))) || ((b > a + c) && (b < Math.abs(a - c))) || ((c > a + b) || (c < Math.abs(a - b)))) {
+    return false;
+  }
+  return true;
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(string) {
+  let total = 0;
+  let resultado = string.match(/\d+/g).map(Number);
+  for (let index = 0; index < resultado.length; index += 1) {
+    total += resultado[index];
+  }
+  if (total === 1) {
+    return `${total} copo de água`;
+  } return `${total} copos de água`;
 }
 
 module.exports = {
