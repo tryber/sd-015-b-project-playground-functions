b0019e0b1ee957942d8b5f5193741202000ac0a1
Walace <walacewab@gmail.com>
2021-08-03 14:40:25 -0300

    [Walace Borges] Iniciando projeto e Desafio 1 feito

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,17 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(valor1, valor2){
+  if (valor1 === false && valor2 === true){
+    return false;
+  }
+  else if (valor1 === true && valor2 === false){
+    return false;
+  }
+  else if (valor1 === false && valor2 === false){
+      return false;
+  }
+  else if (valor1 === true && valor2 === true){
+      return true
+  }
 }
 
 // Desafio 2
