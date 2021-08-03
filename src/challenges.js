03b1ccf18241611566116eb55aa526682bbab5a9
PedroAugmed <pedroaugmed7@gmail.com>
2021-08-03 18:50:24 -0300

    Passando o codigo pra pasta chalenge

--- src/challenges.js
+++ src/challenges.js
@@ -1,42 +1,110 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+
+function compareTrue(a ,b){
+
+    if (a && b === true){
+     return true
+    } else {
+        return false
+    }
+
 }
+compareTrue(false, false)
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height){
+    return base * height / 2
 }
+
+calcArea(51, 1)
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(a){
+let x;
+x = a.split(' ')
+
+    return x
 }
+
+splitSentence('go Trybe')
+
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
-}
 
-// Desafio 5
-function footballPoints() {
-  // seu código aqui
+function concatName(a){
+   let xd = a;
+
+   let xe = ''
+   
+   xe = xd[xd.length - 1] + ',' + ' ' + xd[0]
+    
+    return xe
+ 
 }
+concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'])
+
+
+// Desafio 5 
+function footballPoints(wins, ties){
+    let pointTotal = wins * 3 + ties
+    return pointTotal
+}
+footballPoints(0, 0)
+
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+
+function highestCount(a){
+
+let quantidade = 0;
+    for (let i = 0; i < a.length; i++){
+ if (Math.max(...a) === a[i]) {
+    quantidade++
+}   
 }
 
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
+return quantidade
+
 }
+
+highestCount([00, 00, 00, 0])
+
+
+// Desafio 7 
+ 
+
+function catAndMouse(mouse, cat1, cat2){
+    let DistanceCat1 = mouse - cat1
+    let DistanceCat2 = mouse - cat2
+    if (DistanceCat1 < DistanceCat2){
+        return "cat1"
+    } else if (DistanceCat1 > DistanceCat2){ 
+        return "cat2"
+    } else { 
+        return "os gatos trombam e o rato foge"
+}
+}
+catAndMouse(8, 2, 2);
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(a){
+    let newA = []
+
+
+    for (let i = 0; i < a.length; i++){
+        if (a[i] % 3 === 0 && a[i] % 5 !== 0){
+            newA.push("fizz")
+        } else if (a[i] % 5 === 0 && a[i] % 3 !== 0){
+            newA.push("buzz")
+        } else if (a[i] % 3 === 0 && a[i] % 5 === 0){
+            newA.push("fizzBuzz")
+        }else if (a[i] % 3 !== 0 && a[i] % 5 !== 0){
+            newA.push("bug!")
+        }
+    }
+    return newA
 }
+fizzBuzz([7, 9])
 
 // Desafio 9
 function encode() {
