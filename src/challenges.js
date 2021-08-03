0d08f663ff1d3779d620b7846e2c923e57eff69b
ArthurMoreira <arthurmcs96@gmail.com>
2021-08-03 16:46:42 -0300

    Resolve o desafio 2

--- src/challenges.js
+++ src/challenges.js
@@ -9,9 +9,11 @@ function compareTrue(value1, value2) {
 compareTrue(true, true)
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height / 2) 
+  
 }
+calcArea(51, 1)
 
 // Desafio 3
 function splitSentence() {
