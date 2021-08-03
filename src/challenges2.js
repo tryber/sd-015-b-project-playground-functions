6cfd608db6bc7d252674e08e415bf8d4601380d9
anniehau <anniehaurani@gmail.com>
2021-08-03 17:16:24 -0300

    Adiciona exercicio 11.

--- src/challenges2.js
+++ src/challenges2.js
@@ -21,8 +21,34 @@ function techList(tech,name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(array) {
   // seu código aqui
+  // Começar com uma variável que cria o DDD com espaço, depois usar um for para gerar o resto dos números e um if dentro do for. Além disso, vou precisar fazer alguns if's no começo e retornar um resultado já caso um if seja sucedido.
+  
+  if (array.length != 11) { return "Array com tamanho incorreto." }
+  for (i = 0; i < array.length; i += 1) {
+    let repeats = 1;
+    for (i2 = i+1; i2 < array.length; i2 += 1) {
+      if (array[i] === array[i2]) {
+        repeats += 1;
+      }
+    }
+    if (array[i] < 0 || array[i] > 9 || repeats >= 3) {
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+  }
+
+  let phoneNumber = '(' + array[0] + array[1] + ')' + ' ';
+
+  for (i = 2; i < array.length; i += 1) {
+    phoneNumber += array[i];
+    if (i === 6) {
+      phoneNumber += '-';
+    }
+  }
+
+  return phoneNumber;
+
 }
 
 // Desafio 12
@@ -42,4 +68,4 @@ module.exports = {
   triangleCheck,
 };
 
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],"Lucas"))
+console.log(generatePhoneNumber([0, 2, 3, 4, 4, 2, 7, 8, 9, 9, 4]))
