076825b53b9fbf41a4df4a825cc266a63db3359e
Danilo <dcalado.fisio@yahoo.com.br>
2021-08-03 15:01:14 -0300

    desafio 2 feito

--- src/challenges.js
+++ src/challenges.js
@@ -4,15 +4,11 @@ function compareTrue(a, b) {
       return true;
     else
       return false;
-  }
-  
- 
- 
-  
+  } 
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+    return (base * height) / 2
 }
 
 // Desafio 3
