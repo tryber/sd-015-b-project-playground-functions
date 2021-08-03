54e3b2df6919575f8cec2067a79a89c99402d410
alvesgf16 <alvesgabriel1994@gmail.com>
2021-08-03 16:59:43 -0300

    Adiciona desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,26 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techs, name) {
+  if (techs.length === 0) {
+    return "Vazio!"
+  } else {
+    techs.sort();
+
+    let techObjects = [];
+
+    for (index = 0; index < techs.length; index += 1) {
+      let techObject = {
+        "tech": techs[index],
+        "name": name
+      }
+
+      techObjects.push(techObject);
+    }
+
+    return techObjects;
+  }
 }
+
+console.log(techList([], "Lucas"));
 
 // Desafio 11
 function generatePhoneNumber() {
