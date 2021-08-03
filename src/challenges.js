b12c7268ef6f83aeec9ed0e6fa3f3b1f504612bd
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2021-08-03 20:11:37 -0300

    removido os console.log

--- src/challenges.js
+++ src/challenges.js
@@ -4,8 +4,6 @@ function compareTrue() {
   return isEven;
 // seu código aqui
 }
-                                    console.log(compareTrue(num1=5, num2=10));
-
 
 // Desafio 2
 function calcArea() {
@@ -13,23 +11,13 @@ function calcArea() {
   return area;  
   // seu código aqui
 }
-                                    console.log(calcArea(base=10, height=50));
-                                    console.log(calcArea(base=5, height=2));
-                                    console.log(calcArea(base=51, height=1));
-
-
 
 // Desafio 3
 function splitSentence(frase) {
-    let palavras = frase;
-    return palavras.split(' ');
-     // seu código aqui
+  let palavras = frase;
+  return palavras.split(' ');
+  // seu código aqui
   }
-                                    console.log(splitSentence("go Trybe"));
-                                    console.log(splitSentence("vamo que vamo"));
-                                    console.log(splitSentence("foguete"));
- 
-
 
 // Desafio 4
 function concatName(array) {
@@ -37,12 +25,6 @@ function concatName(array) {
   return last+first;
   // seu código aqui
 }
-                                    console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
-                                    console.log(concatName(['foguete', 'não', 'tem', 'ré']));
-                                    console.log(concatName(['captain', 'my', 'captain']));
-
-
-
 
 // Desafio 5
 function footballPoints(jogos) {
@@ -50,11 +32,6 @@ function footballPoints(jogos) {
   return pontos;
   // seu código aqui
 }
-                                    console.log(footballPoints(wins=14, ties=8));
-                                    console.log(footballPoints(wins=1, ties=2));
-                                    console.log(footballPoints(wins=0, ties=0));
-
-
 
 // Desafio 6
 function highestCount() {
