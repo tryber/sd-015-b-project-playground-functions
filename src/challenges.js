fb3870982e0abb0765095796670adbf894a16662
Rafael <rafael.gomesbarretomoura@gmail.com>
2021-08-03 15:21:47 -0300

    atualiza diretórios

--- src/challenges.js
+++ src/challenges.js
@@ -1,16 +1,26 @@
 // Desafio 1
 function compareTrue() {
-  // seu código aqui
+  function compareTrue(param1, param2) {
+    if (param1 && param2 % 2 === 0) {
+      return true
+    } else {
+      return false
+    }
+  }
 }
 
 // Desafio 2
 function calcArea() {
-  // seu código aqui
+  function calcArea(base, height) {
+    return (base * height) / 2
+  }
 }
 
 // Desafio 3
 function splitSentence() {
-  // seu código aqui
+  function splitSentence(string) {
+    return string.split(" ")
+  }
 }
 
 // Desafio 4
