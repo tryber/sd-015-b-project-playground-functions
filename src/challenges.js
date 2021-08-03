b2852d7498fc242329ac022efb3c4cb086080a13
Antonio Souza <antoniofelipe2001@hotmail.com>
2021-08-03 15:10:16 -0300

    feitas funções 5-8

--- src/challenges.js
+++ src/challenges.js
@@ -1,50 +1,81 @@
 // Desafio 1
 function compareTrue(boolean_1, boolean_2) {
   if((boolean_1 && boolean_2) === true) {
-    return true
+    return true;
   } else {
-    return false
+    return false;
   }
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  result = (base * height) / 2
-  return result
+  result = (base * height) / 2;
+  return result;
 }
 
 // Desafio 3
 function splitSentence(phrase) {
-  split_phrase = phrase.split(" ")
-  return split_phrase
+  split_phrase = phrase.split(" ");
+  return split_phrase;
 }
 
 // Desafio 4
 function concatName(name_array) {
-  return_string = name_array[name_array.length - 1].concat(", ", name_array[0])
-  return return_string
+  return_string = name_array[name_array.length - 1].concat(", ", name_array[0]);
+  return return_string;
 }
 
-console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']))
-
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  total_points = (wins * 3) + ties;
+  return total_points;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numbers_array) {
+  let highest_number = Number.NEGATIVE_INFINITY;
+  let counter = 0;
+  for(number of numbers_array) {
+    if(number > highest_number) {
+      highest_number = number;
+    }
+  }
+  for (number of numbers_array) {
+    if(number === highest_number) {
+      counter += 1;
+    }
+  }
+  return counter;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let d_cat1 = Math.abs(cat1 - mouse);
+  let d_cat2 = Math.abs(cat2 - mouse);
+  if (d_cat1 < d_cat2) {
+    return "cat1";
+  } else if (d_cat2 < d_cat1) {
+    return "cat2";
+  } else if (d_cat2 === d_cat1) {
+    return "os gatos trombam e o rato foge";
+  }
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numbers_array) {
+  let return_array = [];
+  for(number of numbers_array) {
+    if(((number % 3) === 0) && ((number % 5) === 0)) {
+      return_array.push("fizzBuzz");
+    } else if((number % 3) === 0) {
+      return_array.push("fizz");
+    } else if ((number % 5) === 0) {
+      return_array.push("buzz");
+    } else {
+      return_array.push("bug!");
+    }
+  }
+  return return_array;
 }
 
 // Desafio 9
