22ab8af8c9fad0ecc5b31cfb8f1c9d866ffded60
Flávia Labanca <flavialabanca@gmail.com>
2021-08-03 15:11:40 -0300

    Começa projeto Playground Functions

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,29 @@
 // Desafio 1
 function compareTrue() {
   // seu código aqui
+
+
+
+
+
+
+
+
+
+
+  // JavaScript possui um operador lógico &&, o qual recebe dois valores e retorna true se ambos os valores são verdadeiros, e retorna false se algum dos valores não o for.
+
+  // Considerando isso, implemente na função compareTrue, um código que ao receber dois parâmetros booleanos deve:
+  
+  // Retornar true se ambos os valores forem verdadeiros;
+  // Retornar false se um ou ambos os parâmetros forem falsos.
+  // Faça a função somente utilizando o operador &&.
+
+
+
+
+
+
 }
 
 // Desafio 2
