0466a9ae489bc6709cd76870cbaf0c5593366f62
PedroAugmed <pedroaugmed7@gmail.com>
2021-08-03 17:56:21 -0300

    Troc console.log para return

--- ProjetcPlaygroundFunctions.js
+++ ProjetcPlaygroundFunctions.js
@@ -3,9 +3,9 @@
 function compareTrue(a ,b){
 
     if (a && b === true){
-     console.log(true)
+     return(true)
     } else {
-        console.log(false)
+        return(false)
     }
 
 }
@@ -13,7 +13,7 @@ compareTrue(false, false)
 
 2 
 function calcArea(base, height){
-    console.log(base * height / 2)
+    return(base * height / 2)
 }
 
 calcArea(51, 1)
@@ -23,7 +23,7 @@ function splitSentence(a){
 let x;
 x = a.split(' ')
 
-    console.log(x)
+    return(x)
 }
 
 splitSentence('go Trybe')
@@ -37,7 +37,7 @@ function concatName(a){
    
    xe = xd[xd.length - 1] + ',' + ' ' + xd[0]
     
-    console.log(xe)
+    return(xe)
  
 }
 concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'])
@@ -45,7 +45,7 @@ concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'])
 5 
 function footballPoints(wins, ties){
     let pointTotal = wins * 3 + ties
-    console.log(pointTotal)
+    return(pointTotal)
 }
 footballPoints(0, 0)
 
@@ -60,7 +60,7 @@ let quantidade = 0;
 }   
 }
 
-console.log(quantidade)
+return(quantidade)
 
 }
 
@@ -72,11 +72,11 @@ function catAndMouse(mouse, cat1, cat2){
     let DistanceCat1 = mouse - cat1
     let DistanceCat2 = mouse - cat2
     if (DistanceCat1 < DistanceCat2){
-        console.log("cat1")
+        return("cat1")
     } else if (DistanceCat1 > DistanceCat2){ 
-        console.log("cat2")
+        return("cat2")
     } else { 
-        console.log("os gatos trombam e o rato foge")
+        return("os gatos trombam e o rato foge")
 }
 }
 catAndMouse(8, 2, 2);
@@ -87,10 +87,16 @@ function fizzBuzz(a){
 
 
     for (let i = 0; i < a.length; i++){
-        if (a[i] % 3 === 0){
-            newA.push(a[i])
+        if (a[i] % 3 === 0 && a[i] % 5 !== 0){
+            newA.push("fizz")
+        } else if (a[i] % 5 === 0 && a[i] % 3 !== 0){
+            newA.push("buzz")
+        } else if (a[i] % 3 === 0 && a[i] % 5 === 0){
+            newA.push("fizzBuzz")
+        }else if (a[i] % 3 !== 0 && a[i] % 5 !== 0){
+            newA.push("bug!")
         }
     }
-    console.log(newA)
+    return(newA)
 }
-fizzBuzz()
+fizzBuzz([7, 9])
