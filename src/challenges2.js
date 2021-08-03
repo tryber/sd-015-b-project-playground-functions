c295baf18edeb4dbe6c8644c87fcd6c4137c771f
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-03 20:19:54 -0300

    Finaliza desafio10.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,35 @@
 // Desafio 10
-function techList() {
+let listaTecnologias = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+let nome = 'Daniel';
+function techList(lista, nome) {
   // seu código aqui
+
+  // Coloca o tamanho do array em uma variável para comparação
+  let tamanhoArray = lista.length;
+  // console.log(tamanhoArray);
+
+  // Compara o array, Se Vazio, retorna a mensagem
+  if (tamanhoArray === 0)
+  {
+    // console.log('Vazio'); // [DEBUG]
+    return 'Vazio!';
+
+  } else {
+    // Variável auxiliar que ordena os elementos
+    let aux = lista.sort();
+    // Variável onde o resultado da ordenação do Array será armazenado, com o nome ao final
+    let result = [];
+    for (let index of aux)
+    {
+      // Dá um push do conteúdo do index para dentro da variável result
+      result.push( {tech: index, name: nome} );
+    }
+    // Função Retorna o resultado ordenado
+    return result;
+  }
 }
+
+techList(listaTecnologias, nome);
 
 // Desafio 11
 function generatePhoneNumber() {
