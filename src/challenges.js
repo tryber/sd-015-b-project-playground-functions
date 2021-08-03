bae5644a0bcf71cf0197b57b8a200930c1372674
Gabriel Peralta <prates_peralta@hotmail.com>
2021-08-03 19:21:24 -0300

    corrige a função 7

--- src/challenges.js
+++ src/challenges.js
@@ -46,12 +46,16 @@ function highestCount(numbers) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  if((mouse - cat1) > (mouse - cat2)) {
-    return "cat2"
-  } else if((mouse - cat1) < (mouse - cat2)) {
-    return "cat1"
+
+  let distanceCat1 = Math.abs(cat1 - mouse)
+  let distanceCat2 = Math.abs(cat2 - mouse)
+
+  if(distanceCat1 > distanceCat2) {
+    return "cat2";
+  } else if(distanceCat1 < distanceCat2) {
+    return "cat1";
   } else {
-    return "os gatos se trombam e o rato foge"
+    return "os gatos trombam e o rato foge"
   }
 }
 
