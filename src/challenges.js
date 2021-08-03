61bacaaa0f32babdeb233c6fa186c55aab53361d
samuel <samuelsoutosilva14@gmail.com>
2021-08-03 16:34:10 -0300

    Requisito 3 - concluido

--- src/challenges.js
+++ src/challenges.js
@@ -14,8 +14,10 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(a) {
+  let string = a;
+  let array = string.split(" ")
+  return array;
 }
 
 // Desafio 4
