422085a12c14b9c289aa53fa63f67ad8c2b22680
matheusg18 <matheusgabriel.18@gmail.com>
2021-08-03 17:16:01 -0300

    Primeiro requisito bonus feito

--- src/challenges2.js
+++ src/challenges2.js
@@ -21,9 +21,52 @@ function techList(techs, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function isValidNumber(numbers) {
+  numbers.sort((a, b) => a - b);
+  let actualNumber = numbers[0];
+  let repeat = 0;
+
+  for (const num of numbers) {
+    if (num < 0 || num > 9) {
+      return false;
+    }
+
+    if (num === actualNumber) {
+      repeat += 1;
+
+      if (repeat >= 3) {
+        return false;
+      }
+    } else {
+      actualNumber = num;
+      repeat = 1;
+    }
+  }
+
+  return true;
 }
+
+function generatePhoneNumber(numbers) {
+  // seu código aqui
+  if (numbers.length != 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  //https://www.samanthaming.com/tidbits/35-es6-way-to-clone-an-array/
+  if (isValidNumber([...numbers]) === false) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+
+  let phoneNumber = numbers.join('');
+  let ddd = phoneNumber.substr(0, 2);
+  let firstPart = phoneNumber.substr(0 + 2, 5);
+  let secondPart = phoneNumber.substr(0 + 2 + 5, 4);
+  phoneNumber = '(' + ddd + ')' + ' ' + firstPart + '-' + secondPart;
+
+  return phoneNumber;
+}
+
+console.log(generatePhoneNumber([5, 2, 8, 1, 5, 3, 7, 2, 8, 9, 0]));
 
 // Desafio 12
 function triangleCheck() {
