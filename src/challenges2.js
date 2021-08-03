b8a922b36060e583be50e9e85bdf57a220975701
Denis Jonathan <denisjonathan7@gmail.com>
2021-08-03 20:47:01 -0300

    Concluído: desafio 11 do challeneges 2

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,8 +20,51 @@ function techList(arr, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arr) {
+  let phoneNumber = ''
+  
+  if (arr.length !== 11) {
+    return 'Array com tamanho incorreto.'
+  }
+
+  for (let index = 0; index < arr.length; index += 1) {
+    if (arr[index] < 0 || arr[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores'
+    }
+  }
+
+  for (let index = 0; index < arr.length; index += 1) {
+    let count = 0
+    for (let comparate = 0; comparate < arr.length; comparate += 1) {
+      if (arr[index] === arr[comparate]) {
+        count += 1
+      }
+    }
+    if (count >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores'
+    }
+  }
+
+  for (let index = 0; index < arr.length; index += 1) {
+    switch(index) {
+      case 0:
+        phoneNumber += `(${arr[index]}`
+        break
+
+      case 1:
+        phoneNumber += `${arr[index]}) `
+        break
+
+      case 6:
+        phoneNumber += `${arr[index]}-`
+        break
+
+      default:
+        phoneNumber += arr[index]
+    }
+  }
+
+  return phoneNumber
 }
 
 // Desafio 12
