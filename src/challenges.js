29bc9d2d78e48997930a09f5de9ad0dedbdb087b
Lucas Maieski Marques <lucmaieski@gmail.com>
2021-08-03 18:23:48 -0300

    Realiza o quinto exercicio

--- src/challenges.js
+++ src/challenges.js
@@ -23,9 +23,11 @@ function concatName(nome) {
   }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let pontos = (3 * wins) + (1 * ties);
+  return (pontos)
 }
+console.log(footballPoints(3, 2));
 
 // Desafio 6
 function highestCount() {
