8e8c25d347882255e560b0d3e7a97444f54937be
Wander Luiz <wander.srv@gmail.com>
2021-08-03 17:57:08 -0300

    Resoluçao dos exercicios 1 a 7

--- src/challenges.js
+++ src/challenges.js
@@ -1,36 +1,80 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(valor1,valor2) {
   // seu código aqui
+  if (valor1 === true && valor2 === true){
+    return true;
+  } else{
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base,altura) {
   // seu código aqui
+  return (base * altura) / 2;
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(palavra) {
   // seu código aqui
+  let novoArray = (palavra.split(' ',palavra.length));
+  return novoArray;
 }
 
 // Desafio 4
-function concatName() {
+function concatName(novoArray) {
   // seu código aqui
+  let juntaString = novoArray[novoArray.length-1];
+  juntaString += (', ' + novoArray[0]);
+    return juntaString;
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  let pontos = 3;
+  let calculo = (wins * pontos) + ties;
+  return calculo;
 }
 
 // Desafio 6
-function highestCount() {
+function highestCount(novoArray) {
   // seu código aqui
+  let numMaior= Math.max.apply(null,novoArray);
+  let contador = 0;
+  for (let index = 0; index < novoArray.length; index += 1){
+    if (numMaior === novoArray[index]){
+      contador += 1;
+    }
+  }
+  return contador;
 }
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse,cat1,cat2) {
   // seu código aqui
+  let calcCat1 = 0;
+  let calcCat2 = 0;
+
+  if (cat1 < mouse){
+    calcCat1 = mouse - cat1;
+  } else{
+    calcCat1 = cat1 - mouse;
+  }
+  
+  if (cat2 < mouse){
+    calcCat2 = mouse - cat2;
+  } else{
+    calcCat2 = cat2 - mouse;
+  }
+
+  if (calcCat1 < calcCat2){
+    return 'cat1';
+  } else if (calcCat1 > calcCat2){
+    return 'cat2';
+  } else {
+    return 'os gatos trombam e o rato foge';
+  }
 }
 
 // Desafio 8
@@ -58,3 +102,5 @@ module.exports = {
   highestCount,
   splitSentence,
 };
+
+console.log(catAndMouse(7,10,9))
