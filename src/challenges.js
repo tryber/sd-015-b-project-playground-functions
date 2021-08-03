3e113fdd450cc7104f0324216fa15aad510931d6
assisMeneghetti <assismfabri@hotmail.com>
2021-08-03 16:29:13 -0300

    adiciona requisito 3

--- src/challenges.js
+++ src/challenges.js
@@ -1,30 +1,26 @@
 // Desafio 1
 function compareTrue(a, b) {
-  let compare = true;
+
   if (a && b === true) {
-    compare = true;
-  } else {
-    compare = false
+    return true;
   }
+  return false;
 }
-compareTrue(true, true)
 
 // Desafio 2
 function calcArea(base, altura) {
-  let area = 0;
-  area = (base * altura) / 2;
-  console.log(area);
+  return (base * altura) / 2;
 }
-calcArea(10, 50);
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(sentense) {
+  return sentense.split(" ");
 }
 
 // Desafio 4
 function concatName() {
-  // seu código aqui
+  let name;
+  return name[0] + ", "name[length - 1];
 }
 
 // Desafio 5
