39e936e63572c6768d081c22bf5cc9c1510f6ebf
iuryroque <iury.roque@hotmail.com>
2021-08-03 17:40:59 -0300

    Desafio 8 concluido

--- src/challenges.js
+++ src/challenges.js
@@ -52,16 +52,81 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(palavras) {
+  let vetorPalavras = [];
+  for (let index = 0; index < palavras.length; index++) {
+    if (palavras[index] % 3 === 0 && palavras[index] % 5 === 0){
+      vetorPalavras.push("fizzBuzz")   
+    }else if (palavras[index] % 3 === 0) {
+      vetorPalavras.push("fizz");
+    }else if (palavras[index] % 5 === 0){
+      vetorPalavras.push("buzz")
+    }else{
+      vetorPalavras.push("bug!")
+    }
+  }return vetorPalavras
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(frase) {
+  let fraseCodificada = [];
+  for (let index = 0; index < frase.length; index++) {
+    switch (frase[index]) {
+        case "a":
+            fraseCodificada[index] = "1";
+            break;
+
+        case "e":
+            fraseCodificada[index] = "2";
+            break;
+        
+        case "i":
+            fraseCodificada[index] = "3";
+            break;
+
+        case "o":
+            fraseCodificada[index] = "4";
+            break;
+
+        case "u":
+            fraseCodificada[index] = "5";
+            break;
+
+        default:
+          fraseCodificada[index] = frase [index]
+            break;
+    }
+  }return fraseCodificada.join("")
 }
-function decode() {
-  // seu código aqui
+function decode(frase) {
+let fraseCodificada = [];
+for (let index = 0; index < frase.length; index +=1) {
+    switch (frase[index]) {
+        case "1":
+            fraseCodificada[index] = "a";
+            break;
+
+        case "2":
+            fraseCodificada[index] = "e";
+            break;
+        
+        case "3":
+            fraseCodificada[index] = "i";
+            break;
+
+        case "4":
+            fraseCodificada[index] = "o";
+            break;
+
+        case "5":
+            fraseCodificada[index] = "u";
+            break;
+
+        default:
+          fraseCodificada[index] = frase [index]
+            break;
+    }
+  }return fraseCodificada.join("")
 }
 
 module.exports = {
