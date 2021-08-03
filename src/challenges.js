27cec75109d9bdc58f4fb4b1c05bd261836fe97d
Nicolas Franzolin <nicolas.franzolin@gmail.com>
2021-08-03 15:06:27 -0300

    Adiciona Requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -7,8 +7,9 @@ function compareTrue(argument1, argument2) {
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  return (base * height) / 2;
 }
 
 // Desafio 3
