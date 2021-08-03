f8247b9704c3790e5123b64dabd216678a1b4568
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2021-08-03 18:10:22 -0300

    testa o desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -39,13 +39,11 @@ function highestCount(arrayNumeros) {
   console.log(highestCount([9, 1, 2, 3, 9, 5, 7])) 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2){
-  if(cat1 < cat2){
-    return 'cat1';
-  }else if(cat2 < cat1){
-    return 'cat2'
-  }else if(cat1 === cat2){
-    'os gatos trombam e o rato foge'
-  }
+if((cat1 - mouse) > (cat2 - mouse)){
+  return 'cat2'
+}else if((cat2 - mouse) > (cat1 - mouse)){
+  return 'cat1'
+}else return 'os gatos trombam e o rato foge'
 }
 
 // Desafio 8
