f40ce01f41314aac7e9d18bd461f0261ef090d67
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-03 14:46:52 -0300

    Add challenge 01

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,17 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(a, b) {
+  if (a === true && b == true) {
+    return true;
+  }
+  else if (a === false && b === false) {
+    return false;
+  }
+  else if (a === false && b === true) {
+    return false;
+  }
+  else if (a === true && b === false){
+    return false;
+  }
 }
 
 // Desafio 2
