a0c7cc3aa31942dd4d913e614040178e3bfef7d8
Robert Martins <robert.rm131@gmail.com>
2021-08-03 16:50:07 -0300

    ajustar errata desafio 6

--- src/challenges.js
+++ src/challenges.js
@@ -6,27 +6,15 @@ function compareTrue(a, b) {
   return false;
 }
 
-compareTrue(false, true);
-compareTrue(false, false);
-compareTrue(true, true);
-
 // Desafio 2
 function calcArea(base, height) {
   return (base* height)/2;
 }
 
-calcArea(10, 50);
-calcArea(5, 2);
-calcArea(51, 1);
-
 // Desafio 3
 function splitSentence(s) {
   return s.split(" ");
 }
-
-splitSentence("go trybe");
-splitSentence("vamo que vamo");
-splitSentence("foguete");
 
 // Desafio 4
 function concatName(nome) {
@@ -34,40 +22,47 @@ function concatName(nome) {
   return `${nome[tamanho-1]}, ${nome[0]}`;
 }
 
-concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']);
-concatName(['foguete', 'não', 'tem', 'ré']);
-concatName(['captain', 'my', 'captain']);
-
 // Desafio 5
 function footballPoints(wins, ties) {
   return ((wins * 3)+(ties * 1));
 }
 
-footballPoints(14, 8);
-footballPoints(1, 2);
-footballPoints(0, 0);
-
 // Desafio 6
 function highestCount(v) {
   let maior = 0;
   let cont = 0;
+  let menorQueZero = false;
   for (let i of v) {
-    if(i > maior) {
-      maior = i
-    }
+    if(i < 0) {
+      menorQueZero = true;
+      if (maior === 0) {
+        maior = i*-1;
+      } else {
+        if (i*-1 < maior) {
+          maior = i*-1;
+        } 
+      }
+    } else {
+        if(i > maior) {
+          maior = i;
+        }
+      }
   }
   for (let j of v) {
+    console.log(maior, j*-1)
+    if (menorQueZero === true) {
+      if (j*-1 === maior) {
+        cont += 1;
+      }
+    } else {
     if (j === maior) {
-      cont += 1;
+        cont += 1;
+      }
     }
   }
-  return cont;
+  return cont;  
 }
-
-highestCount([9, 1, 2, 3, 9, 5, 7]);
-highestCount([0, 4, 4, 4, 9, 2, 1]);
-highestCount([0, 0, 0]);
-
+ 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   if( mouse - cat1 < mouse - cat2) {
