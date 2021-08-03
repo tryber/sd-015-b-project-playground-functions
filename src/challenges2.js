4b1bf02553af6b0bfee5f080a28684f793babaf6
Danilo <dcalado.fisio@yahoo.com.br>
2021-08-03 17:48:05 -0300

    desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,26 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
+  function techList(technologys, name) {
+    let objetos = [];
+    let objeto = {
+        tech: "NomeTech",
+        name: "nome",
+    }
+    let technologysEmOrdem = technologys.sort();
+    for (let i in technologysEmOrdem) {
+        objeto = {
+            tech: technologysEmOrdem[i],
+            name: name,
+        }
+        objetos.push(objeto);
+        objeto = 0;
+    }
+    if (technologys.length === 0){
+        return 'Vazio!';
+    } else {
+        return objetos;
+    }
+  }
+
 
 // Desafio 11
 function generatePhoneNumber() {
