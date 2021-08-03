dd65501aff4e1df93a83ae7f7940cb7a356e400d
Richard Leal <rwsleal@gmail.com>
2021-08-03 16:13:04 -0300

    Primeiro e segundo requisito finalizados

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,15 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(a, b) {
+  if (a === true && b === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height)/2
 }
 
 // Desafio 3
@@ -58,3 +62,5 @@ module.exports = {
   highestCount,
   splitSentence,
 };
+
+console.log(compareTrue(1,0))
