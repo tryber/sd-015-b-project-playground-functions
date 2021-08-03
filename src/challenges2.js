2d1dab62c812ff9ead8caddba5459b368a26cd8e
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-08-03 17:59:08 -0300

    Finaliza desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,21 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(wantToLearn, name) {
+  let list = []
+  wantToLearn.sort()
+  for (let index = 0; index < wantToLearn.length; index += 1) {
+    let object = {}
+    object["tech"] = (wantToLearn[index])
+    object["name"] = (name)
+    list.push (object)
+  } 
+  if (wantToLearn.length > 0) {
+    return list;
+  } else {
+    return "Vazio!"
+  }
 }
+  
+ console.log (techList([], "Lucas"))
 
 // Desafio 11
 function generatePhoneNumber() {
