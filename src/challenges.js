30b59b9093d2bdcfdfa138004d85c8e57c1d25ef
1lucascq <1lucascq@gmail.com>
2021-08-03 18:27:30 -0300

    Requisitos 1 a 6

--- src/challenges.js
+++ src/challenges.js
@@ -1,31 +1,49 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui.
+function compareTrue(a, b) {
+  if (a && b === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(a) {
+  let afterSplit = a.split(' ');
+  return afterSplit;
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  let inverted = array.reverse();
+  return `${inverted[0]}, ${inverted[inverted.length - 1]}`;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let pontos = (wins * 3) + ties;
+  return pontos;
+  
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(a) {
+  let count = 0;
+  function bigger (b, c) {
+    return b - c;
+  }
+  let sortedArray = a.sort(bigger);
+  let highest = a[sortedArray.length - 1];
+  for (let i = 0; i < sortedArray.length; i += 1) {
+    if (sortedArray[i] === highest) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 // Desafio 7
