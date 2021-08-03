f7eac0a2ddf84142bc738af136b4f543c57b874a
David Souza <dabcs@poli.ufrj.br>
2021-08-03 18:11:52 -0300

    exercicio cel number

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,8 +14,61 @@ function techList(arrTech, name) {
   }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+
+function checkNumber(number){
+  let contRepeat = 0;
+  for (let i in number){
+      if (number[i] < 0) {
+          return 0;
+          break;
+      }
+  }
+  for (let i in number){
+      if (number[i] > 9) {
+          return 1;
+          break;
+      }
+  }
+  number = number.sort();
+  let nRepeat = 0;
+  let nRepeatQ = 0;
+  for (let i = 1; i < number.length; i += 1){
+      if (number[i] === number[i-1]){
+          contRepeat += 1;
+          if (contRepeat > 2){
+              nrepeat = number[i];
+              for (let j in number){
+                  if (nrepeat == number[j]){
+                      nRepeatQ += 1;
+                  }
+              }
+              if (nRepeatQ >= 3) {
+                  return 2;
+                  break;
+              }
+          }
+      }
+  }
+  return 3;
+}
+
+function generatePhoneNumber(telNumber) {
+  if (telNumber.length === 11 && checkNumber(telNumber) == 3) {
+      let numberFormated;
+      numberFormated = telNumber.join('');
+      numberFormated = numberFormated.replace(/(\d{0})(\d{2})(\d{5})(\d{4})/,"$1($2) $3-$4");
+      return numberFormated;
+
+      //Uso do replace tirado do blog da Trybe https://blog.betrybe.com/javascript/javascript-replace/
+  } else if (telNumber.length !== 11){
+      return "Array com tamanho incorreto.";
+  }else if(checkNumber(telNumber) === 0) {
+      return "não é possível gerar um número de telefone com esses valores";
+  } else if (checkNumber(telNumber) === 1) {
+      return "não é possível gerar um número de telefone com esses valores";
+  } else if (checkNumber(telNumber) === 2){
+      return "não é possível gerar um número de telefone com esses valores";
+  } 
 }
 
 // Desafio 12
