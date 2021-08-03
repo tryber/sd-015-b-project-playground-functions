0dc8905d433207ac152a91767052949fde42a49f
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2021-08-03 15:16:24 -0300

    Adiciona exercicios 4 e 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
 function compareTrue(a, b) {
-  if (true === a && true === b) {
+  if (a === true && b === true) {
     return true;
   } else {
     return false;
@@ -18,15 +18,16 @@ function splitSentence(string) {
   return array;
 }
 
-
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  let string = array.pop() + ", " + array.shift();
+  return string;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let totalPoints = (wins * 3) + (ties * 1);
+  return totalPoints; 
 }
 
 // Desafio 6
