7f6314cd5062790f782d9986e3527ae924d59cd7
Miguel_Reis <guelsouzar@gmail.com>
2021-08-03 20:02:46 -0300

    fiz os requisitos 1-2-3-4-5

--- src/challenges.js
+++ src/challenges.js
@@ -1,26 +1,54 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-}
+function compareTrue(valor1, valor2) {
+  
+    if (valor1 && valor2 === true){
+        let comparar = true
+    
+        console.log(comparar)
+        
+        return comparar
+    } else{
+        
+        let comparar = false 
+    
+        console.log(comparar)
+    
+        return comparar
+    }
+    }
+     compareTrue(true, false)
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+    let calcular = (base * height) / 2
+    return calcular
+    }
+    
+// Desafio 3
+function splitSentence(string) 
+{
+  let separacao =  string.split(' ')
+  return separacao
+  
 }
 
-// Desafio 3
-function splitSentence() {
-  // seu código aqui
-}
+
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(ArrayDeStrings) {  
+let primeiro = ArrayDeStrings[0];
+let ultimo = ArrayDeStrings[ArrayDeStrings.length-1];
+let results = ultimo+', '+primeiro;
+return results ;
 }
 
+
+
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins , ties) {
+  let vitorias = wins * 3;
+  resultado = vitorias + ties
+  return resultado
 }
 
 // Desafio 6
