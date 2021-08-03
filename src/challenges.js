795469f19fc3ecdfb30849512e9f9e4325012da2
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-08-03 14:47:12 -0300

    implementa a funcao que calcula a area do triangulo

--- src/challenges.js
+++ src/challenges.js
@@ -7,8 +7,10 @@ function compareTrue(value1, value2) {
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  let area = (base * height) / 2;
+  return area;
 }
 
 // Desafio 3
