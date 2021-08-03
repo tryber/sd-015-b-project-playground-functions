e4418a78457a4001d6045fe4d663131aec514251
Caio <caiomgp22@gmail.com>
2021-08-03 15:38:52 -0300

    Desafio5

--- src/challenges.js
+++ src/challenges.js
@@ -21,14 +21,22 @@ function splitSentence(string) {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
-}
+function concatName(array) {
+  let ultimaString = array.pop();
+  let primeiraString = array.shift();
+  let resultado = ultimaString + primeiraString;
+
+  return resultado;
+}console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']))
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
-}
+function footballPoints(wins, ties) {
+  let winsPoints = wins * 3;
+  let tiesPoints = ties;
+  let sumsPoints = winsPoints + tiesPoints;
+
+  return sumsPoints;
+}console.log(footballPoints(0, 0));
 
 // Desafio 6
 function highestCount() {
