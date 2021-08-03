e28d39d8728d4c8cb9009041fcca2a6e44a41d35
FERNANDO <Fernando@FERNANDO.local>
2021-08-03 16:09:39 -0300

    Primeiro exercicio

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,14 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  
+function compareTrue(valor1, valor2) {
+  if (valor1 === true && valor2 === true){
+    return true;
+  } else {
+    return false
+  } 
 }
+
+// seu código aqui
+
 
 // Desafio 2
 function calcArea() {
