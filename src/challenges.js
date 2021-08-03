9e9afebcf162453acca7bada8fe416bb3307b6fb
Ulisses Roque <ulissesroque@protonmail.com>
2021-08-03 19:50:11 -0300

    Adiciona os exercicios 1-5, 7 e 8

--- src/challenges.js
+++ src/challenges.js
@@ -1,46 +1,64 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(bool1, bool2) {
+  if (bool1 === true && bool2 === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(sentence) {
+  return sentence.split(' ');
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(arr) {
+  return `${arr[arr.length - 1]}, ${arr[0]}`;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return 3 * wins + ties;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+function highestCount() {}
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
+    return 'cat1';
+  } else if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
+    return 'cat2';
+  } else {
+    return 'os gatos trombam e o rato foge';
+  }
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(arr) {
+  let fizzBuzzArray = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (arr[index] % 3 === 0 && arr[index] % 5 === 0) {
+      fizzBuzzArray.push('fizzBuzz');
+    } else if (arr[index] % 5 === 0) {
+      fizzBuzzArray.push('buzz');
+    } else if (arr[index] % 3 === 0) {
+      fizzBuzzArray.push('fizz');
+    } else {
+      fizzBuzzArray.push('bug!');
+    }
+  } return fizzBuzzArray;
 }
 
 // Desafio 9
 function encode() {
-  // seu código aqui
 }
 function decode() {
   // seu código aqui
