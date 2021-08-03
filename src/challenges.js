8c8a4af6daf739d7363260a6305042328a224cd2
davi <davisoarescoura@gmail.com>
2021-08-03 21:15:49 +0200

    exercicios 2,3,6,7

--- src/challenges.js
+++ src/challenges.js
@@ -1,23 +1,25 @@
-// Desafio 1/
-function compareTrue(){ 
-  console.log('A'); return false; }
-function B(){ 
-  console.log('B'); return true; }
-  console.log( compareTrue() && B() );
-
-
-  // seu código aqui
-
+// Desafio 1
+function compareTrue(a, b) {
+  if (a && b === true) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
+console.log(calcArea(10, 50));
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+
+function splitSentence(string) {
+  let resultado = string.split(' ');
+  return resultado;
 }
+console.log(splitSentence('go Trybe'));
 
 // Desafio 4
 function concatName() {
@@ -30,14 +32,29 @@ function footballPoints() {
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numeros) {
+  let a = Math.max.apply(null, numeros);
+  let contador = 0;
+  for (let i = 0; i <= numeros.length; i++) {
+    if (a == numeros[i]) {
+      contador++;
+    }
+  }
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distanciaCat1 = Math.abs(cat1 - mouse);
+  let distanciaCat2 = Math.abs(cat2 - mouse);
+  if (distanciaCat1 < distanciaCat2) {
+    return 'cat1';
+  } else if (distanciaCat2 < distanciaCat1) {
+    return 'cat2';
+  } else {
+    return 'os gatos trombam e o rato foge';
+  }
 }
+console.log(catAndMouse(1, 0, 2));
 
 // Desafio 8
 function fizzBuzz() {
