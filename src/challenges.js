f721e7b2f0a58a36bad28f45a9c89d3bdd8daf35
Allan Melo <allan-m-silva@hotmail.com>
2021-08-03 15:22:17 -0300

    adiciona funcao splitSentence
    git push

--- src/challenges.js
+++ src/challenges.js
@@ -11,8 +11,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(stringSentence) {
+let arraySentence = stringSentence.split(' ')
+  return arraySentence
 }
 
 // Desafio 4
