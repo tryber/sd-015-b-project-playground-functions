0c9902e819e4aa27d721d6fc5a0cd29bab0e9b1a
Murilo <murilocarrijoadm@gmail.com>
2021-08-03 16:59:17 -0300

    refatora exercicio 2 e 3

--- arquivo-teste.js
+++ arquivo-teste.js
@@ -1,11 +1,22 @@
 // 1 - Crie uma função usando o operador &&
 
+function compareTrue(a,b){ 
+    if (a === 'false' && b === 'true') {
+        console.log('true');
+    } else{
+        console.log('false');
+    }
+}
+
+compareTrue('false');
+compareTrue(1, 11);
 
 
 //2 - Crie uma função que calcule a área de um triângulo
 
 function calcArea(base, height) {
-    console.log((base * height) / 2);
+    let soma = ((base * height) / 2);
+    return soma;
 };
 
 calcArea(10, 50);
@@ -15,10 +26,27 @@ calcArea(51, 1);
 //3 - Crie uma função que divida a frase
 
 function splitSentence(text) {
-    let  concat = text.split(' ');
-    console.log(concat);
+    let  separa = text.split(' ');
+    return separa;
 };
 
 splitSentence('go trybe');
 splitSentence('vamo que vamo');
 splitSentence('foguete');
+
+//4 - Crie uma função que use concatenação de strings
+
+let list = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
+let first = 0;
+let last = 0;
+
+for (let index = 0; index < list.length; index += 1){
+    if (list.length > 0){
+        last = list.length[index];
+    } else {
+        first = list.length[0];
+    }
+};
+
+console.log(last + ', ' + first);
+
