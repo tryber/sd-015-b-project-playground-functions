da073e9a41093f7a36e0729c738ec4f86821784f
Flávia Labanca <flavialabanca@gmail.com>
2021-08-03 15:59:00 -0300

    requisito 3

--- src/challenges.js
+++ src/challenges.js
@@ -20,10 +20,17 @@ console.log(calcArea(10,50));
 console.log(calcArea(5,2));
 console.log(calcArea(51,1));
 
+
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+  let fraseSeparada = '';
+  fraseSeparada = frase.split(" ");
+  return fraseSeparada;
 }
+console.log(splitSentence('go Trybe'));
+console.log(splitSentence('vamo que vamo'));
+console.log(splitSentence('foguete'));
+
 
 // Desafio 4
 function concatName() {
