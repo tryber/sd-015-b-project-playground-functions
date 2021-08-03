3347a8435eb375ea70479636dea52a0069fb7b44
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-03 15:28:33 -0300

    Conclui requisito02.

--- src/challenges.js
+++ src/challenges.js
@@ -14,8 +14,11 @@ function compareTrue(param1, param2) {
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  area = (base * height)/2;
+
+  return area;
 }
 
 // Desafio 3
