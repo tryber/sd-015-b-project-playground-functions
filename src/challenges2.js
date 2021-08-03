762618ab4dcecd29db2bfffd8095346df8ee55dc
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-08-03 19:02:01 -0300

    Requisito 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -42,11 +42,36 @@ function triangleCheck(lineA, lineB, lineC) {
   return eTriangulo;
 }
 
-console.log(triangleCheck(10, 14, 8));
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+// Sub-funções do desafio 13
+function mensagem(coposDeAgua) {
+  let msg;
+  if (coposDeAgua === 0) {
+    msg = 'Nenhum copo de água';
+  } else if (coposDeAgua === 1) {
+    msg = '1 copo de água';
+  } else {
+    msg = `${coposDeAgua} copos de água`;
+  }
+  return msg;
 }
+function hydrate(drinks) {
+  // Para se extrair o número de uma string,
+  // foi utilizada técnica observada em https://www.codegrepper.com/code-examples/javascript/find+integer+in+string+javascript
+  let coposDeBebida = drinks.match(/\d+/g);
+  let copoDeAgua = [];
+  let coposDeAgua = 0;
+
+  if (coposDeBebida !== []) {
+    for (let i = 0; i < coposDeBebida.length; i += 1) {
+      copoDeAgua[i] = parseInt(coposDeBebida[i], 10);
+      coposDeAgua += copoDeAgua[i];
+    }
+  }
+  return mensagem(coposDeAgua);
+}
+
+console.log(hydrate('100 cerveja, porfavor'));
 
 module.exports = {
   generatePhoneNumber,
