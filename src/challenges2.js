05057ba81838977cd5285aba94d021ff0d4f70c2
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-08-03 15:45:26 -0300

    10. techList

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,20 @@
 // Desafio 10
-function techList() {
+function techList(names, name) {
   // seu código aqui
+  if (names.length > 0) {
+    let lista = [];
+    names = names.sort();
+    for (tecnologia of names) {
+      lista.push({ tech: tecnologia, name: name });
+    }
+    return lista;
+  }
+  else{
+    return "Vazio!";
+  }
 }
 
+techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas');
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
