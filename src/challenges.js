8e86ae041ddfe76bab393d4c0a020c5829b71c2b
Gabriel Borges <igblborges@gmail.com>
2021-08-03 16:02:28 -0300

    adiciono exercicio-4 challenges.js

--- src/challenges.js
+++ src/challenges.js
@@ -1,4 +1,4 @@
-// Desafio 1
+
 
 function compareTrue(value1,value2) {
   if (value1 && value2 === true){
@@ -8,30 +8,28 @@ function compareTrue(value1,value2) {
   }
 }
 
-// Desafio 2
+
 function calcArea(base,height) {
     area = (base * height) / 2;
   return("A area do seu triangulo é : ", area);
 }
 
 
-// Desafio 3
+
 function splitSentence(strin) {
   let arraystr = strin.split(" ");  
   return(arraystr);
 }
 
 
-// Desafio 4
-//ao por um array ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'],
-// minha função retorna arraystring[length - 1] && arraystring[0]
+
 function concatName(arraystring) {
 
   let storelaf = [arraystring[arraystring.length - 1],arraystring[0]];
-  storelaf.join()
-   return(storelaf.join());
+  let storelafr = storelaf.join()
+   return(storelafr);
   }
-
+console.log(concatName(["joao","maria","cleiton","claudia","josé"]));
   
 
 // Desafio 5
