199742bc6f30244f11a6719cef39c12e3c1be6fc
Gabriel Peralta <prates_peralta@hotmail.com>
2021-08-03 15:18:42 -0300

    adiciona a segunda função

--- src/challenges.js
+++ src/challenges.js
@@ -8,8 +8,9 @@ function compareTrue(param1, param2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, altura) {
+  let area = (base * altura)/2;
+  return area
 }
 
 // Desafio 3
