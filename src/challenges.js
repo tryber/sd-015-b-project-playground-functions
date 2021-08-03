34b6f57c169cb9764138469d9ecb6b4edfb741ae
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-08-03 19:26:00 -0300

    Requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -149,15 +149,31 @@ function fizzBuzz(numeros) {
 function encode(frase) {
   let fraseEncode = frase;
 
-  console.log(fraseEncode.split);
+  // Utiliza o método replace, conforme descrito em https://www.w3schools.com/js/js_string_methods.asp
+
+  fraseEncode = fraseEncode.replace(/a/g, '1');
+  fraseEncode = fraseEncode.replace(/e/g, '2');
+  fraseEncode = fraseEncode.replace(/i/g, '3');
+  fraseEncode = fraseEncode.replace(/o/g, '4');
+  fraseEncode = fraseEncode.replace(/u/g, '5');
 
   return fraseEncode;
 }
 
-console.log(('gato'.indexOf('a')));
+console.log((encode('gato')));
 
-function decode() {
-  // seu código aqui
+function decode(frase) {
+  let fraseDecode = frase;
+
+  // Utiliza o método replace, conforme descrito em https://www.w3schools.com/js/js_string_methods.asp
+
+  fraseDecode = fraseDecode.replace(/1/g, 'a');
+  fraseDecode = fraseDecode.replace(/2/g, 'e');
+  fraseDecode = fraseDecode.replace(/3/g, 'i');
+  fraseDecode = fraseDecode.replace(/4/g, 'o');
+  fraseDecode = fraseDecode.replace(/5/g, 'u');
+
+  return fraseDecode;
 }
 
 module.exports = {
