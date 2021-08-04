fbd15f4269a42afa9eb237ab6ed107917ea23e76
Joao Marco <joaomarco.matta@gmail.com>
2021-08-03 23:35:57 -0300

    Inicia implementaçao requisito 10.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,19 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arrOfTechs, name) {
+  let list = [];
+  let objectList = {
+    tech: arrOfTechs,
+    name: name,
+  }
+  let newList = arrOfTechs.sort();
+  for (let i = 0; i < newList.length; i += 1) {
+    objectList.tech = newList[i];
+    list.push(objectList);
+  }
+
+  return newlist;
 }
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
 
 // Desafio 11
 function generatePhoneNumber() {
