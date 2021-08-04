79bba82027f0b5ddd25b947a503f8507ccea05af
alvesgf16 <alvesgabriel1994@gmail.com>
2021-08-04 15:42:17 -0300

    Refatora desafios 10 a 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,48 +1,34 @@
 // Desafio 10
 function techList(techs, name) {
   if (techs.length === 0) {
-    return "Vazio!"
-  } else {
-    techs.sort();
-
-    let techObjects = [];
-
-    for (index = 0; index < techs.length; index += 1) {
-      let techObject = {
-        "tech": techs[index],
-        "name": name
-      }
-
-      techObjects.push(techObject);
-    }
-
-    return techObjects;
+    return 'Vazio!';
   }
+
+  techs.sort();
+
+  let techObjects = [];
+
+  for (let index = 0; index < techs.length; index += 1) {
+    let techObject = {
+      tech: techs[index],
+      name,
+    };
+
+    techObjects.push(techObject);
+  }
+
+  return techObjects;
 }
 
 // Desafio 11
-function generatePhoneNumber(numbers) {
-  if (checkArrayContents(numbers) && checkArrayLength(numbers) && checkRepeatedDigits(numbers)) {
-    return "(" + numbers[0] + numbers[1] + ") " + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + "-" + numbers[7] + numbers[8] + numbers[9] + numbers[10]; 
-  }
-  
-  if (checkArrayLength(numbers) === false) {
-    return "Array com tamanho incorreto."
-  }
-  
-  if (checkArrayContents(numbers) === false || checkRepeatedDigits(numbers) === false) {
-    return "não é possível gerar um número de telefone com esses valores";
-  }
-}
-
 function checkArrayLength(array) {
-  return (array.length === 11);
+  return (array.length !== 11);
 }
 
 function checkArrayContents(array) {
   let isAValidDigit = true;
 
-  for (index = 0; index < array.length; index += 1) {
+  for (let index = 0; index < array.length; index += 1) {
     if (array[index] < 0 || array[index] > 9) {
       isAValidDigit = false;
     }
@@ -50,11 +36,10 @@ function checkArrayContents(array) {
   return isAValidDigit;
 }
 
-function checkRepeatedDigits(array) {
-  let isAValidNumber = true;
-  let numberCount = {}
+function countRepeatedDigits(array) {
+  let numberCount = {};
 
-  for (index = 0; index < array.length; index += 1) {
+  for (let index = 0; index < array.length; index += 1) {
     if (numberCount[array[index]]) {
       numberCount[array[index]] += 1;
     } else {
@@ -62,9 +47,14 @@ function checkRepeatedDigits(array) {
     }
   }
 
-  let countingArray = Object.values(numberCount);
+  return numberCount;
+}
 
-  for (index = 0; index < countingArray.length; index += 1) {
+function checkRepeatedDigits(array) {
+  let isAValidNumber = true;
+  let countingArray = Object.values(countRepeatedDigits(array));
+
+  for (let index = 0; index < countingArray.length; index += 1) {
     if (countingArray[index] >= 3) {
       isAValidNumber = false;
     }
@@ -72,29 +62,40 @@ function checkRepeatedDigits(array) {
 
   return isAValidNumber;
 }
+function generatePhoneNumber(numbers) {
+  if (checkArrayLength(numbers)) {
+    return 'Array com tamanho incorreto.';
+  }
 
-// Desafio 12
-function triangleCheck(lineA, lineB, lineC) {
-  return lineCheck(lineA, lineB, lineC) && lineCheck(lineB, lineA, lineC) && lineCheck(lineC, lineA, lineB);
+  if (checkArrayContents(numbers) === false || checkRepeatedDigits(numbers) === false) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+
+  return numbers.join('').replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
 }
 
+// Desafio 12
 function lineCheck(line1, line2, line3) {
   return line1 < line2 + line3 && line1 > Math.abs(line2 - line3);
+}
+
+function triangleCheck(lineA, lineB, lineC) {
+  return lineCheck(lineA, lineB, lineC)
+  && lineCheck(lineB, lineA, lineC)
+  && lineCheck(lineC, lineA, lineB);
 }
 
 // Desafio 13
 function hydrate(string) {
   let drinksQuantities = string.match(/\d+/g);
   let waterCupsQuantity = 0;
-  for (index = 0; index < drinksQuantities.length; index += 1) {
-    waterCupsQuantity += parseInt(drinksQuantities[index]);
+  for (let index = 0; index < drinksQuantities.length; index += 1) {
+    waterCupsQuantity += parseInt(drinksQuantities[index], 10);
   }
   if (waterCupsQuantity > 1) {
-    return waterCupsQuantity + " copos de água";
-  } else {
-    return waterCupsQuantity + " copo de água";
+    return `${waterCupsQuantity} copos de água`;
   }
-  
+  return `${waterCupsQuantity} copo de água`;
 }
 
 module.exports = {
