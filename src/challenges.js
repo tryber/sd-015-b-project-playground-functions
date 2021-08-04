6d0ca60c6c9f2f8ef782cbd3ece800fabb29c244
alvesgf16 <alvesgabriel1994@gmail.com>
2021-08-04 12:53:33 -0300

    Refatora desafios 2 a 4 e 6 a 8

--- src/challenges.js
+++ src/challenges.js
@@ -5,16 +5,18 @@ function compareTrue(param1, param2) {
 
 // Desafio 2
 function calcArea(base, height) {
-  return base * height / 2;
+  return (base * height) / 2;
 }
- // Desafio 3
+// Desafio 3
 function splitSentence(string) {
-  return string.split(" ");
+  return string.split(' ');
 }
 
 // Desafio 4
 function concatName(array) {
-  return array[array.length - 1] + ", " + array[0];
+  let firstElement = array[0];
+  let lastElement = array[array.length - 1];
+  return `${lastElement}, ${firstElement}`;
 }
 
 // Desafio 5
@@ -23,10 +25,22 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
+function getHighestNumber(array) {
+  let highestNumber = array[0];
+
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] > highestNumber) {
+      highestNumber = array[index];
+    }
+  }
+
+  return highestNumber;
+}
+
 function highestCount(array) {
   let numberCount = {};
 
-  for (index = 0; index < array.length; index += 1) {
+  for (let index = 0; index < array.length; index += 1) {
     if (numberCount[array[index]]) {
       numberCount[array[index]] += 1;
     } else {
@@ -36,49 +50,45 @@ function highestCount(array) {
 
   return numberCount[getHighestNumber(array)];
 }
-function getHighestNumber(array) {
-  let highestNumber = array[0];
-
-  for (index = 0; index < array.length; index += 1) {
-    if (array[index] > highestNumber) {
-      highestNumber = array[index];
-    }
-  }
-
-  return highestNumber;
-}
-
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
-    return "cat1";
-  } else if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
-    return "cat2";
-  } else {
-    return 'os gatos trombam e o rato foge'
+    return 'cat1';
+  }
+
+  if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
+    return 'cat2';
+  }
+
+  if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
+    return 'os gatos trombam e o rato foge';
   }
 }
 
 // Desafio 8
+function checkFizzBuzz(number, destinationArray) {
+  if (number % 15 === 0) {
+    destinationArray.push('fizzBuzz');
+  } else if (number % 3 === 0) {
+    destinationArray.push('fizz');
+  } else if (number % 5 === 0) {
+    destinationArray.push('buzz');
+  } else {
+    destinationArray.push('bug!');
+  }
+}
+
 function fizzBuzz(array) {
   let fizzBuzzArray = [];
 
-  for (index = 0; index < array.length; index += 1) {
-    if (array[index] % 3 === 0 || array[index] % 5 === 0) {
-      if (array[index] % 3 === 0 && array[index] % 5 === 0) {
-        fizzBuzzArray.push("fizzBuzz");
-      } else if (array[index] % 3 === 0 && array[index] % 5 !== 0) {
-        fizzBuzzArray.push("fizz");
-      } else if (array[index] % 3 !== 0 && array[index] % 5 === 0) {
-        fizzBuzzArray.push("buzz");
-      }
-    } else {
-      fizzBuzzArray.push("bug!");
-    }
+  for (let index = 0; index < array.length; index += 1) {
+    checkFizzBuzz(array[index], fizzBuzzArray);
   }
 
   return fizzBuzzArray;
 }
+
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9
 function encode(string) {
