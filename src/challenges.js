c033c736d094722142033cf8b00bf79531fc1729
Kdulima <carlosedu151@gmail.com>
2021-08-04 12:57:40 -0300

    correção do lint

--- src/challenges.js
+++ src/challenges.js
@@ -1,42 +1,42 @@
 // Desafio 1
 function compareTrue(a, b) {
-	return a && b
-} 
+  return a && b;
+}
 compareTrue(true, true);
 
 // Desafio 2
-function calcArea (base, height) {
-    return base * height /2;
-} 
-calcArea (10, 50);
-calcArea (5, 2);
-calcArea (51, 1);
+function calcArea(base, height) {
+  return base * height / 2;
+}
+calcArea(10, 50);
+calcArea(5, 2);
+calcArea(51, 1);
 
 // Desafio 3
-function splitSentence (string) {
-    return string.split(' ');
- }
- splitSentence("go Trybe");
- splitSentence("vamo que vamo");
- splitSentence("foguete");
+function splitSentence(string) {
+  return string.split(' ');
+}
+splitSentence('go Trybe');
+splitSentence('vamo que vamo');
+splitSentence('foguete');
 
 // Desafio 4
-function concatName (name) {
-    let first = [0];
-    let last = name.length-1
-    return name [last] + ', ' + name[first];  
-}
- concatName (['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']);
- concatName (['foguete', 'não', 'tem', 'ré']);
- concatName (['captain', 'my', 'captain']);
+function concatName(name) {
+  let first = [0];
+  let last = name.length -1;
+  return name [last] + ', ' + name[first];
+  }
+concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']);
+concatName(['foguete', 'não', 'tem', 'ré']);
+concatName(['captain', 'my', 'captain']);
 
 // Desafio 5
-function footballPoints (wins, ties) {
-    return (wins * 3) + (ties * 1)
+function footballPoints(wins, ties) {
+  return (wins * 3) + (ties * 1);
 }
 footballPoints(14, 8);
 footballPoints(1, 2);
-footballPoints(0, 0); 
+footballPoints(0, 0);
 
 // Desafio 6
 function highestCount() {
