3bcb7c18f446f51079d731a971226271c7c032a4
gustavogss <gustavogss.dev@gmail.com>
2021-08-04 12:35:36 -0300

    identacao

--- src/challenges.js
+++ src/challenges.js
@@ -36,14 +36,14 @@ function highestCount(numbersArray) {
   for (let i of numbersArray) {
     if (i > highestNumber) {
       highestNumber = i;
-    } 
+    }
   }
   for (let j of numbersArray) {
     if (j === highestNumber) {
       count += 1;
     }
   }
-  return count; 
+  return count;
 }
 
 // Desafio 7
@@ -56,7 +56,7 @@ function catAndMouse(mouse, cat1, cat2) {
   if (mouse > cat2) {
     cat2 = mouse - cat2;
   } else {
-    cat2 = cat2 - mouse; 
+    cat2 = cat2 - mouse;
   }
   if (cat1 === cat2) {
      return 'os gatos trombam e o rato foge';
@@ -65,31 +65,55 @@ function catAndMouse(mouse, cat1, cat2) {
     } if (cat2 > cat1) {
   return 'cat1';
   } 
- }
+}
 
 // Desafio 8
 function fizzBuzz(array) {
   let fizzBuzz = [];
   for (let index of array) {
-  if ((index % 3) === 0 && (index % 5) === 0) {
-    fizzBuzz.push("fizzBuzz");
-      } else if ((index % 3) === 0) {
-        fizzBuzz.push("fizz");
-     } else if ((index % 5) === 0) {
-  fizzBuzz.push("buzz");
-       } else {
-      fizzBuzz.push("bug!");
+    if ((index % 3) === 0 && (index % 5) === 0) {
+      fizzBuzz.push('fizzBuzz');
+  } else if ((index % 3) === 0) {
+    fizzBuzz.push("fizz");
+    } else if ((index % 5) === 0) {
+      fizzBuzz.push('buzz');
+  } else {
+      fizzBuzz.push('bug!');
     }
     }
     return fizzBuzz;
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function coder(phrase, codigo) {
+  let phraseDivider = phrase.split('');
+  for (let index = 0; index < phraseDivider.length; index += 1) {
+    if (codigo[phraseDivider[index]] !== undefined) {
+      phraseDivider[index] = codigo[phraseDivider[index]];
+    }
+  }
+  return phraseDivider.join('');
+}
+
+function encode(phrase) {
+  let encoder = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  };
+  return coder(phrase, encoder);
 }
 function decode() {
-  // seu código aqui
+  let decoder = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+  return coder('phrase', decoder);
 }
 
 module.exports = {
