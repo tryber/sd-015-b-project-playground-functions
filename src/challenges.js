805cb489149060030e9991f8377d58d351e31c04
Denis Jonathan <denisjonathan7@gmail.com>
2021-08-04 14:15:44 -0300

    Correção: erros do lint
    Algumas alterações e correções em erros do lint

--- src/challenges.js
+++ src/challenges.js
@@ -2,9 +2,8 @@
 function compareTrue(param1, param2) {
   if (param1 && param2 === true) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
 
 // Desafio 2
@@ -67,20 +66,12 @@ function catAndMouse(mouse, cat1, cat2) {
   let distanceCat1 = mouse - cat1;
   let distanceCat2 = mouse - cat2;
 
-  if (distanceCat1 < 0) {
-    distanceCat1 = distanceCat1 * -1;
-  }
-  if (distanceCat2 < 0) {
-    distanceCat2 = distanceCat2 * -1;
-  }
+  distanceCat1 = Math.abs(distanceCat1);
+  distanceCat2 = Math.abs(distanceCat2);
 
-  if (distanceCat1 < distanceCat2) {
-    return 'cat1';
-  } else if (distanceCat2 < distanceCat1) {
-    return 'cat2';
-  } else if (distanceCat1 === distanceCat2) {
-    return 'os gatos trombam e o rato foge';
-  }
+  if (distanceCat1 < distanceCat2) return 'cat1';
+  if (distanceCat2 < distanceCat1) return 'cat2';
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
@@ -154,7 +145,7 @@ function decode(str) {
       strDecoded += str[index];
     }
   }
-  
+
   return strDecoded;
 }
 
