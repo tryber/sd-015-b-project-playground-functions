e6327b171c48d3d4f2466c7b6a42476add44f55e
Arthur Teixeira <tuzinf5@gmail.com>
2021-08-03 23:13:37 -0300

    Adiciona requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -76,10 +76,48 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
+function encode(string) {
+  let palavraMinuscula = string;
+  let arrayCode = [];
+  for (let index = 0; index < palavraMinuscula.length; index += 1) {
+    if (palavraMinuscula[index] === "a") {
+      arrayCode.push(1);
+    } else if (palavraMinuscula[index] === "e") {
+      arrayCode.push(2);
+    } else if (palavraMinuscula[index] === "i") {
+      arrayCode.push(3);
+    } else if (palavraMinuscula[index] === "o") {
+      arrayCode.push(4);
+    } else if (palavraMinuscula[index] === "u") {
+      arrayCode.push(5);
+    } else {
+      arrayCode.push(palavraMinuscula[index]);
+    }
+  }
+  let frase = arrayCode.join("");
+  return frase;
   // seu código aqui
 }
-function decode() {
+function decode(string) {
+  let palavraMinuscula = string;
+  let arrayCode = [];
+  for (let index = 0; index < palavraMinuscula.length; index += 1) {
+    if (palavraMinuscula[index] === '1') {
+      arrayCode.push('a');
+    } else if (palavraMinuscula[index] === '2') {
+      arrayCode.push('e');
+    } else if (palavraMinuscula[index] === '3') {
+      arrayCode.push('i');
+    } else if (palavraMinuscula[index] === '4') {
+      arrayCode.push('o');
+    } else if (palavraMinuscula[index] === '5') {
+      arrayCode.push('u');
+    } else {
+      arrayCode.push(palavraMinuscula[index]);
+    }
+  }
+  let frase = arrayCode.join('');
+  return frase;
   // seu código aqui
 }
 
