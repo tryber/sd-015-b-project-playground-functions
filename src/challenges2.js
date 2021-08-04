8978f83776ab913015158e909103073916459152
Wander Luiz <wander.srv@gmail.com>
2021-08-03 23:20:28 -0300

    Resoluçao do desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,22 @@
 // Desafio 10
-function techList() {
+function techList(tecnologias, name) {
   // seu código aqui
+  if (tecnologias.length === 0){
+    return 'Vazio!'
+  } else {
+    tecnologias.sort();
+  }
+
+  let nameTecnologias = [];
+
+  for (let index = 0; index < tecnologias.length; index += 1){
+    let objNemaTecnologias = {
+      tech: tecnologias[index],
+      name: name
+    }
+    nameTecnologias.push(objNemaTecnologias);
+  }
+  return nameTecnologias
 }
 
 // Desafio 11
@@ -24,3 +40,5 @@ module.exports = {
   hydrate,
   triangleCheck,
 };
+
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Wander'))
