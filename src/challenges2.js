bb5a48efa7ed8e494c985880c47840c82cf15688
Denis Jonathan <denisjonathan7@gmail.com>
2021-08-04 13:53:44 -0300

    Correção: correções do lint
    Algumas modificações e correções no código, referente aos erros de lint

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,93 +1,84 @@
 // Desafio 10
 function techList(arr, name) {
   if (arr.length < 1) {
-    return 'Vazio!'
+    return 'Vazio!';
   }
 
-  arr.sort()
-  let techsAndNames = []
+  arr.sort();
+  let techsAndNames = [];
 
   for (let index = 0; index < arr.length; index += 1) {
     let info = {
       tech: arr[index],
-      name: name,
-    }
+      name,
+    };
 
-    techsAndNames.push(info)
+    techsAndNames.push(info);
   }
 
-  return techsAndNames
+  return techsAndNames;
 }
 
 // Desafio 11
 function generatePhoneNumber(arr) {
-  let phoneNumber = ''
+  let phoneNumber = '';
   
   if (arr.length !== 11) {
-    return 'Array com tamanho incorreto.'
+    return 'Array com tamanho incorreto.';
   }
 
   for (let index = 0; index < arr.length; index += 1) {
-    if (arr[index] < 0 || arr[index] > 9) {
-      return 'não é possível gerar um número de telefone com esses valores'
-    }
-  }
-
-  for (let index = 0; index < arr.length; index += 1) {
-    let count = 0
+    let count = 0;
     for (let comparate = 0; comparate < arr.length; comparate += 1) {
       if (arr[index] === arr[comparate]) {
-        count += 1
+        count += 1;
       }
     }
-    if (count >= 3) {
-      return 'não é possível gerar um número de telefone com esses valores'
+    if (arr[index] < 0 || arr[index] > 9 || count >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
     }
   }
 
   for (let index = 0; index < arr.length; index += 1) {
-    switch(index) {
-      case 0:
-        phoneNumber += `(${arr[index]}`
-        break
-
-      case 1:
-        phoneNumber += `${arr[index]}) `
-        break
-
-      case 6:
-        phoneNumber += `${arr[index]}-`
-        break
-
-      default:
-        phoneNumber += arr[index]
+    switch (index) {
+    case 0:
+      phoneNumber += `(${arr[index]}`;
+      break;
+    case 1:
+      phoneNumber += `${arr[index]}) `;
+      break;
+    case 6:
+      phoneNumber += `${arr[index]}-`;
+      break;
+    default:
+      phoneNumber += arr[index];
     }
   }
-
-  return phoneNumber
+  
+  return phoneNumber;
 }
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   if (lineA < (lineB + lineC) && lineB < (lineA + lineC)) {
-    return true
+    return true;
   }
-  return false
+  return false;
 }
 
 // Desafio 13
 function hydrate(str) {
-  let onlyNumbers = str.replace(/\D/g, '')
-  let count = 0
+  let onlyNumbers = str.replace(/\D/g, '');
+  let count = 0;
 
   for (let index = 0; index < onlyNumbers.length; index += 1) {
-      count += parseInt(onlyNumbers[index])
+    count += parseInt(onlyNumbers[index]);
   }
 
   if (count <= 1) {
-    return `${count} copo de água`
+    return `${count} copo de água`;
   }
-  return `${count} copos de água`
+  return `${count} copos de água`;
 }
 
 module.exports = {
