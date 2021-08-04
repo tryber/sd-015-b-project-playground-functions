cbd1a54c8e7bc001a0bb704499ef9de96791e816
Adson Gomes Oliveira <adsongoliveira2021@gmail.com>
2021-08-04 06:41:41 -0300

    Decimo requisito concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,33 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
+function techList(array, name) {
+  let output = [];
 
+  for (let index = 0; index < array.length; index++) {
+    output.push({
+      name: name,
+      tech: array[index],
+    });
+  }
+
+  if (output.length === 0) {
+    return 'Vazio!';
+  }
+
+  output.sort(function (x, y) {
+    x = x.tech.toUpperCase();
+    y = y.tech.toUpperCase();
+
+    if (x > y) {
+      return 1;
+    }
+    if (x < y) {
+      return -1;
+    }
+    return 0;
+  });
+
+  return output;
+}
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
