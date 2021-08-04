0f7a2ed4a2cad1674bdc0ca928a92f69de0b7699
ArthurMoreira <arthurmcs96@gmail.com>
2021-08-04 00:22:08 -0300

    altera o desafio 7 e remove o console.log do desafio 6 (nao salvei o arquivo  no desafio anterior)

--- src/challenges.js
+++ src/challenges.js
@@ -1,80 +1,66 @@
 // Desafio 1
 function compareTrue(value1, value2) {
-  if (value1 === true && value2 === true){
-    return (true)
-  }else{
-    return(false)
+  if (value1 === true && value2 === true) {
+    return true;
+  } else {
+    return false;
   }
 }
-compareTrue(true, true)
+compareTrue(true, true);
 
 // Desafio 2
-function calcArea(base, height){
-  return (base * height / 2) 
-  
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
-calcArea(51, 1)
+calcArea(51, 1);
 
 // Desafio 3
-function splitSentence(phrase){
-  let result = phrase.split(" ")
-  return (result)
+function splitSentence(phrase) {
+  let result = phrase.split(' ');
+  return result;
 }
-splitSentence ("go trybe")
+splitSentence('go trybe');
 
 // Desafio 4
-function concatName(name){
-  let result = name[name.length - 1]+ ", "+ name[0]
-  return (result)
+function concatName(name) {
+  let result = name[name.length - 1] + ', ' + name[0];
+  return result;
 }
-concatName(['captain', 'my', 'captain'])
+concatName(['captain', 'my', 'captain']);
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  let result = (wins * 3) + ties
-  return result
+  let result = wins * 3 + ties;
+  return result;
 }
-footballPoints(1, 2)
+footballPoints(1, 2);
 
 // Desafio 6
 function highestCount(numbers) {
+  let highestNumber = 0;
+  let countNumber = 0;
 
-  let highestNumber = 0
-  let countNumber = 0
-
-  for(let index = 0; index < numbers.length; index++){
-    if(numbers[index] > highestNumber){
-      highestNumber = numbers[index]
-      countNumber = 1
-
-     }else if(numbers[index] === highestNumber){
-       countNumber++
-     }
+  for (let index = 0; index < numbers.length; index++) {
+    if (numbers[index] > highestNumber) {
+      highestNumber = numbers[index];
+      countNumber = 1;
+    } else if (numbers[index] === highestNumber) {
+      countNumber++;
+    }
   }
-  return (countNumber)
+  return countNumber;
 }
-
-highestCount([9, 1, 2, 3, 9, 5, 7])
-highestCount([0, 4, 4, 4, 9, 2, 1])
-highestCount([0, 0, 0])
-
 
 // Desafio 7
-function catAndMouse(mouse, cat1, cat2) {
-  let distanceCat1 = mouse - cat1
-  let distanceCat2 = mouse - cat2
-  
-  if(distanceCat1 < distanceCat2){
-    return ("cat1")
-
-  }else if(distanceCat2 < distanceCat1){
-    return ("cat2")
-
-  }else{
-    return ("os gatos trombam e o rato foge")
+function catAndMouse(cat1, cat2) {
+  if (cat1 < cat2) {
+    return 'cat1';
+  } else if (cat2 < cat1) {
+    return 'cat2';
+  } else {
+    return 'os gatos trombam e o rato foge';
   }
 }
-  catAndMouse(10,1,3)
 
 // Desafio 8
 function fizzBuzz() {
