9ed6937ce7189ff5f89a3350c2994ee2bcac94ab
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-08-04 10:46:15 -0300

    Adiciona questao 7, testata.

--- src/challenges.js
+++ src/challenges.js
@@ -50,9 +50,22 @@ function highestCount(arr) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+function catAndMouse (mouse, cat1, cat2) {
+
+  let distanceCat1 = Math.abs(cat1 - mouse);
+    let distanceCat2 = Math.abs(cat2 - mouse);
+    if (distanceCat1 < distanceCat2) {
+      return 'cat1';
+    }
+    else if (distanceCat2 < distanceCat1) {
+      return 'cat2';
+    }
+    else if (distanceCat1 === distanceCat2){
+      return 'os gatos trombam e o rato foge';
+    }
+  }
+
+
 
 // Desafio 8
 function fizzBuzz() {
