618eda608534822bb38631d915d2d6d12bb07007
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-08-04 13:15:49 -0400

    add challenge 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,5 +1,24 @@
 // Desafio 10
-function techList() {
+// funcao .sort retirada da documentacao: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+function techList(array,name) {
+  let listaSaida = [];
+  if(array.length === 0){
+    return "Vazio!"
+  }
+
+  else {
+    for (let tech in array) {
+    array.sort();
+    obj = {
+      tech: "NomeTech",
+      name: name
+      }
+    obj.tech = array[tech];
+    listaSaida.push(obj);
+    }
+  }
+  return listaSaida;
+  // return listaSaida;
   // seu código aqui
 }
 
