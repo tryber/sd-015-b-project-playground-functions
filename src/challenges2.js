af99c911bc2c206577c7c013ea0cfd0585d0b0c1
Ge <gmarcoscardoso@gmail.com>
2021-08-04 11:46:49 -0300

    Atividade 11 completa

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,11 +16,37 @@ function techList(techName, name) {
     return bankOfSkill
 }
 
-console.log(techList([], 'Lucas'));
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(numberFone) {
   // seu código aqui
+  // faz uma mensagem com tudo
+  let numberFone2 = numberFone
+
+  if (numberFone.length !== 11)
+    return "Array com tamanho incorreto."
+  
+  for (let index = 0; index < numberFone.length; index += 1) {
+    if (numberFone[index] < 0 || numberFone[index] > 9)
+      return "não é possível gerar um número de telefone com esses valores"
+  }
+
+  let count
+  for (let index = 0; index < numberFone.length; index += 1) {
+    count = 0
+
+    for (let chave = 0; chave < numberFone.length; chave += 1) {
+      if (numberFone[index] === numberFone2[chave]) {
+        count += 1
+        }
+      }
+
+    if (count >= 3)
+      return "não é possível gerar um número de telefone com esses valores"
+  }
+    
+      return phoneDDD = "(" + numberFone[0] + numberFone[1] +")" + " " + numberFone[2] + numberFone[3] + numberFone[4] + numberFone[5] + numberFone[6] + "-" + numberFone[7] + numberFone[8] + numberFone[9] + numberFone[10] 
 }
+console.log(generatePhoneNumber([0, 2, 3, 4, 4, 2, 7, 8, 9, 9, 4]))
 
 // Desafio 12
 function triangleCheck() {
