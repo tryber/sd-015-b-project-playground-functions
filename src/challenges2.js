c3b61465bb426f530f08f92087c751c3c95bb834
Lucas Peres <lucasperesss@gmail.com>
2021-08-04 03:13:26 -0300

    Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -11,8 +11,40 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function repete3(numbers) {
+  let count = {};
+
+  numbers.forEach(function (x) { count[x] = (count[x] || 0) + 1; });
+
+  for (let y = 1; y <= 11; y += 1) {
+    if (count[y] > 2) {
+      return true;
+    }
+  }
+}
+function possivelGerar(numbers) {
+  let possible;
+  for (let n of numbers) {
+    if (n < 0 || n > 9) {
+      possible = false;
+    }
+  }
+  if (repete3(numbers) === true) {
+    possible = false;
+  }
+  return possible;
+}
+function generatePhoneNumber(array) {
+  let d11;
+  if (array.length !== 11) {
+    d11 = 'Array com tamanho incorreto.';
+  } else if (possivelGerar(array) === false) {
+    d11 = 'não é possível gerar um número de telefone com esses valores';
+  } else {
+    d11 = `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
+  }
+
+  return d11;
 }
 
 // Desafio 12
