f8f4db891341cc0f10f0eb670d55364cdc58c4cf
gustavogss <gustavogss.dev@gmail.com>
2021-08-04 10:50:15 -0300

    identacao

--- src/challenges.js
+++ src/challenges.js
@@ -22,7 +22,7 @@ function concatName() {
 }
 
 // Desafio 5
-function footballPoints(wins, ties) { 
+function footballPoints(wins, ties) {
   let newWins = 3 * wins;
   let totalPoints = newWins + ties;
   return totalPoints;
@@ -37,34 +37,34 @@ function highestCount() {
 function catAndMouse(mouse, cat1, cat2) {
   if (mouse > cat1) {
     cat1 = mouse - cat1;
-  } else {
-    cat1 = cat1 - mouse;
-  }
-  if (mouse > cat2) {
-    cat2 = mouse - cat2;
-  } else { 
-    cat2 = cat2 - mouse;
-  }
-  if (cat1 === cat2) {
+    } else {
+      cat1 = cat1 - mouse;
+      }
+      if (mouse > cat2) {
+        cat2 = mouse - cat2;
+      } else { 
+        cat2 = cat2 - mouse;
+}
+    if (cat1 === cat2) {
     return 'os gatos trombam e o rato foge';
-  } if (cat1 > cat2) {
+    } if (cat1 > cat2) {
     return 'cat2';
-  } if (cat2 > cat1) {
+    } if (cat2 > cat1) {
     return 'cat1';
-  }
+   }
 }
 
 // Desafio 8
 function fizzBuzz(array) {
   let fizzBuzz = [];
-    for (let index of array) {
-    if ((index % 3) === 0 && (index % 5) === 0) {
+  for (let index of array) {
+  if ((index % 3) === 0 && (index % 5) === 0) {
     fizzBuzz.push("fizzBuzz");
-  } else if ((index % 3) === 0) {
-    fizzBuzz.push("fizz");
-  } else if ((index % 5) === 0) {
-    fizzBuzz.push("buzz");
-  } else {
+    } else if ((index % 3) === 0) {
+      fizzBuzz.push("fizz");
+      } else if ((index % 5) === 0) {
+      fizzBuzz.push("buzz");
+      } else {
     fizzBuzz.push("bug!");
   }
   }
