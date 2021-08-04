6f0a2e0c13e80400ea0aee9bbccf7bf334a7f5be
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-08-04 09:51:35 -0300

    Adicionando muitos pontos e virgulas e simplificando compareTrue

--- src/challenges.js
+++ src/challenges.js
@@ -1,106 +1,105 @@
 // Desafio 1
 function compareTrue(parametro1, parametro2) {
   // seu código aqui
-  if (parametro1 === true && parametro2 === true) return true
-  else return false
+  return (parametro1 && parametro2);
 }
 
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui
-  return (base * height) / 2
+  return (base * height) / 2;
 }
 
 // Desafio 3
 function splitSentence(string) {
   // seu código aqui
-  return string.split(' ')
+  return string.split(' ');
 }
 
 // Desafio 4
 function concatName(array) {
   // seu código aqui
-  return `${array.pop()}, ${array.shift()}`
+  return `${array.pop()}, ${array.shift()}`;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
   // seu código aqui
-  return (wins * 3) + ties
+  return (wins * 3) + ties;
 }
 
 // Desafio 6
 function highestCount(numbers) {
   // seu código aqui
-  let maior = biggestNumber(numbers)
-  let cont = 0
+  let maior = biggestNumber(numbers);
+  let cont = 0;
   for (let number of numbers) {
     if (number === maior) cont += 1
   }
-  return cont
+  return cont;
 }
 function biggestNumber(numbers) {
-  maior = numbers[0]
+  maior = numbers[0];
   for (number of numbers) {
-    if (number > maior) maior = number
+    if (number > maior) maior = number;
   }
-  return maior
+  return maior;
 }
 
 // Desafio 7
 function catAndMouse(mouse, catOne, catTwo) {
   // seu código aqui
-  let catOneDis = calcDis(mouse, catOne)
-  let catTwoDis = calcDis(mouse, catTwo)
-  let string = ''
+  let catOneDis = calcDis(mouse, catOne);
+  let catTwoDis = calcDis(mouse, catTwo);
+  let string = '';
 
   if (catOneDis < catTwoDis) string = 'cat1'
   else if (catTwoDis < catOneDis) string = 'cat2'
   else string = 'os gatos trombam e o rato foge'
 
-  return string
+  return string;
 }
 function calcDis(mouse, cat) {
-  return Math.abs(mouse - cat)
+  return Math.abs(mouse - cat);
 }
 
 // Desafio 8
 function fizzBuzz(numbers) {
   // seu código aqui
-  let fizzBuzzes = []
+  let fizzBuzzes = [];
   for (number of numbers) {
-    if (number % 3 === 0 && number % 5 === 0) fizzBuzzes.push('fizzBuzz')
-    else if (number % 3 === 0) fizzBuzzes.push('fizz')
-    else if (number % 5 === 0) fizzBuzzes.push('buzz')
-    else fizzBuzzes.push('bug!')
+    if (number % 3 === 0 && number % 5 === 0) fizzBuzzes.push('fizzBuzz');
+    else if (number % 3 === 0) fizzBuzzes.push('fizz');
+    else if (number % 5 === 0) fizzBuzzes.push('buzz');
+    else fizzBuzzes.push('bug!');
   }
-  return fizzBuzzes
+  return fizzBuzzes;
 }
 
 // Desafio 9
 function encode(string) {
   // seu código aqui
-  let vecString = string.split('')
+  let vecString = string.split('');
   for (let i = 0; i < vecString.length; i += 1) {
-    if (vecString[i] === 'a') vecString[i] = 1
-    else if (vecString[i] === 'e') vecString[i] = 2
-    else if (vecString[i] === 'i') vecString[i] = 3
-    else if (vecString[i] === 'o') vecString[i] = 4
-    else if (vecString[i] === 'u') vecString[i] = 5
+    if (vecString[i] === 'a') vecString[i] = 1;
+    else if (vecString[i] === 'e') vecString[i] = 2;
+    else if (vecString[i] === 'i') vecString[i] = 3;
+    else if (vecString[i] === 'o') vecString[i] = 4;
+    else if (vecString[i] === 'u') vecString[i] = 5;
   }
-  return vecString.join('')
+  return vecString.join('');
 }
 function decode(string) {
   // seu código aqui
-  let vecString = string.split('')
+  let vecString = string.split('');
   for (let i = 0; i < vecString.length; i += 1) {
-    if (vecString[i] === '1') vecString[i] = 'a'
-    else if (vecString[i] === '2') vecString[i] = 'e'
-    else if (vecString[i] === '3') vecString[i] = 'i'
-    else if (vecString[i] === '4') vecString[i] = 'o'
-    else if (vecString[i] === '5') vecString[i] = 'u'
+    if (vecString[i] === '1') vecString[i] = 'a';
+    else if (vecString[i] === '2') vecString[i] = 'e';
+    else if (vecString[i] === '3') vecString[i] = 'i';
+    else if (vecString[i] === '4') vecString[i] = 'o';
+    else if (vecString[i] === '5') vecString[i] = 'u';
   }
-  return vecString.join('')
+  return vecString.join('');
 }
 
 module.exports = {
