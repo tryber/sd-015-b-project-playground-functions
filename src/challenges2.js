0bb51db60a372ffe3a2a95c63cdcca520f827b9d
Ge <gmarcoscardoso@gmail.com>
2021-08-04 09:30:22 -0300

    Atividade 10 completa

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,22 @@
 // Desafio 10
-function techList() {
+function techList(techName, name) {
   // seu código aqui
+  let bankOfSkill = []
+  let changeSort = techName
+  changeSort.sort()
+  let flip
+
+  for (let index = 0; index < changeSort.length; index += 1){
+    flip = {  tech: changeSort[index], name: name,}
+    bankOfSkill.push(flip)
+  }
+  if (bankOfSkill.length === 0)
+    return "Vazio!"
+  else
+    return bankOfSkill
 }
 
+console.log(techList([], 'Lucas'));
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
