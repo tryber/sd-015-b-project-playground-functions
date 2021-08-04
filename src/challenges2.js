f5e678216038093f2636eb0b63b02d2b22e0cac4
Camilla Del Guerso <camilladelguerso@gmail.com>
2021-08-03 22:23:23 -0300

    Adiciona desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,23 +1,29 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
+function techList(arrayTech, name) {
+  let objTechs;
+  let pushArray = [];
 
+  if (arrayTech.length < 1) {
+    return 'Vazio!';
+  } for (let i in arrayTech) {
+    arrayTech.sort();
+    objTechs = { tech: arrayTech[i], name: name };
+    pushArray.push(objTechs);
+  }
+  return pushArray;
+}
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
 }
-
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
 }
-
 // Desafio 13
 function hydrate() {
   // seu código aqui
 }
-
 module.exports = {
   generatePhoneNumber,
   techList,
