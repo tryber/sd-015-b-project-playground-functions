7c299d61f8076b4d634ef1e1c7dd8a8a4e2a337a
FERNANDO <Fernando@FERNANDO.local>
2021-08-03 22:18:42 -0300

    Exercicio doze

--- src/challenges2.js
+++ src/challenges2.js
@@ -10,9 +10,15 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA < (lineB + lineC) && lineB < (lineA + lineC) && lineC < (lineA + lineB) && lineA > (Math.abs(lineB - lineC)) && lineB > (Math.abs(lineA - lineC)) && lineC > (Math.abs(lineA - lineB))){
+    return true
+  } else{
+    return false
+  }
+    
+  }
+
 
 // Desafio 13
 function hydrate() {
