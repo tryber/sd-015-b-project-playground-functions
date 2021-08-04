fc0225acffd4522ab20058a9afc6ee3d76a20851
Amanda Fernandes <aferanda@gmail.com>
2021-08-03 22:33:38 -0300

    Adiciona desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,26 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arrayTech, name) {
+  let list = [];
+  let obj = {};
+  for (let index = 0; index < arrayTech.length; index += 1) {
+    obj.tech = arrayTech[index];
+    obj.name = name;
+    list.push(obj);
+    obj = {};
+  }
+  list.sort(function (a, b) {
+    if (a.tech < b.tech) {
+      return -1;
+    } return true;
+  });
+  if (list.length > 0) {
+    return list;
+  } return 'Vazio!';
 }
+
+let arrayTech = ['React', 'Jest', 'HTML', 'CSS', 'JavaScript'];
+let name = 'Amanda';
+console.log(techList(arrayTech, name));
 
 // Desafio 11
 function generatePhoneNumber() {
