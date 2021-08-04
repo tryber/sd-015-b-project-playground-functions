90d7ae16224f430177629904e1522e7aab725ee3
Samuel Pereira <gmr.quest.10@gmail.com>
2021-08-03 23:00:46 -0300

    Requisito 12 concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,10 +4,7 @@ function techList(array, name) {
   let result = [];
 
   for (let i of array) {
-    result.push({
-      tech: i, 
-      name: name
-    });
+    result.push({ tech: i, name: name });
   }
   if (array.length === 0) {
     return 'Vazio!';
@@ -22,8 +19,16 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(a, b, c) {
+  if (a + b < c || Math.abs(a - b) > c) {
+    return false;
+  } else if (a + c < b || Math.abs(a - c) > b) {
+    return false;
+  } else if (c + b < a || Math.abs(c - b) > a) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 // Desafio 13
