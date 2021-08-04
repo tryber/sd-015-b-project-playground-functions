b14c1ded8e16109a0d82a72b389e3f2c7d7dc50a
Guilherme Saboia <gsaboia@yahoo.com>
2021-08-03 21:44:08 -0300

    Adiciona Desafio 12 completo

--- src/challenges2.js
+++ src/challenges2.js
@@ -75,7 +75,7 @@ function generatePhoneNumber(array) {
 // Desafio 12
 function createSideArray(lineA, lineB, lineC) {
   let sideArray = [lineA, lineB, lineC];
-  return sideArray
+  return sideArray;
 }
 function createSumArray(lineA, lineB, lineC) {
   let sumAB = lineA + lineB;
@@ -84,27 +84,62 @@ function createSumArray(lineA, lineB, lineC) {
   let sumArray = [sumBC, sumAC, sumAB];
   return sumArray;
 }
-function checkSum(lineA, lineB, lineC) {
-  let sideArray = createSideArray(lineA, lineB, lineC);
-  let sumArray = createSumArray(lineA, lineB, lineC);
-  for (let counter = 0; counter < sideArray.lenght; counter += 1) {
-    
-  }
-}
-function checkDiff() {
+function createDiffArray(lineA, lineB, lineC) {
   let diffAB = Math.abs(lineA - lineB);
   let diffAC = Math.abs(lineA - lineC);
   let diffBC = Math.abs(lineB - lineC);
+  let diffArray = [diffBC, diffAC, diffAB];
+  return diffArray;
+}
+function createSideMinusSum(lineA, lineB, lineC) {
+  let sideArray = createSideArray(lineA, lineB, lineC);
+  let sumArray = createSumArray(lineA, lineB, lineC);
+  let sideMinusSum = [];
+  for (let side of sideArray) {
+    for (let sum of sumArray) {
+      sideMinusSum.push(sum - side);
+    }
+  }
+  return sideMinusSum;
+}
+function createSideMinusDiff(lineA, lineB, lineC) {
+  let sideArray = createSideArray(lineA, lineB, lineC);
+  let diffArray = createDiffArray(lineA, lineB, lineC);
+  let sideMinusDiff = [];
+  for (let side of sideArray) {
+    for (let diff of diffArray) {
+      sideMinusDiff.push(side - diff);
+    }
+  }
+  return sideMinusDiff;
+}
+function checkSum(lineA, lineB, lineC) {
+  let sideMinusSum = createSideMinusSum(lineA, lineB, lineC);
+  for (let item of sideMinusSum) {
+    if (item < 0) {
+      return false;
+    }
+  }
+  return true;
+}
+function checkDiff(lineA, lineB, lineC) {
+  let sideMinusDiff = createSideMinusDiff(lineA, lineB, lineC);
+  for (let item of sideMinusDiff) {
+    if (item < 0) {
+      return false;
+    }
+  }
+  return true;
 }
 function triangleCheck(lineA, lineB, lineC) {
-  let test = checkSum(lineA, lineB, lineC);
-  if (test < 0) {
-    return false;
+  let testSum = checkSum(lineA, lineB, lineC);
+  let testDiff = checkDiff(lineA, lineB, lineC);
+  if (testSum && testDiff) {
+    return true;
   }
-  
-  return test;
+  return false;
 }
-console.log(triangleCheck(10, 14, 8))
+
 // Desafio 13
 function hydrate() {
   // seu código aqui
