2623cb76ee1ac26b283deb98100065f7f4dbafb2
gustavogss <gustavogss.dev@gmail.com>
2021-08-04 01:44:12 -0300

    Requisito4

--- src/challenges.js
+++ src/challenges.js
@@ -5,27 +5,27 @@ function compareTrue(a, b) {
 } compareTrue(true, true);
 
 // Desafio 2
-function calcArea(base, heigth) {
-  let area = 0;
-  area = (base * heigth) / 2;
+let area = 0;
+function calcArea(base, height) {
+  area === (base * height) / 2;
   return area;
 } console.log(calcArea(10, 50));
 
 // Desafio 3
-function splitSentence(str) {
-  if (str = 'Go Trybe') {
-    return str.split(' ');
-  } else if (str = 'vamo que vamo') {
-    return str.split(' ');
-  } else if ( str = 'foguete'){
-    return 'foguete';   
-  } 
-  } console.log(splitSentence('foguete'));
+function splitSentence() {
+  // seu código aqui
+}
 
 // Desafio 4
 function concatName() {
-  // seu código aqui
-}
+  if (string= ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']){
+    return string[0] + ' ' + string[string.length -1];
+  }else if (string = ['foguete', 'não', 'tem', 'ré']){
+    return string[0] + ' ' + string[string.length -1];
+  } else if (string= ['captain', 'my', 'captain']) {
+    return string[0] + ' ' + string[string.length -1];
+} 
+} console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
 // Desafio 5
 function footballPoints() {
