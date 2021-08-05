4af48c9b18f0673e88b0d67f3ead3bc2d0651adf
Joao Marco <joaomarco.matta@gmail.com>
2021-08-05 16:44:03 -0300

    Implementa funçoes do requisito 11.

--- src/challenges2.js
+++ src/challenges2.js
@@ -8,17 +8,64 @@ function techList(arrOfTechs, name) {
 
   arrOfTechs.sort();
   for (let tech of arrOfTechs) {
-    list.push({tech, name});
+    list.push({ tech, name });
   }
 
   return list;
 }
-console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function validateArraySize(phoneNumber) {
+  if (phoneNumber.length !== 11) {
+    return false;
+  }
+
+  return true;
 }
+
+function checkRepeatingNumbers(phoneNumber) {
+  for (let i in phoneNumber) {
+    let repeatedNumbersCount = 0;
+    for (let j = 0; j < phoneNumber.length; j += 1) {
+      if (phoneNumber[i] === phoneNumber[j]) {
+        repeatedNumbersCount += 1;
+      }
+    }
+    if (repeatedNumbersCount >= 3) { 
+      return false;
+    };
+  }
+
+  return true;
+}
+
+function validateArrayValue(phoneNumber) {
+  for (let i in phoneNumber) {
+    if (phoneNumber[i] < 0 || phoneNumber[i] > 9) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function isValidNumber(phoneNumber) {
+  return validateArraySize(phoneNumber) && validateArrayValue(phoneNumber) && checkRepeatingNumbers(phoneNumber);
+}
+
+function phoneNumberMask() {
+  let maskedArr = ['(', 0, 0, ')', 0, 0, 0, 0, 0, '-', 0, 0, 0, 0,];
+  return maskedArr;
+}
+
+function generatePhoneNumber(phoneNumber) {
+  if (!isValidNumber(phoneNumber)) return 'Array inválido';
+
+  for (let i in phoneNumberMask()) {
+
+  }
+}
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
@@ -26,8 +73,12 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function getIntegers(string) {
+  
+}
+
+function hydrate(string) {
+  
 }
 
 module.exports = {
