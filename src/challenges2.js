c6040d2d66b19cf7cdcd52cb9822855822433ea1
Israel Jeronimo <israel.nodejs@gmail.com>
2021-08-05 17:42:21 -0300

    12 - Crie uma função de Condição de existência de um triângulo

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,16 +1,43 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
+function techList(arr, name) {
+  let obj = {};
+  let arrayDeObj = [];
 
+  if (arr.length > 0) {
+    for (const value of arr) {
+      obj['tech'] = value;
+      obj['name'] = name;  
+      //arrayDeObj = obj;
+      arrayDeObj.push(obj);
+      for (const key in obj) {
+        obj.tech = value;
+      }
+    }
+  } else {
+    return 'Vazio!';
+  }
+  return arrayDeObj;
+}
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "israel"));
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(la, lb, lc) {
+  let result = false;
+
+  if ((la < (lb + lc)) && (lb < (la + lc)) && (lc < (la + lb))){
+    if ((la > Math.abs(lb - lc)) && (lb > Math.abs(la - lc)) && (lc > Math.abs(la - lb))) {
+      result = true;
+    } else {
+      result;
+    }
+  } else {
+    result;
+  }
+  return result;
 }
 
 // Desafio 13
