f8c46289110e4d9ae12559ff34ca74a15380f73c
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2021-08-05 18:52:04 -0300

    adiciona desafio 12

--- src/challenges.js
+++ src/challenges.js
@@ -1,88 +1,88 @@
 // Desafio 1
 function compareTrue (parameter1, parameter2) {
   if (parameter1 === true && parameter2 === true) {
-    return true;
+    return true
   } else {
-    return false;
+    return false
   }
 }
 
 // Desafio 2
 function calcArea (base, height) {
-  const triangleArea = (base * height) / 2;
-  return triangleArea;
+  const triangleArea = (base * height) / 2
+  return triangleArea
 }
 // Desafio 3
-function splitSentence(phrase) {
-  const myString = phrase;
-  const splitedSentence = myString.split(' ');
-  return splitedSentence;
+function splitSentence (phrase) {
+  const myString = phrase
+  const splitedSentence = myString.split(' ')
+  return splitedSentence
 }
 
 // Desafio 4
 function concatName (array) {
-  const myArray = array;
-  const invertedArray = myArray.reverse();
-  const result1 = invertedArray[0];
-  const result2 = invertedArray[invertedArray.length - 1];
-  const result = result1 + ', ' + result2;
+  const myArray = array
+  const invertedArray = myArray.reverse()
+  const result1 = invertedArray[0]
+  const result2 = invertedArray[invertedArray.length - 1]
+  const result = result1 + ', ' + result2
 
-  return result;
+  return result
 }
 // Desafio 5
 function footballPoints (wins, ties) {
-  const victories = 3 * wins;
-  const draws = 1 * ties;
-  const points = victories + draws;
-  return points;
+  const victories = 3 * wins
+  const draws = 1 * ties
+  const points = victories + draws
+  return points
 }
 
 // Desafio 6
 function highestCount (numbersArray) {
-  const ordenedArray = numbersArray.sort(function (a, b) { return b - a });
-  const higherNumber = ordenedArray[0];
-  let quantasVezesSeRepete = 0;
+  const ordenedArray = numbersArray.sort(function (a, b) { return b - a })
+  const higherNumber = ordenedArray[0]
+  let quantasVezesSeRepete = 0
 
   for (let index = 0; index < ordenedArray.length; index += 1) {
     if (ordenedArray[index] === higherNumber) {
-      quantasVezesSeRepete += 1;
+      quantasVezesSeRepete += 1
     }
   }
-  return quantasVezesSeRepete;
+  return quantasVezesSeRepete
 }
 // Desafio 7
 function catAndMouse (mouse, cat1, cat2) {
-  let resposta = 0;
+  let resposta = 0
   if (cat1 - mouse === cat2 - mouse || cat1 - mouse === mouse - cat2 || mouse - cat1 === mouse - cat2 || mouse - cat1 === cat2 - mouse) {
-    resposta = 'os gatos trombam e o rato foge';
+    resposta = 'os gatos trombam e o rato foge'
   } else if (cat1 < cat2) {
-    resposta = 'cat1';
+    resposta = 'cat1'
   } else {
-    resposta = 'cat2';
+    resposta = 'cat2'
   }
   return resposta
 }
-  // Desafio 8
-  function fizzBuzz (numbers){
-    let answer = []
+// Desafio 8
+function fizzBuzz (numbers) {
+  const answer = []
 
-  for (let index = 0; index < numbers.length; index += 1){
-    if (numbers[index] % 3 === 0 && numbers[index] % 5 === 0){
-      answer.push('fizzBuzz');
-    } else{
-      if (numbers[index] % 3 === 0){
-        answer.push('fizz');
-      } else if (numbers[index] % 5 === 0){
-        answer.push('buzz');
+  for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] % 3 === 0 && numbers[index] % 5 === 0) {
+      answer.push('fizzBuzz')
+    } else {
+      if (numbers[index] % 3 === 0) {
+        answer.push('fizz')
+      } else if (numbers[index] % 5 === 0) {
+        answer.push('buzz')
       } else {
-        answer.push('bug!');
+        answer.push('bug!')
       }
     }
   }
-  return answer;
+  return answer
 }
 
-  // Desafio 9
+// Desafio 9
 function encode (string) {
 
 }
--- src/sddssd.js
+++ src/sddssd.js
@@ -1,13 +1,13 @@
 
 //  // Desafio 8
 // // function fizzBuzz (numbers) {
-// //   let myArray = [] ; 
+// //   let myArray = [] ;
 // //   const rules = {
 // //       div3: 'fizz',
 // //       div5: 'buzz',
 // //       div3And5: 'fizzBuzz',
 // //       none: 'bug!',
-// //       } 
+// //       }
 // //   for (let i = 0; i < numbers.length; i += 1){
 // //     if(numbers % 3 === 0 && numbers[i] % 5 === 0){
 // //      myArray.push(rules.div3And5);
@@ -19,12 +19,6 @@
 // //   }
 // //   return myArray
 // //   }
-
-
-
-
-
-
 
 // function fizzBuzz(numberArray){
 //     let resposta = [];
@@ -42,7 +36,7 @@
 //         }
 //       }
 //       }
-  
+
 //   function div5(numberArray){
 //     for(let index of numberArray){
 //       if (numberArray[index] % 3 !== 0 && numberArray[index] % 5 === 0){
@@ -57,14 +51,11 @@
 //         }
 //       }
 //       }
-    
+
 //         return resposta;
 //   }
 //   const numberArray = [20,0,15,30,50]
 //   console.log(fizzBuzz(numberArray));
-
-
-
 
 //   function fizzBuzz (numbers){
 //     var answer = [];
@@ -81,30 +72,27 @@
 //         return answer;
 //     }
 //     }
-    
+
 //     console.log(fizzBuzz([7, 9]))
 
+function fizzBuzz (numbers) {
+  const answer = []
 
-
-
-function fizzBuzz (numbers){
-    let answer = []
-
-  for (let index = 0; index < numbers.length; index += 1){
-    if (numbers[index] % 3 === 0 && numbers[index] % 5 === 0){
-      answer.push('fizzBuzz');
-    } else{
-      if (numbers[index] % 3 === 0){
-        answer.push('fizz');
-      } else if (numbers[index] % 5 === 0){
-        answer.push('buzz');
+  for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] % 3 === 0 && numbers[index] % 5 === 0) {
+      answer.push('fizzBuzz')
+    } else {
+      if (numbers[index] % 3 === 0) {
+        answer.push('fizz')
+      } else if (numbers[index] % 5 === 0) {
+        answer.push('buzz')
       } else {
-        answer.push('bug!');
+        answer.push('bug!')
       }
     }
   }
-  return answer;
+  return answer
 }
-console.log(fizzBuzz([2, 15, 7, 9, 45]));
-console.log(fizzBuzz([7, 9]));
-console.log(fizzBuzz([9, 25]));
+console.log(fizzBuzz([2, 15, 7, 9, 45]))
+console.log(fizzBuzz([7, 9]))
+console.log(fizzBuzz([9, 25]))
