addcbfc983265ddd4de249b0b7532c66266be4be
fariasao <enzofarias2010@gmail.com>
2021-08-05 19:23:54 -0300

    desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -43,8 +43,17 @@ function highestCount(arrayNum) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let cat1position = Math.abs(mouse - cat1);
+  let cat2position = Math.abs(mouse - cat2);
+  
+  if(cat1position < cat2position) {
+    return 'cat1';
+  } else if (cat2position < cat1position) {
+    return 'cat2'
+  } else {
+    return 'os gatos trombam e o rato foge'
+  }
 }
 
 // Desafio 8
