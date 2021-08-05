da47f8f41fb9ff8a8b6c37027619e6a9800a0e3d
ArthurMoreira <arthurmcs96@gmail.com>
2021-08-05 18:54:49 -0300

    Resolve  o desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,20 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, name) {
+  if(tech == 0){
+    return console.log('Vazio!')
+  }else{
+    tech.sort();
+    let result = [];
+    for (let index = 0; index < tech.length; index += 1) {
+      result.push({
+        tech: tech[index],
+        name: name,
+      });
+    }
+    return console.log(result);
+  }
 }
+
 
 // Desafio 11
 function generatePhoneNumber() {
