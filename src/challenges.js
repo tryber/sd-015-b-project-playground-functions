188791bf7b508810ad07eff2823e5986fd9e4625
samuel <samuelsoutosilva14@gmail.com>
2021-08-05 20:15:49 -0300

    Requisito 6 - concluido

--- src/challenges.js
+++ src/challenges.js
@@ -1,52 +1,65 @@
 // Desafio 1
 function compareTrue(a, b) {
-  if(a && b === true) {
+  if (a && b === true) {
     return true;
-  } else {
-    return false;
-  }
+  } 
+  return false;
 }
+
+console.log(compateTrue(3,4))
 
 // Desafio 2
 function calcArea(base, height) {
-  AreaTri = (base * height)/2
+  let AreaTri;
+  AreaTri = (base * height) / 2;
   return AreaTri;
 }
 
 // Desafio 3
 function splitSentence(a) {
   let string = a;
-  let array = string.split(" ")
+  let array = string.split(' ');
   return array;
 }
 
 // Desafio 4
 function concatName(array) {
   let ultimo = array[array.length - 1];
-  let primeiro = array[0]
-  let arr = []
-  let space = ' '
-  arr.unshift(ultimo)
-  arr.push(space.concat(primeiro))
-  return(arr + '')
+  let primeiro = array[0];
+  let arr = [];
+  let space = ' ';
+  arr.unshift(ultimo);
+  arr.push(space.concat(primeiro));
+  return arr + '';
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
   let NumVit = 3;
   let NumEmp = 1;
-  QtdPontos = (wins * NumVit) + (ties * NumEmp);
+  let QtdPontos;
+  QtdPontos = wins * NumVit + ties * NumEmp;
   return QtdPontos;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  let contRept = 0;
+  array.sort(function (a, b) {return a - b;})
+  for (let index in array) {
+    if (array[index] === array[array.length - 1]) {
+      contRept+=1;
+    }
+  }
+  return contRept;
 }
+
+  let array = [0, 4, 4, 4, 9, 2, 1]
+  console.log(highestCount(array))
 
 // Desafio 7
 function catAndMouse() {
-  // seu código aqui
+  
 }
 
 // Desafio 8
