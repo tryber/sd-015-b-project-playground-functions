ae7815cbcdd954465db2d864873102ec60cf4b0b
Gabriel Peralta <prates_peralta@hotmail.com>
2021-08-05 11:45:30 -0300

    adiciona a função 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,19 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(list, user) {
+  let orderedList = list.sort();
+  let newObject = [];
+
+  if (list.length === 0) {
+    return 'Vazio!';
+  } else {
+    for (let i = 0; i < list.length; i += 1) {
+      newObject.push({
+        tech: orderedList[i],
+        name: user,
+      });
+    }
+    return newObject;
+  }
 }
 
 // Desafio 11
