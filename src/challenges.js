5b25cc8f1242cca767b5f30e0befd18a43fba408
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2021-08-05 13:06:37 -0300

    exercicio  6 executado

--- src/challenges.js
+++ src/challenges.js
@@ -29,7 +29,7 @@ function splitSentence(frase) {
 // Desafio 4
 function concatName(array) {
   let {length, 0: first, [length-1]: last} = array;
-return last+first;
+  return last+first;
 }
 //console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
@@ -37,14 +37,28 @@ return last+first;
 // Desafio 5
 function footballPoints(wins , ties) {
   let pontos = ((wins*3)+(ties*1));
-return pontos;
+  return pontos;
 }
-console.log(footballPoints(14, 8));
-// Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+//console.log(footballPoints(14, 8));
 
+
+// Desafio 6
+function highestCount(numbers) {
+  let numMaior = 0;
+  let contador = 0;
+    for (let index=0; index < numbers.length; index += 1) {
+      if(numbers[index] > numMaior) {
+      numMaior = numbers[index];
+      }
+    }
+    for (let index=0; index < numbers.length; index += 1) {
+      if (numbers[index] === numMaior) {
+      contador = contador + 1 ;
+      } 
+    }
+  return contador;
+}
+ console.log(highestCount([9, 1, 2, 3, 9, 5, 7]))
 // Desafio 7
 function catAndMouse() {
   // seu código aqui
