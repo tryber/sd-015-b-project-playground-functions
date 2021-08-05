ec97cebb32ac28ed0209f1b4c5403d09a3e1296e
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2021-08-05 14:35:44 -0300

    corrige requisitos

--- src/challenges.js
+++ src/challenges.js
@@ -3,22 +3,26 @@
 function compareTrue() {
    
 }
-
-// Desafio 2 DONE
+// Desafio 2
 function calcArea(base, height) {
   let triangleArea = (base * height) /2
   return triangleArea;
 }
 // console.log('Desafio 2:' , calcArea(10, 50));
-
-// Desafio 3 DONE
+// Desafio 3
+function splitSentence(phrase) {
+  let myString = phrase;
+  let splitedSentence = myString.split(' ');
+     return splitedSentence
+}
+// console.log('Desafio 2:' , calcArea(10, 50));
+// Desafio 3
 function splitSentence(phrase) {
   let myString = phrase;
   let splitedSentence = myString.split(' ');
      return splitedSentence
 }
 // console.log('Desaafio 3' , splitSentence('Go trybe'))
-
 // Desafio 4
 //usar index[0] e index[index.length - 1]
 //invertedArray[0] + invertedArray[invertedArray.length - 1];
@@ -34,11 +38,13 @@ function concatName(array) {
 
   return result;
 } 
-let nomes = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']
+// let nomes = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']
 
-console.log(concatName(nomes))
 
-// Desafio 5 DONE
+// console.log(concatName(nomes))
+
+// Desafio 5
+
 function footballPoints(wins, ties) {
   let victories = 3 * wins;
   let draws = 1 * ties
@@ -47,7 +53,7 @@ function footballPoints(wins, ties) {
 }
 // console.log(footballPoints(1, 2) , 'pontos')
 
-// Desafio 6  DONE
+// Desafio 6
 
 //a função verifica quantas vezes que o  maior numro do array se repete. 
 //portanto preciso: 1- verificar qual o maior numero do array
@@ -65,57 +71,50 @@ function highestCount(numbersArray) {
 }
       return quantasVezesSeRepete;
 }
-// console.log(highestCount([9, 1, 2, 3, 9, 5, 7]))
+let numberisArray = [9, 1, 2, 3, 9, 5, 7];
+console.log(highestCount([9, 1, 2, 3, 9, 5, 7]))
 
 // Desafio 7
-// 
 function catAndMouse(mouse, cat1, cat2) {
-  let resposta = 0;
-  
-  if ( cat1 - mouse === cat2 - mouse || cat1 - mouse ===  mouse - cat2 ||  mouse - cat1 === mouse -cat2 || mouse - cat1 === cat2 - mouse) {
-    resposta = 'os gatos trombam e o rato foge'
-  } else if (cat1 < cat2){
-    resposta = 'cat1'
-  } else { 
-    resposta = 'cat2'
+    let resposta = 0;
+    
+    if ( cat1 - mouse === cat2 - mouse || cat1 - mouse ===  mouse - cat2 ||  mouse - cat1 === mouse -cat2 || mouse - cat1 === cat2 - mouse) {
+      resposta = 'os gatos trombam e o rato foge'
+    } else if (cat1 < cat2){
+      resposta = 'cat1'
+    } else { 
+      resposta = 'cat2'
+    }
+    return resposta
   }
-  return resposta
-}
-
-// console.log(catAndMouse(10, 5, 15));
-// console.log(catAndMouse(0, 3, 2));
-// console.log(catAndMouse(0, 6, 12));
-// console.log(catAndMouse(0, 1, 1))
+  
+  // console.log(catAndMouse(10, 5, 15));
+  // console.log(catAndMouse(0, 3, 2));
+  // console.log(catAndMouse(0, 6, 12));
+  // console.log(catAndMouse(0, 1, 1))
 
 // Desafio 8
-
-// let myArray = [] ; 
-// for (let i = 0; i < numeros.length; i += 1){
-//   if(numeros % 3 === 0 && numeros[i] % 5 === 0){
-//    myArray.push("fizzBuzz")
-//   }else if (numeros[i] % 5 === 0){
-//    myArray.push("buzz")
-//   } else if ( numeros % 3 === 0 ){
-//    myArray.push("fizz")
-//   } else { myArray.push("bug!")}
-// }
-// return myArray
-
-// posso fazer uma função que recebe os parametros que sao os fizzbuzz, buzz, fizz & bug 
-// function fizzBuzz(numeros) {
-//   function 
-// }
+function fizzBuzz(numeros) {
+  let myArray = [] ; 
+  for (let i = 0; i < numeros.length; i += 1){
+    if(numeros % 3 === 0 && numeros[i] % 5 === 0){
+     myArray.push("fizzBuzz")
+    }else if (numeros[i] % 5 === 0){
+     myArray.push("buzz")
+    } else if ( numeros % 3 === 0 ){
+     myArray.push("fizz")
+    } else { myArray.push("bug!")}
+  }
+  return myArray
+}
 // let theArray = [45, 15, 7, 9, 45]
 // console.log(fizzBuzz(theArray))
 
 // Desafio 9
 function encode() {
-
 }
 function decode() {
-
 }
-
 module.exports = {
   calcArea,
   catAndMouse,
@@ -127,4 +126,5 @@ module.exports = {
   footballPoints,
   highestCount,
   splitSentence,
+
 } ;
