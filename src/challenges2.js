263d7a218d7baa59a6113f3eefe40b4999b9cdd0
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-08-05 18:36:45 -0300

    Ajustando o retorno da funcao da questao 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -45,17 +45,17 @@ function generatePhoneNumber(array) {
       } else {
         count = 0
       }
-    } return count
+    } return count;
   }
-  let phoneNumber = "(" + array[0] + array[1] + ")" + array[2] + array[3] + array[4] + array[5] + array[6] + "-" + array[7] + array[8] + array[9] + array[10]
+  let phoneNumber = "(" + array[0] + array[1] + ")" + " " + array[2] + array[3] + array[4] + array[5] + array[6] + "-" + array[7] + array[8] + array[9] + array[10]
   if (check(array) === false) {
     return "Array com tamanho incorreto."
   } else if (check2(array) === false || check3(array) === false) {
     return "não é possível gerar um número de telefone com esses valores"
   } else {
-    return phoneNumber
+    return phoneNumber;
   }
-}; 
+}; console.log (generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
   
 
 
