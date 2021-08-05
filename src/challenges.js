481b09b1823ee3ad574d44de5f4184bd2adc370e
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2021-08-05 19:45:00 -0300

    Adiciona exercicio 11

--- src/challenges.js
+++ src/challenges.js
@@ -23,7 +23,7 @@ function concatName(array) {
 // Desafio 5
 function footballPoints(wins, ties) {
   let totalPoints = (wins * 3) + (ties * 1);
-  return totalPoints; 
+  return totalPoints;
 }
 
 // Desafio 6
@@ -51,7 +51,7 @@ function catAndMouse(mouse, cat1, cat2) {
 
   if (cat1Absoluto > cat2Absoluto) {
     return 'cat2';
-  } else if (cat1Absoluto < cat2Absoluto) {
+  } if (cat1Absoluto < cat2Absoluto) {
     return 'cat1';
   }
   return 'os gatos trombam e o rato foge';
@@ -65,7 +65,7 @@ function fizzBuzz(arrayNumbers) {
   for (let index of arrayNumbers) {
     if (index % 3 === 0 && index % 5 === 0) {
       arrayFizzBuzz.push('fizzBuzz');
-    } else if (index % 3 === 0){
+    } else if (index % 3 === 0) {
       arrayFizzBuzz.push('fizz');
     } else if (index % 5 === 0) {
       arrayFizzBuzz.push('buzz');
@@ -78,46 +78,46 @@ function fizzBuzz(arrayNumbers) {
 
 // Desafio 9
 function encode(str) {
-    let codado = [];
+  let codado = [];
   for (let index = 0; index <= str.length; index += 1) {
-    if (str[index] === "a") {
-      codado.push("1")
-    } else if (str[index] === "e") {
-      codado.push("2");
-    } else if (str[index] === "i") {
-      codado.push("3");
-    }  else if (str[index] === "o") {
-      codado.push("4");
-    }  else if (str[index] === "u") {
-      codado.push("5");
-    }  else {
+    if (str[index] === 'a') {
+      codado.push('1');
+    } else if (str[index] === 'e') {
+      codado.push('2');
+    } else if (str[index] === 'i') {
+      codado.push('3');
+    } else if (str[index] === 'o') {
+      codado.push('4');
+    } else if (str[index] === 'u') {
+      codado.push('5');
+    } else {
       codado.push(str[index]);
     }
   }
-  return(codado.join(''));
+  return (codado.join(''));
 }
-encode("hll");
+encode('hll');
 
 function decode(str) {
   let codado = [];
   for (let index = 0; index <= str.length; index += 1) {
-    if (str[index] === "1") {
-      codado.push("a")
-    } else if (str[index] === "2") {
-      codado.push("e");
-    } else if (str[index] === "3") {
-      codado.push("i");
-    }  else if (str[index] === "4") {
-      codado.push("o");
-    }  else if (str[index] === "5") {
-      codado.push("u");
-    }  else {
+    if (str[index] === '1') {
+      codado.push('a');
+    } else if (str[index] === '2') {
+      codado.push('e');
+    } else if (str[index] === '3') {
+      codado.push('i');
+    } else if (str[index] === '4') {
+      codado.push('o');
+    } else if (str[index] === '5') {
+      codado.push('u');
+    } else {
       codado.push(str[index]);
     }
   }
-  return(codado.join(''));
+  return (codado.join(''));
 }
-decode("h2ll4");
+decode('h2ll4');
 
 module.exports = {
   calcArea,
--- src/challenges2.js
+++ src/challenges2.js
@@ -8,7 +8,7 @@ function techList(arrayTech, name) {
   } { for (let index = 0; index < arr.length; index += 1) {
     arrayTechList.push({
       tech: arr[index],
-      name: name,
+      name,
     });
   }
   }
@@ -16,9 +16,27 @@ function techList(arrayTech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrayNumbers) {
+  if (arrayNumbers.length !== 11) {
+    return ('Array com tamanho incorreto.');
+  }
+  for (let index = 0; index <= arrayNumbers.length; index += 1) {
+    let vezesQueRepete = 0;
+    for (let i = 0; i <= arrayNumbers.length; i += 1) {
+      if (arrayNumbers[index] === arrayNumbers[i]) {
+        vezesQueRepete += 1;
+      } else if (arrayNumbers[i] < 0 || arrayNumbers[i] > 9) {
+        return ('não é possível gerar um número de telefone com esses valores');
+      }
+    }
+    if (vezesQueRepete >= 3) {
+      return ('não é possível gerar um número de telefone com esses valores');
+    }
+  }
+  arrayNumbers = `(${arrayNumbers[0]}${arrayNumbers[1]}) ${arrayNumbers[2]}${arrayNumbers[3]}${arrayNumbers[4]}${arrayNumbers[5]}${arrayNumbers[6]}-${arrayNumbers[7]}${arrayNumbers[8]}${arrayNumbers[9]}${arrayNumbers[10]}`;
+  return arrayNumbers;
 }
+(generatePhoneNumber([9, 2, 3, 0, 5, -6, 7, 8, -7, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
