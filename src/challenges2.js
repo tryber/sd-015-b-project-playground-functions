b800f49968b7d2a27ba846a845876ee6f9619c2f
Wilian Pereira <wilian_pereira92@outlook.com>
2021-08-05 15:37:58 -0300

    Desafio 12: Atualizando o if

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,7 +18,7 @@ function triangleCheck(lineA, lineB, lineC) {
   let somalineBLineC = (lineB + lineC);
   let subtracaoLineBLineC = Math.abs(lineB-lineC);
   let resultado = 0;
-  if (somalineBLineC > lineA  < subtracaoLineBLineC && somaLineALineC > lineB < subtracaoLineALineC && somaLineALineB > lineC < subtracaoLineALineB) {
+  if ((lineA < somalineBLineC && lineA > subtracaoLineBLineC) && (lineB < somaLineALineC && lineB > subtracaoLineALineC) && (lineC < somaLineALineB && lineC > subtracaoLineALineB)){
     resultado = true;
   }
   else {
@@ -26,6 +26,13 @@ function triangleCheck(lineA, lineB, lineC) {
   }
   return resultado;
 }
+console.log(triangleCheck(16, 9, 2))
+console.log(triangleCheck(10, 3, 4))
+console.log(triangleCheck(6, 2, 2))
+console.log("\n"); 
+console.log(triangleCheck(10, 14, 8))
+console.log(triangleCheck(2, 3, 4))
+console.log(triangleCheck(16, 20, 30))
 
 // Desafio 13
 function hydrate() {
