ae16715278c2e732aee3ea4f655e98df9388dac2
Gabrielle-Murat <belle.murat@gmail.com>
2021-08-05 14:48:43 -0300

    Adiciona o requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,35 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function createObjectsArray(array, name) {
+  let newList = [];
+  for (let index = 0; index < array.length; index += 1) {
+    let technology = {
+      tech: array[index],
+      name,
+    };
+    newList.push(technology);
+  }
+  return newList;
+}
+
+function sortArray(technology) {
+  return technology.sort(function (a, b) {
+    if (a.tech > b.tech) {
+      return 1;
+    }
+    if (a.tech < b.tech) {
+      return -1;
+    }
+    return 0;
+  });
+}
+
+function techList(techArray, name) {
+  if (techArray.length < 1) {
+    return 'Vazio!';
+  }
+  let technology = createObjectsArray(techArray, name);
+  let sortedTechs = sortArray(technology);
+  return sortedTechs;
 }
 
 // Desafio 11
