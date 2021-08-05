c42937ea6e633eb4335f20fcd606669062da15d4
Wander Luiz <wander.srv@gmail.com>
2021-08-05 15:35:04 -0300

    REsoluçao do Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,9 +20,38 @@ function techList(tecnologias, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numTelefone){
+  // seu código aqui  
+  if (numTelefone.length != 11){
+    return 'Array com tamanho incorreto.';
+  }
+
+  for (let index = 0; index < numTelefone.length; index += 1){
+    let numerosRepetidos = 0;
+    for (let index2 = 0; index2 < numTelefone.length; index2 += 1){
+      if (numTelefone[index] === numTelefone[index2]){
+        numerosRepetidos += 1;
+      }
+    }
+    if (numTelefone[index] < 0 || numTelefone[index] > 9 || numerosRepetidos >= 3){
+      return 'não é possível gerar um número de telefone com esses valores'
+    }  
+  }
+  let numeroTelefone = '';
+    for (let index = 0; index < numTelefone.length; index += 1){
+      if (index === 0){
+        numeroTelefone += '(' + numTelefone[index];
+      } else if (index === 1){
+        numeroTelefone += numTelefone[index] + ') ';
+      } else if (index === 6){
+        numeroTelefone += numTelefone[index] + '-';
+      } else{
+        numeroTelefone += numTelefone[index];
+      }
+    }
+  return numeroTelefone;
 }
+
 
 // Desafio 12
 function triangleCheck() {
@@ -41,4 +70,4 @@ module.exports = {
   triangleCheck,
 };
 
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Wander'))
+console.log(generatePhoneNumber([1,2,3,4,5,6,7,8,9,0,1]))
