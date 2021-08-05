6c5b4573997f5be856714ba6301979f363a03f0f
jonatan-teixeira <jonatan.teixeira@live.com>
2021-08-05 17:50:47 -0300

    Adicionando requisito 1

---
+++ projeto.js
@@ -0,0 +1,9 @@
+    //1
+    function compareTrue(param1, param2){
+        if (param1 >= 10 && param2 <= 15){
+        return true;
+    } else {
+        return false;    }
+    }
+    compareTrue(10, 10);
+    compareTrue(8, 10);
