572901db1841238d9cd9ddb34ef8341a74fca0c1
Israel Jeronimo <israel.nodejs@gmail.com>
2021-08-05 02:30:38 -0300

    9 - Crie uma função que Codifique e Decodifique

--- src/challenges.js
+++ src/challenges.js
@@ -54,8 +54,11 @@ function highestCount(arr) {
 
 // Desafio 7
 function catAndMouse(gato1, gato2, rato) {
-  let distanciaGato1 = (rato - gato1);
-  let distanciaGato2 = (rato - gato2);
+  let g1 = Math.abs(gato1);
+  let g2 = Math.abs(gato2);
+  let r = Math.abs(rato);
+  let distanciaGato1 = (r - g1);
+  let distanciaGato2 = (r - g2);
   let cat1 = 'cat1';
   let cat2 = 'cat2';
   let trombam = 'os gatos trombam e o rato foge';
@@ -88,28 +91,75 @@ function fizzBuzz(arr) {
     divisivelPor3 = arr[key] % 3 === 0;
     divisivelPor5 = arr[key] % 5 === 0;
   
-  if (divisivelPor3 === true && divisivelPor5 === false) {
+    if (divisivelPor3 === true && divisivelPor5 === false) {
       novoArray.push(fizz);
   
-  } else if (divisivelPor5 === true && divisivelPor3 === false){
+    } else if (divisivelPor5 === true && divisivelPor3 === false) {
       novoArray.push(buzz);
   
-  } else if (divisivelPor5 === true && divisivelPor3 === true){
+    } else if (divisivelPor5 === true && divisivelPor3 === true) {
       novoArray.push(fizzBuzz);
-  } else {
+    } else {
       novoArray.push(bug);
+    }
   }
-  }
-    return novoArray;
-  }
+  return novoArray;
+}
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(text) {
+  let str = text.split('')
+
+  for (const key in str) {
+    switch (str[key]) {
+      case 'a':  
+          str[key] = '1';  
+        break;
+      case 'e':  
+          str[key] = '2';  
+        break;
+      case 'i':  
+          str[key] = '3';  
+        break;
+      case 'o':  
+          str[key] = '4';  
+        break;
+      case 'u':  
+          str[key] = '5';  
+        break;
+    }
+  }
+  str = str.join('');
+  return str;
 }
-function decode() {
-  // seu código aqui
+
+function decode(text) {
+  let str = text.split('')
+
+  for (const key in str) {
+    switch (str[key]) {
+      case '1':  
+          str[key] = 'a';  
+        break;
+      case '2':  
+          str[key] = 'e';  
+        break;
+      case '3':  
+          str[key] = 'i';  
+        break;
+      case '4':  
+          str[key] = 'o';  
+        break;
+      case '5':  
+          str[key] = 'u';  
+        break;
+    }
+  }
+  str = str.join('');
+  return str;
 }
+
+
 
 module.exports = {
   calcArea,
