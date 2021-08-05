37611689dc5a9c2adf235a44985c4d82922cf7c0
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2021-08-05 18:47:53 -0300

    adiciona desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -9,9 +9,17 @@ function generatePhoneNumber () {
 }
 
 // Desafio 12
-function triangleCheck () {
-  // seu código aqui
+// a medida de qqr lado seja menor q a soma dos outros dois e maior q o valor da diferença
+// Math.abs(lineA - lineB)
+
+function triangleCheck (lineA, lineB,lineC) {
+  if(lineA + lineB > lineC && lineC > Math.abs(lineA - lineB) || lineC + lineB > lineA && lineA > Math.abs(lineC - lineB) || lineA + lineC > lineB && lineB > Math.abs(lineA - lineC)){
+    return true;
+  } else {
+    return false;
+  }
 }
+console.log(triangleCheck(10, 150, 8));
 
 // Desafio 13
 function hydrate () {
