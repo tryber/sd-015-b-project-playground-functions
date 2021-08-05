c10d1b7195bf2e03a5e961f1a42417e377907b70
Camilla Del Guerso <camilladelguerso@gmail.com>
2021-08-05 19:23:40 -0300

    Adiciona desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,4 +1,5 @@
 // Desafio 10
+// 10 - Crie uma função de Lista de Tecnologias
 function techList(arrayTech, name) {
   let objTechs;
   let pushArray = [];
@@ -13,8 +14,29 @@ function techList(arrayTech, name) {
   return pushArray;
 }
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+// 11 - Crie uma função de Número de Telefone
+function generatePhoneNumber(arrayNumeros) {
+  let contador = 0;
+  let strigArray = '';
+  if (arrayNumeros.length < 11 || arrayNumeros.length > 11) {
+    return 'Array com tamanho incorreto.';
+  } for (let i in arrayNumeros) {
+    if (arrayNumeros[i] < 0 || arrayNumeros[i] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    } for (let i2 in arrayNumeros) {
+      if (arrayNumeros[i] === arrayNumeros[i2]) {
+        contador += 1;
+        if (contador > 2) {
+          return 'não é possível gerar um número de telefone com esses valores';
+        }
+      }
+    } contador = 0;
+  } arrayNumeros.splice(0, 0, '(');
+  arrayNumeros.splice(3, 0, ')');
+  arrayNumeros.splice(4, 0, ' ');
+  arrayNumeros.splice(10, 0, '-');
+  strigArray = arrayNumeros.join('');
+  return strigArray;
 }
 // Desafio 12
 function triangleCheck() {
