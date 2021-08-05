51aee646400221d2c39fa56da8794c29887955c4
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-08-04 23:21:24 -0300

    Corrigindo mais erros de lint

--- src/challenges2.js
+++ src/challenges2.js
@@ -7,7 +7,7 @@ function techList(array, name) {
     for (let i = 0; i < array.length; i += 1) {
       let obj = {
         tech: array[i],
-        name: name
+        name,
       };
       techArray.push(obj);
     }
@@ -17,32 +17,6 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber(numbers) {
-  // seu código aqui
-  if (numbers.length !== 11) {
-    return 'Array com tamanho incorreto.'
-  } else if (verificationOfPhoneNumber(numbers) === true && verificationOfNumberRepetition(numbers)) {
-    let phoneNumber = [];
-    for (let i = 0; i < numbers.length; i += 1) {
-      if (i === 0) phoneNumber.push('(');
-      else if (i === 2) phoneNumber.push(') ');
-      else if (i === 7) phoneNumber.push('-');
-      phoneNumber.push(numbers[i]);
-    }
-    return phoneNumber.join('');
-  } else {
-    return 'não é possível gerar um número de telefone com esses valores';
-  }
-}
-
-function verificationOfPhoneNumber(numbers) {
-  for (let i = 0; i < numbers.length; i += 1) {
-    if (numbers[i] < 0 || numbers[i] > 9) {
-      return false;
-    }
-  }
-  return true;
-}
 function verificationOfNumberRepetition(numbers) {
   let validNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   for (let i = 0; i < numbers.length; i += 1) {
@@ -59,18 +33,41 @@ function verificationOfNumberRepetition(numbers) {
   }
   return true;
 }
+function verificationOfPhoneNumber(numbers) {
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] < 0 || numbers[i] > 9) {
+      return false;
+    }
+  }
+  return true;
+}
+function generatePhoneNumber(numbers) {
+  // seu código aqui
+  if (numbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  } else if (verificationOfPhoneNumber(numbers) === true && verificationOfNumberRepetition(numbers)) {
+    let phoneNumber = [];
+    for (let i = 0; i < numbers.length; i += 1) {
+      if (i === 0) phoneNumber.push('(');
+      else if (i === 2) phoneNumber.push(') ');
+      else if (i === 7) phoneNumber.push('-');
+      phoneNumber.push(numbers[i]);
+    }
+    return phoneNumber.join('');
+  } else {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+}
+
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
-  if (Math.abs(lineB - lineC) < lineA && lineA < (lineB + lineC) && Math.abs(lineA - lineC) < lineB && lineB < (lineA + lineC) && Math.abs(lineA - lineB) < lineC && lineC < (lineA + lineB)) return true;
-  else return false;
+  return (Math.abs(lineB - lineC) < lineA && lineA < (lineB + lineC) && Math.abs(lineA - lineC) < lineB && lineB < (lineA + lineC) && Math.abs(lineA - lineB) < lineC && lineC < (lineA + lineB));
 }
 
 // Desafio 13
 function hydrate(string) {
-  // seu código aqui
-
   // Peguei essa parte do stack overflow link: https://stackoverflow.com/questions/1623221/how-to-find-a-number-in-a-string-using-javascript/30160994
   let regex = /\d+/g;
   let matches = string.match(regex);
@@ -78,7 +75,7 @@ function hydrate(string) {
   let sum = 0;
   let finalString = '';
 
-  for (number of matches) {
+  for (let number of matches) {
     sum += parseInt(number);
   }
 
