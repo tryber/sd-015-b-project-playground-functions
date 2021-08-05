1985eafa28bb5dceddf0155d0ff8be2dd8b58ed4
Gustavo Gracio <gustavogracio@hotmail.com>
2021-08-05 19:41:21 -0300

    Requisito 10 completo (necessita refatoracao)

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,8 +18,41 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function comparacao(tamanho) {
+  if (tamanho <= 0 || tamanho > 9) {
+    return tamanho;
+  }
+}
+function repete(array) {
+  let objeto = {};
+  for (let key of array) {
+    if (!objeto[key]) {
+      objeto[key] = 1;
+    } else {
+      objeto[key] += 1;
+    }
+    if (objeto[key] >= 3) {
+      return true;
+    }
+  }
+}
+function generatePhoneNumber(numberPhone) {
   // seu código aqui
+  if (numberPhone.length === 11) {
+    let resultadoComparacao = numberPhone.filter(comparacao);
+    if (resultadoComparacao.length >= 1) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    let resultadoRepete = repete(numberPhone);
+    // console.log(resultadoRepete);
+    if (resultadoRepete === true) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    let numberSpace = numberPhone.join('');
+    let resultado = numberSpace.replace(/(\d{0})(\d{2})(\d{5})/, '$1($2) $3-');
+    return resultado;
+  }
+  return 'Array com tamanho incorreto.';
 }
 
 // Desafio 12
