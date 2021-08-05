5f40876a3216b0d99029640fc148630854beb9a9
fariasao <enzofarias2010@gmail.com>
2021-08-05 19:44:52 -0300

    desafio 8

--- src/challenges.js
+++ src/challenges.js
@@ -1,12 +1,12 @@
 // Desafio 1
 function compareTrue(valor1, valor2) {
   let resultado = valor1 === true && valor2 === true;
-  return resultado
+  return resultado;
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  let area = (base*height) / 2;
+  let area = (base * height) / 2;
   return area;
 }
 
@@ -20,7 +20,7 @@ function splitSentence(string) {
 function concatName(arrayNomes) {
   let primeiroNome = arrayNomes[0];
   let ultimoNome = arrayNomes[arrayNomes.length - 1];
-  return ultimoNome + ', ' + primeiroNome;
+  return `${ultimoNome}, ${primeiroNome}`;
 }
 
 // Desafio 5
@@ -34,39 +34,58 @@ function highestCount(arrayNum) {
   let maiorNum = Math.max.apply(null, arrayNum);
   let count = 0;
 
-  for (let index=0; index < arrayNum.length; index +=1) {
+  for (let index = 0; index < arrayNum.length; index += 1) {
     if (arrayNum[index] === maiorNum) {
-      count++
+      count += 1;
     }
   }
-  return count
+  return count;
 }
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   let cat1position = Math.abs(mouse - cat1);
   let cat2position = Math.abs(mouse - cat2);
-  
-  if(cat1position < cat2position) {
+
+  if (cat1position < cat2position) {
     return 'cat1';
-  } else if (cat2position < cat1position) {
-    return 'cat2'
-  } else {
-    return 'os gatos trombam e o rato foge'
+  } if (cat2position < cat1position) {
+    return 'cat2';
   }
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+
+function divisao(numeroArrayFizzBuzz) {
+  let resultado = '';
+  if (numeroArrayFizzBuzz % 3 === 0 && numeroArrayFizzBuzz % 5 === 0) {
+    resultado = 'fizzBuzz';
+  } else if (numeroArrayFizzBuzz % 3 === 0) {
+    resultado = 'fizz';
+  } else if (numeroArrayFizzBuzz % 5 === 0) {
+    resultado = 'buzz';
+  } else {
+    resultado = 'bug!';
+  }
+  return resultado;
+}
+
+function fizzBuzz(arrayFizzBuzz) {
+  let array = [];
+
+  for (let index = 0; index < arrayFizzBuzz.length; index += 1) {
+    array.push(divisao(arrayFizzBuzz[index]));
+  }
+  return array;
 }
 
 // Desafio 9
 function encode() {
-  // seu código aqui
+
 }
 function decode() {
-  // seu código aqui
+
 }
 
 module.exports = {
