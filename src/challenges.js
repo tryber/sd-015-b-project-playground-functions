b0e7e0f2f283c59342b0ee146df07ff0ff8a0105
vinicius rampazzo <vinicius_rampazzo10@hotmail.com>
2021-08-05 18:26:12 -0300

    arrumando erros

--- src/challenges.js
+++ src/challenges.js
@@ -1,32 +1,31 @@
 // Desafio 1
 function compareTrue(a, b) {
- 
- if (a === true && b === true) {
-   return true;
- }
- else {
-   return false;
- }
+  if (a === true && b === true) {
+    return true;
+  }
+  else {
+    return false;
+  }
 } 
 // console.log(compareTrue(1 === 1, 2 === 2));
-
-
+// 
+// 
 // Desafio 2
 function calcArea(base, height) {
   const resultado = (base * height) / 2;
   return resultado;
 }
 // console.log(calcArea(51, 1));
-
-
+// 
+// 
 // Desafio 3
 function splitSentence(string) {
   const resultado = string.split(' ')
   return resultado;
 }
 // console.log(splitSentence('Go Trybe'))
-
-
+// 
+// 
 // Desafio 4
 function concatName(array) {
   let resposta1 = array[array.length-1];
@@ -35,9 +34,9 @@ function concatName(array) {
   return resultado;
 }
 // console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
-
-
-
+// 
+// 
+// 
 // Desafio 5
 function footballPoints(wins, ties) {
   let valorWins = wins * 3;
@@ -46,9 +45,9 @@ function footballPoints(wins, ties) {
   return valorWins + valorTies;
 }
 // console.log(footballPoints(14, 8));
-
-
-
+// 
+// 
+// 
 // Desafio 6
 function highestCount(array) {
   let resultado = Math.max.apply(null, array);
@@ -61,12 +60,11 @@ function highestCount(array) {
   return soma;
 }
 // console.log(highestCount([1, 9, 2, 3, 9, 5, 7]));
-
-
-
+// 
+// 
+// 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-
   if (mouse === 1 && cat1 === 0 && cat2 === 2) {
     return 'os gatos trombam e o rato foge'
   }
@@ -78,13 +76,13 @@ function catAndMouse(mouse, cat1, cat2) {
   }
 }
 // console.log(catAndMouse(1, 0, 2))
-
-
-
+// 
+// 
+// 
 // Desafio 8
 function fizzBuzz(array) {
-  let acumulador = [];
-  
+  let acumulador = []; 
+
   for (let i = 0; i < array.length; i +=1) {    
     if (array[i] % 15 === 0) {
       acumulador.push("fizzBuzz");
@@ -102,8 +100,10 @@ function fizzBuzz(array) {
   return acumulador;
 }
 // console.log(fizzBuzz([2, 15, 7, 9, 45]))
-
-
+// 
+// 
+// 
+// 
 // Desafio 9
 function encode(str) {
 
@@ -133,8 +133,9 @@ function encode(str) {
   }
       return acumulador.join('')
 }
-  // console.log(encode('hello'));
-  
+// console.log(encode('hello'));
+// 
+// 
 function decode(str) {
   let acumulador = [];
 
@@ -162,8 +163,8 @@ function decode(str) {
   }
       return acumulador.join('')
 }
-  console.log(decode('h2ll4'))
-
+// console.log(decode('h2ll4'))
+// 
 module.exports = {
   calcArea,
   catAndMouse,
