4aa2554f861c836aedac53e60220643a3f0ab0ad
gustavogss <gustavogss.dev@gmail.com>
2021-08-05 13:23:00 -0300

    requisito 11

--- src/challenges.js
+++ src/challenges.js
@@ -62,21 +62,20 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  let fizzBuzz = [];
-  for (let index of fizzBuzz) {
-    if ((index % 3) === 0 && (index % 5) === 0) {
+function fizzBuzz(fizzBuzz) {
+  for (let i=0; i<fizzBuzz.length; i+=1) {
+    if ((fizzBuzz[i] % 3) === 0 && (fizzBuzz[i] % 5) === 0) {
       fizzBuzz.push('fizzBuzz');
-    } else if ((index % 3) === 0) {
+    } else if ((fizzBuzz[i] % 3) === 0) {
       fizzBuzz.push('fizz');
-    } else if ((index % 5) === 0) {
+    } else if ((fizzBuzz[i] % 5) === 0) {
       fizzBuzz.push('buzz');
     } else {
       fizzBuzz.push('bug!');
     }
   }
   return fizzBuzz;
-}
+}console.log(fizzBuzz[2, 15, 7, 9, 45]);
 
 // Desafio 9
 function encode(str) {
--- src/challenges2.js
+++ src/challenges2.js
@@ -17,11 +17,36 @@ function techList(array, name) {
 } console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"],
 "Lucas"));
 
-
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+function generatePhoneNumber(array) {
+  if(array.length != 11){
+    return 'Array com tamanho incorreto.';
+  }
+  let count = 0;
+  let error = false;
+
+  for (let i = 0; i < array.length; i += 1){
+    if (array[i] > 9 || array[i] < 0){
+      error = true;
+      break;
+    }
+    count = 0;
+    for(let numbers in array){
+      if(array[numbers] === array[i]){
+        count += 1;
+      }
+    }
+    if (count >= 3){
+      error = true;
+      break;
+    }    
+  }
+  if(error){
+    return 'não é possível gerar um número de telefone com esses valores'
+  }
+  return '('+array[0]+array[1]+') '+array[2]+array[3]+array[4]+array[5]+array[6]+'-'+array[7]+array[8]+array[9]+array[10];
+} console.log (generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
