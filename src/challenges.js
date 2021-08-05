5fd0b16d3d8c3590929ac4151feb55665c4f9617
vinicius rampazzo <vinicius_rampazzo10@hotmail.com>
2021-08-05 15:04:16 -0300

    realizando requisito 8

--- src/challenges.js
+++ src/challenges.js
@@ -82,9 +82,27 @@ function catAndMouse(mouse, cat1, cat2) {
 
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let acumulador = [];
+  
+  for (let i = 0; i < array.length; i +=1) {    
+    if (array[i] % 15 === 0) {
+      acumulador.push("fizzBuzz");
+    }
+    else if (array[i] % 5 === 0){
+      acumulador.push("buzz");
+    }
+    else if (array[i] % 3 === 0) {
+      acumulador.push("fizz");
+    }
+    else {
+      acumulador.push("bug!");
+    }
+  } 
+  return acumulador;
 }
+console.log(fizzBuzz([2, 15, 7, 9, 45]))
+
 
 // Desafio 9
 function encode() {
