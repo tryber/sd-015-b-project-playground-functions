4ed1cf5082b54beb36d82cf18949db0e1788c46a
Robert Martins <robert.rm131@gmail.com>
2021-08-06 18:39:49 -0300

    desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -10,13 +10,54 @@ function techList(lista, name) {
   }
   return "Vazio!";
 }
-  
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function verOcorrencias(n) {
+  let cont = 0;
+  for (let i = 0; i < n.length; i++) {
+      cont = n.split(n[i]).length - 1;
+      if(cont >= 3) {
+        return "não é possível gerar um número de telefone com esses valores";
+      }
+  }
+  return 2;
 }
 
+function verTamanhos(n) {
+  if(n.length === 11) {
+    return 2;
+  }
+  return 0; 
+}
+
+function verMaiorMenor(n) {
+  let resultado = 0;
+  console.log(n);
+  if((Math.max(...n) > 9) || (Math.min(...n) < 0)) {
+    resultado = 1;
+  } else {
+    resultado = 2;
+  }
+  return resultado;
+}
+function generatePhoneNumber(numeros) {
+  let resultado = "";
+  resultado = verTamanhos(numeros);
+  if (resultado === 0) {
+    return "Array com tamanho incorreto.";
+  }
+  resultado = verMaiorMenor(numeros);
+  if (resultado === 1) {
+    return "não é possível gerar um número de telefone com esses valores"
+  }
+  let listaArrumada = numeros.join("");
+  resultado = verOcorrencias(listaArrumada);
+  if(resultado === 2) {
+    resultado = `(${listaArrumada.slice(0,2)}) ${listaArrumada.slice(2,7)}-${listaArrumada.slice(7)}`; 
+  } 
+  return resultado; 
+}
+  
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
