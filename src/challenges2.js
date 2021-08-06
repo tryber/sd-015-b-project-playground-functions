937f8f2bee6d9757b77818d829f0c1252222cccf
ArthurMoreira <arthurmcs96@gmail.com>
2021-08-06 15:46:17 -0300

    Resolve o desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,8 @@
 // Desafio 10
 function techList(tech, name) {
-  if(tech == 0){
-    return 'Vazio!'
-  }else{
+  if (tech.length === 0) {
+    return 'Vazio!';
+  } else {
     tech.sort();
     let result = [];
     for (let index = 0; index < tech.length; index += 1) {
@@ -15,10 +15,32 @@ function techList(tech, name) {
   }
 }
 
-
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbers) {
+  if (numbers.length != 11) {
+    return 'Array com tamanho incorreto';
+  }
+
+  for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] < 0 || numbers[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    let repeat = 1;
+    for (let cont = index + 1; cont < numbers.length; cont += 1) {
+      if (numbers[index] === numbers[cont]) {
+        repeat += 1;
+        if (repeat >= 3) {
+          return 'não é possível gerar um número de telefone com esses valores';
+        }
+      }
+    }
+  }
+  let format = numbers.join('');
+  let prefix = format.slice(0, 2);
+  let firstPart = format.slice(2, 7);
+  let secondPart = format.slice(7, 12);
+  let phoneNumber = '(' + prefix + ')' + firstPart + '-' + secondPart;
+  return phoneNumber;
 }
 
 // Desafio 12
