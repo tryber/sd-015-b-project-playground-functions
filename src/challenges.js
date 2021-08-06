e7d48bcc9228a730866e4e3ad67a26ea7fcdb0aa
daydreamer10 <fcostalex.dev@gmail.com>
2021-08-06 01:40:16 -0300

    subindo desafios prontos para o github

--- src/challenges.js
+++ src/challenges.js
@@ -26,44 +26,135 @@ function splitSentence(frase) {
 }
 
 // Desafio 4
-names = ['joão', 'paulo', 'daniel'];
+let names = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
+primeiroItem = []
+ultimoItem = []
+together = []
 
 function concatName(names) {
   // seu código aqui
-  let primeiroItem =""
-  for (let name in names) {
-    primeiroItem = primeiroItem + names[name[0]];
-    return(primeiroItem);    
-  }
-  let ultimoItem = ""
-  for (let name in names) {
-    let ultimoItem = ultimoItem + names[name[1]];
-    return ultimoItem;
-  }
-  }
+ for (let name in names) {
+   primeiroItem = names[0];
+ }
+ //console.log(primeiroItem);
+ for (let name in names) {
+   ultimoItem = names[names.length - 1];
+ }
+ together = ultimoItem + ',' + ' ' + primeiroItem;
 
-console.log(concatName(names));
+ 
+ //console.log(ultimoItem);
+ return together;
+}
 
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  let pontuacao = (wins * 3) + ties;
+  return pontuacao;
 }
+//console.log(footballPoints(14, 8));
 
 // Desafio 6
-function highestCount() {
+let numeros = [9, 1, 2, 3, 9, 5, 9];
+function highestCount(numeros) {
   // seu código aqui
+  let maiorNumero = 0;
+  let cont = 0;
+  for (let n of numeros) {
+    if(n > maiorNumero) {
+      maiorNumero = n;
+    }     
+  }
+  for (let j of numeros) {
+    if (j === maiorNumero) {
+      cont += 1;
+    }
+  }
+  return cont;     
 }
+//console.log (highestCount(numeros)) 
+
+
+
+    
+ 
+  
+//}
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+  let distmc1 = mouse - cat1;
+  let distmc2 = mouse - cat2;
+  
+  if (distmc1 < distmc2) {
+    console.log("cat1");
+  } else if (distmc2 < distmc1) {
+    console.log("cat2");
+  } else {
+    console.log("os gatos trombam e o rato foge.");
+  }
+  return catAndMouse;
 }
 
+
 // Desafio 8
-function fizzBuzz() {
+let numeros = [2, 15, 7, 9, 45]
+function fizzBuzz(numeros) {
   // seu código aqui
+
+ 
+
+console.log(fizzBuzz(numeros));
+
+
+
+
+  
+
+
+    if (numeros[n] % 5 === 0) {
+      console.log('buzz')
+    }
+    if (numeros[n] % 3 === 0 && numeros[n] % 5 === 0) {
+      console.log('fizzbuzz')
+    }
+  }
+    
+    
+    
+    else if (numeros[n] % 5 === 0) {
+      console.log('buzz');
+    }
+    }
+
+  }
+    
+    
+    
+    else if {
+      (numeros[n] % 5 === 0) {
+      console.log('buzz')
+    }
+  }
 }
+   
+  
+  
+
+   else if (numeros % 5 === 0) {
+      console.log('buzz');
+    } else if (numeros % 3 === 0 && numeros % 3 === 5) {
+      console.log('fizzbuzz');
+    } else {
+      console.log('bug!');
+    }
+  }
+  return fizzBuzz;
+}
+console.log(fizzBuzz)
 
 // Desafio 9
 function encode() {
