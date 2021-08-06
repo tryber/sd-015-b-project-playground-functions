9ae8de55a48d30e75bb7e2aaca092befd5695938
Walace <walacewab@gmail.com>
2021-08-06 20:41:36 -0300

    Desafio 12 triangleCheck

--- src/challenges2.js
+++ src/challenges2.js
@@ -6,7 +6,6 @@ function techList(range, name) {
   if (range.length === 0){
     return "Vazio!";
   }
-
   range.sort();
   for (let tech of range){
     afterList.push({ tech, name });
@@ -20,10 +19,18 @@ function generatePhoneNumber(numbers) {
 }
 
 // Desafio 12
-function triangleCheck() {
+function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
+  if (lineA < lineB + lineC && lineA > Math.abs(lineB - lineC)){
+    return true;
+} else if (lineB < lineA + lineC && lineB > Math.abs(lineA - lineC)){
+    return true;
+} else if (lineC < lineA + lineB && lineC > Math.abs(lineA - lineB)){
+    return true;
+} else {
+  return false;
 }
-
+}
 // Desafio 13
 function hydrate() {
   // seu código aqui
