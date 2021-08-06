1c1a90ccc6aa676b6bbd66e4c6f54c6c1384a3a5
Adriana <adrianam754@gmail.com>
2021-08-05 23:00:57 -0300

    Correçao exercicio 01 ao 09

--- src/challenges.js
+++ src/challenges.js
@@ -6,7 +6,6 @@ function compareTrue(a, b) {
     return false;
   }
 }
-compareTrue(a, b);
 
 // Desafio 2
 function calcArea(base, height) {
@@ -15,18 +14,10 @@ function calcArea(base, height) {
 calcArea(10, 50);
 
 // Desafio 3
-<<<<<<< HEAD
 function splitSentence(phrase) {
   return phrase.split(' ');
-=======
-function splitSentence(phrase, str) {
-  str = ' ';
-  phrase = str.split(' ');
-  return phrase;
->>>>>>> f87318910ee0a22d66d0622c37a15e2c956e94b2
 }
 // Desafio 4
-<<<<<<< HEAD
 function concatName(list) {
   start = list[0];
   final = list[list.length - 1];
@@ -36,22 +27,6 @@ function concatName(list) {
 // Desafio 5
 function footballPoints(wins, ties) {
   total = wins * 3 + ties;
-=======
-function concatName(array) {
-  let list = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
-
-  let start = list[0];
-  let final = list[list.length - 1];
-  let array = [final, start];
-  return array;
-}
-
-// Desafio 5
-function footballPoints(total) {
-  let wins = 14;
-  let ties = 8;
-  let total = wins * 3 + ties * 1;
->>>>>>> f87318910ee0a22d66d0622c37a15e2c956e94b2
   return total;
 }
 
@@ -74,7 +49,6 @@ function highestCount(array) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-<<<<<<< HEAD
   if (cat1 < cat2) {
     return 'cat1';
   }
@@ -84,28 +58,9 @@ function catAndMouse(mouse, cat1, cat2) {
     return 'os gatos trombam e o rato foge';
   }
 }
-=======
-  cat1 = 6;
-  cat2 = 12;
-  mouse = 0;
->>>>>>> f87318910ee0a22d66d0622c37a15e2c956e94b2
 
-  if (cat1 < cat2) {
-    return 'Gato 1';
-  } else if (cat2 < cat1) {
-    return 'Gato 2';
-  } else {
-    return 'Os gatos se trombam';
-  }
-}
 // Desafio 8
-<<<<<<< HEAD
 function lista(array) {
-=======
-function fizzBuzz(array) {
-  array = [64];
-
->>>>>>> f87318910ee0a22d66d0622c37a15e2c956e94b2
   if (array % 3 === 0 && array % 5 === 0) {
     return 'fizzBuzz';
   }
@@ -115,7 +70,6 @@ function fizzBuzz(array) {
   if (array % 5 === 0) {
     return 'buzz';
   } else {
-<<<<<<< HEAD
     return 'bug!';
   }
 }
@@ -126,18 +80,10 @@ function fizzBuzz(arrayFizzBuzz) {
     array.push(lista(arrayFizzBuzz[index]));
   }
   return array;
-=======
-    return 'bug';
-  }
->>>>>>> f87318910ee0a22d66d0622c37a15e2c956e94b2
 }
 
 // Desafio 9
 function encode(phrase, replace) {
-<<<<<<< HEAD
-=======
-  phrase = ' um dia lindo';
->>>>>>> f87318910ee0a22d66d0622c37a15e2c956e94b2
   return phrase
     .replace(/a/g, '1')
     .replace(/e/g, '2')
@@ -146,10 +92,6 @@ function encode(phrase, replace) {
     .replace(/u/g, '5');
 }
 function decode(phrase2, replace) {
-<<<<<<< HEAD
-=======
-  phrase2 = 'h3 th2r2!';
->>>>>>> f87318910ee0a22d66d0622c37a15e2c956e94b2
   return phrase2
     .replace(/1/g, 'a')
     .replace(/2/g, 'e')
