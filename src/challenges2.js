f8638a9c3cc432a3ca7b08d0ab8954f4d9d56f18
DanielHott <daniel_hott@outlook.com>
2021-08-06 20:03:43 -0300

    Adicionando elementos no desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,18 +1,23 @@
 // Desafio 10
-function techList() {    
-
-}
-techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Lucas')
+function techList()
 // Desafio 11
 function generatePhoneNumber(array) {
-  if(array.length === 10){
-  }
-  // seu código aqui
+      let formato = "(xx) xxxxx-xxxx";
+      let numeros = 0;
+      for(let i = 0; i < array.length; i++){
+      formato = formato.replace('x', array[i]);
+      if (array[i] < 0 || array[i] > 9){
+        return 'não é possível gerar um número de telefone com esses valores'
+      } console.log(numeros)
+    } if(array.length !== 11){
+      return 'Array com tamanho incorreto'
+  } 
+   return formato;
 }
-
+console.log(generatePhoneNumber([1, 2, 2, 2, 5, 4, 7, 8, 9, 0, 5]))
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+    if(lineA > (b)){}
 }
 
 // Desafio 13
@@ -20,9 +25,9 @@ function hydrate(string) {
   let text = ( string.replace(/[^\d]+/g, ' ') );
   let numeros = text.split('');
   let soma = 0
-  for(let i = 0; i < numeros.length; i++){
-    for(let j = 0; j < numeros[i]; j++) {
-      soma ++
+  for(let i = 0; i < numeros.length; i+=1){
+    for(let j = 0; j < numeros[i]; j+=1) {
+      soma += 1;
     }
   } if (soma != 1){
     return soma + " copos de água"
