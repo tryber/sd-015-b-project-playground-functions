49fde50b7d6339844b8bb5f715a7a1849ad47417
Andre Luiz <andreluizgyn1@gmail.com>
2021-08-06 19:51:29 -0300

    Feito exercicios 5 e 6

--- src/challenges.js
+++ src/challenges.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 // Desafio 1
 function compareTrue(n, n2) {
   if (n && n2 === true) {
@@ -26,13 +27,27 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-
+function footballPoints(wins, ties) {
+  let pontos = (wins * 3) + ties;
+  return pontos;
 }
 
-// Desafio 6
-function highestCount() {
+function highestCount(num) {
+  // verificar qual o maior número da array e retornar quantas vezes ele se repete.
+  // let test;
+  let repeticaoDoMaiorNumero = 0;
+  let maiorNumero = num[0];
+  for (let index = 0; index < num.length; index += 1) {
+    if (num[index] > maiorNumero) {
+      maiorNumero = num[index];
+      repeticaoDoMaiorNumero = 0;
+    }
+    if (num[index] === maiorNumero) {
+      repeticaoDoMaiorNumero += 1;
+    }
+  }
 
+  return repeticaoDoMaiorNumero;
 }
 
 // Desafio 7
