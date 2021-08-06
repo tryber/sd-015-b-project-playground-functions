cb5bdfdba0ce74dde227fdf8772a8fd9c91f2f96
Wilian Pereira <wilian_pereira92@outlook.com>
2021-08-05 22:08:09 -0300

    Desafio 11: Funçao numero de telefone

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,8 +4,29 @@ function techList() {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(numbers) {
   // seu código aqui
+  let contador = 0;
+  let mensagemAlerta = 0;
+  let tamanhoIncorreto = 0;
+  let transformarArrayEmString = numbers.join('');
+  let ddd = transformarArrayEmString.substr(0,2);
+  let primeiraParteNumero = transformarArrayEmString.substr(2,5);
+  let segundaParteNumero = transformarArrayEmString.substr(7,4);
+  if (numbers.length !== 11) {
+    return tamanhoIncorreto = 'array com tamanho incorreto.';
+  }
+  for (let index = 0; index < numbers.length; index += 1) {
+    for(let i = index + 1; i <= numbers.length; i += 1) {
+      if (numbers[index] === numbers[i]) {
+        contador += 1;
+      }
+      if (numbers[index] < 0 || numbers[index] > 9 || contador >= 3) {
+      return mensagemAlerta = 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+  }
+  return `(${ddd}) ${primeiraParteNumero}-${segundaParteNumero}`
 }
 
 // Desafio 12
@@ -16,7 +37,7 @@ function triangleCheck(lineA, lineB, lineC) {
   let somaLineALineC = (lineA + lineC);
   let subtracaoLineALineC = Math.abs(lineA - lineC);
   let somalineBLineC = (lineB + lineC);
-  let subtracaoLineBLineC = Math.abs(lineB-lineC);
+  let subtracaoLineBLineC = Math.abs(lineB - lineC);
   let resultado = 0;
   if ((lineA < somalineBLineC && lineA > subtracaoLineBLineC) && (lineB < somaLineALineC && lineB > subtracaoLineALineC) && (lineC < somaLineALineB && lineC > subtracaoLineALineB)){
     resultado = true;
