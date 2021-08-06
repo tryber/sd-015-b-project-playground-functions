5d4f0b635050ace185a250c88015e29f7fe9b106
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-08-06 01:23:20 -0300

    Refatorado exercicios de challenges

--- src/challenges.js
+++ src/challenges.js
@@ -48,91 +48,82 @@ function highestCount(valor) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let position = 0;
-  let position2 = 0;
-  if (mouse > cat1 && mouse > cat2) {
-    position = mouse - cat1;
-    position2 = mouse - cat2;
-  } else if (mouse < cat1 && mouse < cat2) {
-    position = cat1 - mouse;
-    position2 = cat2 - mouse;
-  } else if (mouse < cat1 && mouse > cat2) {
-    position = cat1 - mouse;
-    position2 = mouse - cat2;
-  } else {
-    position = mouse - cat1;
-    position2 = cat2 - mouse;
-  }
-
+  let position = Math.abs(mouse - cat1);
+  let position2 = Math.abs(mouse - cat2);
+  let output;
   if (position === position2) {
-    return 'os gatos trombam e o rato foge';
+    output = 'os gatos trombam e o rato foge';
   } if (position < position2) {
-    return 'cat1';
+    output = 'cat1';
   } if (position2 < position) {
-    return 'cat2';
+    output = 'cat2';
   }
+  return output;
 }
 
 // Desafio 8
+function divisionFiveAndThree(number) {
+  if (number % 3 === 0 && number % 5 === 0) {
+    return true;
+  }
+}
+
+function divisionThree(number) {
+  if (number % 3 === 0 && number % 5 !== 0) {
+    return true;
+  }
+}
+
+function divisionFive(number) {
+  if (number % 5 === 0 && number % 3 !== 0) {
+    return true;
+  }
+}
+
+function notDivisibleFiveAndThree(number) {
+  if (number % 3 !== 0 && number % 5 !== 0) {
+    return true;
+  }
+}
+
+function checkDivisiblesAndNot(number) {
+  let output;
+  if (divisionFiveAndThree(number)) {
+    output = 'fizzBuzz';
+  }
+  if (divisionThree(number)) {
+    output = 'fizz';
+  }
+  if (divisionFive(number)) {
+    output = 'buzz';
+  }
+  if (notDivisibleFiveAndThree(number)) {
+    output = 'bug!';
+  }
+  return output;
+}
+
 function fizzBuzz(array) {
   let fizbuz = [];
   for (let show of array) {
-    if (show % 3 === 0 && show % 5 === 0) {
-      fizbuz.push('fizzBuzz');
-    } else if (show % 3 === 0) {
-      fizbuz.push('fizz');
-    } else if (show % 5 == 0) {
-      fizbuz.push('buzz');
-    } else {
-      fizbuz.push('bug!');
-    }
+    fizbuz.push(checkDivisiblesAndNot(show));
   }
   return fizbuz;
 }
 
 // Desafio 9
 function encode(valor) {
-  let split = valor.split('');
-  let juncao2 = [];
-  let retorno = '';
-  for (let show of split) {
-    if (show === 'a') {
-      juncao2.push(1);
-    } else if (show === 'e') {
-      juncao2.push(2);
-    } else if (show === 'i') {
-      juncao2.push(3);
-    } else if (show === 'o') {
-      juncao2.push(4);
-    } else if (show === 'u') {
-      juncao2.push(5);
-    } else {
-      juncao2.push(show);
-    }
-  }
-  return juncao2.join('', retorno);
+  let out;
+  out = valor.replace(/a/g, 1).replace(/e/g, 2).replace(/i/g, 3).replace(/o/g, 4)
+    .replace(/u/g, 5);
+  return out;
 }
 
 function decode(valor) {
-  let split = valor.split('');
-  let juncao = [];
-  let retorno = '';
-  for (let show of split) {
-    if (show === '1') {
-      juncao.push('a');
-    } else if (show === '2') {
-      juncao.push('e');
-    } else if (show === '3') {
-      juncao.push('i');
-    } else if (show === '4') {
-      juncao.push('o');
-    } else if (show === '5') {
-      juncao.push('u');
-    } else {
-      juncao.push(show);
-    }
-  }
-  return juncao.join('', retorno);
+  let out;
+  out = valor.replace(/1/g, 'a').replace(/2/g, 'e').replace(/3/g, 'i').replace(/4/g, 'o')
+    .replace(/5/g, 'u');
+  return out;
 }
 
 module.exports = {
