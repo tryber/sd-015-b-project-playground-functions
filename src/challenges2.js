6f1cdfda69cac26db8f20b43382843c2947a8c6d
Robert Martins <robert.rm131@gmail.com>
2021-08-06 15:43:36 -0300

    desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,16 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(lista, name) {
+  let tecnologias = lista.sort();
+  let novoObjeto = [];
+  if(lista.length) {
+    for (let i = 0; i < lista.length; i++) {
+      novoObjeto.push({tech: tecnologias[i], name: name});
+    }
+    return novoObjeto;
+  }
+  return "Vazio!";
 }
+  
 
 // Desafio 11
 function generatePhoneNumber() {
