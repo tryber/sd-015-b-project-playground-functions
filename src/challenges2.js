5e5ac413898246620a64a832b056efd82f622a69
Ulisses Roque <ulissesroque@protonmail.com>
2021-08-06 17:25:37 -0300

    Ajusta desafio 13 ao lint

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,9 +17,9 @@ function hydrate(sentence) {
   let counter = 0;
   const arr = sentence.match(regex);
 
-  for (let index in arr) {
-    arr[index] = parseFloat(arr[index]);
-    counter += arr[index];
+  for (let value of arr) {
+    value = parseFloat(value);
+    counter += value;
   } if (counter === 1) {
     return '1 copo de água';
   } return `${counter} copos de água`
