276e7d0972bb2824e2cb977489175bf21ffe243c
Nicolas Franzolin <nicolas.franzolin@gmail.com>
2021-08-07 15:28:23 -0300

    Adiciona Requisito 12 provisorio

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,13 +18,25 @@ function techList(techsToLearn, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(phoneNumberList) {
   // seu código aqui
+
 }
 
 // Desafio 12
-function triangleCheck() {
+function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
+  let contador = 0;
+  if (lineA < (lineB + lineC) && lineA > Math.abs(lineB - lineC)) {
+    return true;
+  } else if (lineB < (lineA + lineC) && lineB > Math.abs(lineA - lineC) ) {
+    return true;
+  } else if (lineC < (lineA + lineB) && lineC > Math.abs(lineA - lineB)) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
 
 // Desafio 13
