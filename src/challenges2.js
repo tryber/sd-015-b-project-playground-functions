a96973669b9e36fd323b8f8f9bd3ae80182835e7
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-08-07 16:37:56 -0300

    Resolve requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -8,14 +8,30 @@ function techList(tech, name) {
   for (let itemList of tech) {
     techLists.push({  //.push() para acrescentar os novos objetos na lista.
       tech: itemList,
-      name: name, }); 
+      name: name,
+    });
   }
-  return techLists
+  return techLists;
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbers) {
+  let phoneNumberConcat = '(' + numbers[0] + numbers[1] + ') ' + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + '-' + numbers[7] + numbers[8] + numbers[9] + numbers[10];
+  if (numbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (num of numbers) {
+    let count = 0;
+    for (let second = 0; second < numbers.length; second += 1) {
+      if (num === numbers[second]) {
+        count += 1;
+      }
+      if (count >= 3 || num < 0 || num > 9) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+  }
+  return phoneNumberConcat;
 }
 
 // Desafio 12
