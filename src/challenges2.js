60e5b9dce3bc8544afba37ab9abf2a223df6fa17
Sayro <sayrodamaceno70@gmail.com>
2021-08-07 02:58:10 -0300

    adiciona desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,16 +18,57 @@ function createObjects(ordem, names) {
   return verifVazio(array);
 }
 
-
-
 function techList(tech, names) {
   let ordem = tech.sort();
   return createObjects(ordem, names);
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function numberRepet(array) {
+  let cont2 = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    let cont = 0;
+    for (let j = 0; j < array.length; j += 1) {
+      if (array[i] === array[j]) {
+        cont += 1;
+      }
+    }
+    if (cont > cont2) {
+      cont2 = cont;
+    }
+  }
+  return cont2;
+}
+
+function verificaNumeros(numbers) {
+  for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] < 0 || numbers[index] > 9 || numberRepet(numbers) > 2) {
+      return true;
+    }
+  }
+}
+
+function organization(numbers) {
+  let primeiros = [];
+  primeiros.push(`(${numbers[0]}${numbers[1]})`);
+  primeiros.push(' ');
+  primeiros.push(`${numbers[2]}${numbers[3]}${numbers[4]}${numbers[5]}${numbers[6]}`);
+  primeiros.push(`-${numbers[7]}${numbers[8]}${numbers[9]}${numbers[10]}`);
+  return primeiros;
+}
+
+function joinNumbers(numbers) {
+  return organization(numbers).join('');
+}
+
+function generatePhoneNumber(numbers) {
+  if (numbers.length < 11 || numbers.length > 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  if (verificaNumeros(numbers)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  return joinNumbers(numbers);
 }
 
 // Desafio 12
--- src/index.js
+++ src/index.js
@@ -1,27 +1,55 @@
-// Desafio 10
-
-function createObjects(ordem, names) {
-  let array = [];
-  for (let index = 0; index < ordem.length; index += 1) {
-    let objects = {
-      tech: ordem[index],
-      name: names,
-    };
-    array.push(objects);
+// Desafio 11
+function numberRepet(array) {
+  let cont2 = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    let cont = 0;
+    for (let j = 0; j < array.length; j += 1) {
+      if (array[i] === array[j]) {
+        cont += 1;
+      }
+    }
+    if (cont > cont2) {
+      cont2 = cont;
+    }
   }
-  return verifVazio(array)
+  return cont2;
 }
 
-function verifVazio(array) {
-  if (array.length > 0) {
-    return array;
+function verificaNumeros(numbers) {
+  for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] < 0 || numbers[index] > 9 || numberRepet(numbers) > 2) {
+      return true;
+    }
   }
-  return 'Vazio!';
 }
 
-function techList(tech, names) {
-  let ordem = tech.sort();
-  return createObjects(ordem, names);
+function organization(numbers) {
+  let primeiros = [];
+  primeiros.push(`(${numbers[0]}${numbers[1]})`);
+  primeiros.push(' ');
+  primeiros.push(`${numbers[2]}${numbers[3]}${numbers[4]}${numbers[5]}${numbers[6]}`);
+  primeiros.push(`-${numbers[7]}${numbers[8]}${numbers[9]}${numbers[10]}`);
+  return primeiros;
 }
 
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'lucas'));
+function joinNumbers(numbers) {
+  return organization(numbers).join('');
+}
+
+function tamanhoArray(numbers) {
+  if (numbers.length < 11 || numbers.length > 11) {
+    return 'Array com tamanho incorreto.';
+  }
+}
+
+function generatePhoneNumber(numbers) {
+  if (tamanhoArray(numbers)) {
+    return tamanhoArray(numbers)
+  }
+  if (verificaNumeros(numbers)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  return joinNumbers(numbers);
+}
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2]))
