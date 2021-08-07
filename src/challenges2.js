dab38f18a7112d76f2d7d8d4aaa7144cd4b1c325
Marina Marques <marina.marqueso@outlook.com>
2021-08-06 21:52:36 -0300

    Adiciona desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -11,12 +11,41 @@ function techList(arrayTech, name) {
   }
   return techObject;
 }
-  console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
+
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+function generatePhoneNumber(arrayPhone) {
+  let phoneNumber = "";
+
+  if (arrayPhone.length != 11) {
+    return "Array com tamanho incorreto."
+  } 
+  for (let i = 0; i < arrayPhone.length; i++) {
+    let repeticoes = [];
+    if (arrayPhone[i] < 0 || arrayPhone[i] > 9) {
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+    for (let j = 0; j < arrayPhone.length; j++){
+      if (arrayPhone[i] === arrayPhone[j]){
+        repeticoes.push(arrayPhone[j]);
+      }
+    }
+    
+    if (repeticoes.length >= 3) {
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+
+    if (i === 0) {
+      phoneNumber = phoneNumber + "(";
+    } else if (i===2) {
+      phoneNumber = phoneNumber + ") ";
+    } else if (i===7) {
+      phoneNumber = phoneNumber + "-";
+    }
+    phoneNumber = phoneNumber + arrayPhone[i];
+    
+  } return phoneNumber;
+} console.log(generatePhoneNumber([0, -2, 3, 8, 4, 2, 7, 8, 9, 9, 4]));
 
 // Desafio 12
 function triangleCheck() {
