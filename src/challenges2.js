35a855f298a7f26f464d150df83b018005ae32ec
claudiocassimiro <days_surfing@live.com>
2021-08-07 11:35:22 -0300

    Desafio 11 concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -21,8 +21,48 @@ function techList(array, name) {
 };
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let numbers = array;
+  let countRepeatedNumber = 0;
+  let countNumber = 0;
+  let removeComma = '';
+  let toString = '';
+  let ddd = '';
+  let part1 = '';
+  let part2 = '';
+  let stringFormated = '';
+
+  if (numbers.length !== 11) {
+    return ('Array com tamanho incorreto.');
+  }
+
+  for (let index in numbers){
+    let verificaNum = numbers[index];
+    for (let index2 in numbers) {
+      if (verificaNum === numbers[index2]) {
+        countNumber += 1;
+      }
+    }
+    if (countNumber > countRepeatedNumber) {
+      countRepeatedNumber = countNumber;
+    }
+    countNumber = 0;
+    
+    if (numbers[index] < 0 || numbers[index] > 9 || countRepeatedNumber > 2) {
+      return ("não é possível gerar um número de telefone com esses valores");
+    }
+  }
+
+  removeComma = numbers.join();
+  toString = removeComma.replace(/\,/g, '');
+
+  ddd = toString.slice(0, 2);
+  part1 = toString.slice(2, 7);
+  part2 = toString.slice(7, 11);
+
+  stringFormated = `(${ddd}) ${part1}-${part2}`;
+
+   return stringFormated;
 }
 
 // Desafio 12
