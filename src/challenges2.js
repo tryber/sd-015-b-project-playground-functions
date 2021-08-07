739bd350c3894193213db5bd0cbd94c407c04f06
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-08-07 16:32:32 -0300

    Cumprido o requisito 10, com ajudas.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,25 @@
 // Desafio 10
-function techList() {
+function makeObjectTechList (techList, name) {
+  techList.sort();
+  let createdObject = [];
+
+  for (let index = 0; index < techList.length; index += 1) {
+    console.log(techList[index]);
+    let createObject = {tech: techList[index], name: name};
+    createdObject.push(createObject);
+  }
+  return createdObject;
+  /**
+   * Dica para criar objeto dinâmicamente dada pelo usuário hugomg, em:
+   * https://pt.stackoverflow.com/questions/64116/%C3%89-poss%C3%ADvel-criar-um-objeto-dinamicamente-em-js-sem-utilizar-eval
+   * Também tive ajuda dos colegas de formação conforme conversa que se iniciou em:
+   * https://trybecourse.slack.com/archives/C027T2VU8U8/p1628354184324500
+   */
+}
+
+function techList(receivedList, receivedName) {
   // seu código aqui
-  
+  return (receivedList.length > 0) ? makeObjectTechList(receivedList, receivedName) : 'Vazio!';
 }
 
 // Desafio 11
