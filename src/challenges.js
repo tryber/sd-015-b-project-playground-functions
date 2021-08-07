266b60c62c56e7221bd626a18f4a7653c1f70122
Robert Martins <robert.rm131@gmail.com>
2021-08-07 17:04:02 -0300

    retirar desafio 6 para refatorar, ajustar alguns lints

--- src/challenges.js
+++ src/challenges.js
@@ -8,84 +8,53 @@ function compareTrue(a, b) {
 
 // Desafio 2
 function calcArea(base, height) {
-  return (base* height)/2;
+  return (base * height) / 2;
 }
 
 // Desafio 3
 function splitSentence(s) {
-  return s.split(" ");
+  return s.split(' ');
 }
 
 // Desafio 4
 function concatName(nome) {
   let tamanho = nome.length;
-  return `${nome[tamanho-1]}, ${nome[0]}`;
+  return `${nome[tamanho - 1]}, ${nome[0]}`;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  return ((wins * 3)+(ties * 1));
+  return ((wins * 3) + (ties * 1));
 }
 
 // Desafio 6
 function highestCount(v) {
-  let maior = 0;
-  let cont = 0;
-  let menorQueZero = false;
-  for (let i of v) {
-    if(i < 0) {
-      menorQueZero = true;
-      if (maior === 0) {
-        maior = i*-1;
-      } else {
-        if (i*-1 < maior) {
-          maior = i*-1;
-        } 
-      }
-    } else {
-        if(i > maior) {
-          maior = i;
-        }
-      }
-  }
-  for (let j of v) {
-    console.log(maior, j*-1)
-    if (menorQueZero === true) {
-      if (j*-1 === maior) {
-        cont += 1;
-      }
-    } else {
-    if (j === maior) {
-        cont += 1;
-      }
-    }
-  }
-  return cont;  
+  //refatorar...
 }
- 
+
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  if( Math.abs(Math.abs(mouse) - Math.abs(cat1)) < Math.abs(Math.abs(mouse) - Math.abs(cat2)) ) {
-    return "cat1"
+  if ( Math.abs(Math.abs(mouse) - Math.abs(cat1)) < Math.abs(Math.abs(mouse) - Math.abs(cat2))){
+    return 'cat1';
   }
-  if (Math.abs(mouse - cat1) === Math.abs(mouse - cat2)) {
-    return "os gatos trombam e o rato foge"
+  if (Math.abs(mouse - cat1) === Math.abs(mouse - cat2)){
+    return 'os gatos trombam e o rato foge';
   }
-  return "cat2";
+  return 'cat2';
 }
 
 // Desafio 8
 function fizzBuzz(numeros) {
   let ar = [];
-  for (let i of numeros) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      ar.push("fizzBuzz");
-    } else if (i % 3 === 0) {
-      ar.push("fizz");
-    } else if (i % 5 === 0) {
-      ar.push("buzz");
+  for (let i of numeros){
+    if (i % 3 === 0 && i % 5 === 0){
+      ar.push('fizzBuzz');
+    } else if (i % 3 === 0){
+      ar.push('fizz');
+    } else if (i % 5 === 0){
+      ar.push('buzz');
     } else {
-      ar.push("bug!");
+      ar.push('bug!');
     }
   }
   return ar;
@@ -93,37 +62,36 @@ function fizzBuzz(numeros) {
 
 // Desafio 9
 function encodeMudaBusca(indice) {
-  let ar = "aeiou";
+  let ar = 'aeiou';
   return ar[indice];
 }
 
 function encode(s) {
   let cont = 1;
   let str = s;
-  for(let i=0; i < 6; i++ ) {
+  for (let i = 0; i < 6; i += 1 ){
     let letra = encodeMudaBusca(i);
-    for (let k = 0; k < s.length; k++) {
-      if(letra === s[k]) {
+    for (let k = 0; k < s.length; k += 1){
+      if (letra === s[k]){
         str = str.replace(letra, cont);
       }
     }
-    cont  ++;
+    cont += 1;
   }
   return str;
 }
 
 function decodeMudaBusca(index) {
-  let ar = "12345";
+  let ar = '12345';
   return ar[index];
 }
 
 function decode(s) {
   let str = s;
-  teste = s;
-  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i += 1){
     let numero = decodeMudaBusca(i);
-    for (let k=0; k < s.length; k++) {
-      if(numero === s[k]) {
+    for (let k = 0; k < s.length; k += 1){
+      if (numero === s[k]){
         str = str.replace(numero, encodeMudaBusca(i));
       }
     }
--- src/challenges2.js
+++ src/challenges2.js
@@ -2,9 +2,9 @@
 function techList(lista, name) {
   let tecnologias = lista.sort();
   let novoObjeto = [];
-  if(lista.length) {
-    for (let i = 0; i < lista.length; i++) {
-      novoObjeto.push({tech: tecnologias[i], name: name});
+  if (lista.length) {
+    for (let i = 0; i < lista.length; i += 1) {
+      novoObjeto.push({ tech: tecnologias[i], name: name });
     }
     return novoObjeto;
   }
@@ -14,17 +14,17 @@ function techList(lista, name) {
 // Desafio 11
 function verOcorrencias(n) {
   let cont = 0;
-  for (let i = 0; i < n.length; i++) {
-      cont = n.split(n[i]).length - 1;
-      if(cont >= 3) {
-        return 2;
-      }
+  for (let i = 0; i < n.length; i += 1) {
+    cont = n.split(n[i]).length - 1;
+    if (cont >= 3) {
+      return 2;
+    }
   }
   return 3;
 }
 
 function verTamanhos(n) {
-  if(n.length === 11) {
+  if (n.length === 11) {
     return 3;
   }
   return 0; 
@@ -32,7 +32,7 @@ function verTamanhos(n) {
 
 function verMaiorMenor(n) {
   let resultado = 0;
-  if((Math.max(...n) > 9) || (Math.min(...n) < 0)) {
+  if ((Math.max(...n) > 9) || (Math.min(...n) < 0)) {
     resultado = 1;
   } else {
     resultado = 3;
@@ -41,15 +41,15 @@ function verMaiorMenor(n) {
 }
 function generatePhoneNumber(numeros) {
   let resultado = [];
-  let resultadoFinal = "";
+  let resultadoFinal = '';
   resultado.push(verTamanhos(numeros));
   resultado.push(verMaiorMenor(numeros));
-  let listaArrumada = numeros.join("");
+  let listaArrumada = numeros.join('');
   resultado.push(verOcorrencias(listaArrumada));
-  let resultadosPossiveis = ["Array com tamanho incorreto.", "não é possível gerar um número de telefone com esses valores", "não é possível gerar um número de telefone com esses valores"];
+  let resultadosPossiveis = ['Array com tamanho incorreto.' , 'não é possível gerar um número de telefone com esses valores' , 'não é possível gerar um número de telefone com esses valores'];
   resultadoFinal = `(${listaArrumada.slice(0,2)}) ${listaArrumada.slice(2,7)}-${listaArrumada.slice(7)}`;
   for (let i = 0; i < resultado.length; i += 1) {
-    if(resultado[i] === i) {
+    if (resultado[i] === i) {
       return resultadosPossiveis[i];
     }
   }
@@ -57,12 +57,11 @@ function generatePhoneNumber(numeros) {
 }
 
 // Desafio 12
-// a < b+c && a> Math.abs(b-c)
-function medidas (x,y,z) {
-  let a = ((x < y+z) && (x > Math.abs(y-z)));
-  let b = ((y < x+z) && (y > Math.abs(x-z)));;
-  let c = ((z < y+x) && (z > Math.abs(y-x)));;
-  return (a===b===c);
+function medidas(x, y, z) {
+  let a = ((x < y + z) && (x > Math.abs(y - z)));
+  let b = ((y < x + z) && (y > Math.abs(x - z)));
+  let c = ((z < y + x) && (z > Math.abs(y - x)));
+  return (a === b === c);
 }
 function triangleCheck(lineA, lineB, lineC) {
   return medidas(lineA, lineB, lineC);
@@ -71,13 +70,13 @@ function triangleCheck(lineA, lineB, lineC) {
 // Desafio 13
 function hydrate(s) {
    let r = 0;
-   let resultado = "";
-   for (let i = 1; i <= 9; i++) {
+   let resultado = '';
+   for (let i = 1; i <= 9; i += 1) {
      r += (s.split(i).length-1)*i;
    }
    resultado = `${r} copos de água`;
-   if(r === 1) {
-     resultado = "1 copo de água";
+   if (r === 1) {
+     resultado = '1 copo de água';
    }
    return resultado;
 }
