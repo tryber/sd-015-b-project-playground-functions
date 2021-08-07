390de279920e4eaaf390c31e68d6282b11036db4
Yamamoto Andre <andre.yamamoto07@gmail.com>
2021-08-06 22:08:30 -0300

    modificação challenges2: adicionado exercicios 11,12,13

--- src/challenges2.js
+++ src/challenges2.js
@@ -7,28 +7,60 @@ function techList(arrTechList, name) {
   arrTechList.sort()
 
   for (let cont = 0; cont < arrTechList.length; cont += 1) {
-    
+
     lista.push({
       tech: arrTechList[cont],
-      name:name,
+      name: name,
     })
   }
-return lista
+  return lista
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrGePhoNum) {
+  if (arrGePhoNum.length != 11) return "Array com tamanho incorreto."
+  for (let number of arrGePhoNum) {
+    if (number < 0 || number > 9) return "não é possível gerar um número de telefone com esses valores"
+
+    let guarana = 0
+
+    for (let dollynho of arrGePhoNum) {
+      if (dollynho === number) {
+        guarana += 1
+      }
+      if (guarana === 3) {
+        return "não é possível gerar um número de telefone com esses valores"
+      }
+    }
+  }
+  return `(${arrGePhoNum.slice(0, 2).join("")}) ${arrGePhoNum.slice(2, 7).join("")}-${arrGePhoNum.slice(7).join("")}`
+
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA < lineB + lineC && lineA > Math.abs(lineB - lineC) 
+  || lineC < lineB + lineA && lineC > Math.abs(lineB - lineA) 
+  || lineB < lineC + lineA && lineB > Math.abs(lineC - lineA)) {
+    return true
+  }else{
+    return false
+  }
+
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(strDollynho) {
+  let regex = /\d+/g
+  let savRegex = strDollynho.match(regex)
+  let soma = 0
+  for(let dolly of savRegex){
+    soma+= Number(dolly)
+    
+  }
+  
+  return soma>1? `${soma} copos de água`: `${soma} copo de água`
+
 }
 
 module.exports = {
