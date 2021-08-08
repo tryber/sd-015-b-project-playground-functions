55a75916cf18f991b6a04bb05d0bc9d437eaa256
Walace <walacewab@gmail.com>
2021-08-07 22:41:47 -0300

    Desafio 11 generatePhoneNumber

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,8 +14,51 @@ function techList(range, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber(numbers) {
+function generatePhoneNumber(telefone) {
   // seu código aqui
+  
+  if (telefone.length != 11){
+    return "Array com tamanho incorreto.";
+  }
+
+  for (let numero of telefone){
+    if (numero < 0 || numero > 9){
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+  }
+
+  for (let numero of telefone){
+    let contagem = 0;
+    for (let numeroVerificado of telefone){
+      if (numeroVerificado === numero){
+        contagem ++;
+      }
+      if (contagem >= 3){
+        return "não é possível gerar um número de telefone com esses valores";
+      }
+  }   
+}
+  // // Esse trecho do codigo foi reproduzido a partir da Fernanda Michetti.
+  // Segue o link: https://github.com/tryber/sd-015-b-project-playground-functions/tree/fernanda-michetti-playground-functions
+  
+  let numero =
+    "(" +
+    telefone[0] +
+    telefone[1] +
+    ")" +
+    " " +
+    telefone[2] +
+    telefone[3] +
+    telefone[4] +
+    telefone[5] +
+    telefone[6] +
+    "-" +
+    telefone[7] +
+    telefone[8] +
+    telefone[9] +
+    telefone[10] ;
+    
+  return numero;
 }
 
 // Desafio 12
