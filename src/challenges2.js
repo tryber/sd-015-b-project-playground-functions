06f216b36bb162297f901de75d0fe78f713b8dd9
iuryroque <iury.roque@hotmail.com>
2021-08-08 12:44:08 -0300

    Desafio 13 hydrate - concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -62,7 +62,16 @@ function triangleCheck(lineA,lineB,lineC) {
 
 // Desafio 13
 function hydrate(frase) {
-
+  let vetorNumeros = frase.match(/\d+/g);
+  let soma = 0;
+  for (let index = 0; index < vetorNumeros.length; index++) {
+    soma += parseInt(vetorNumeros [index]);
+  }
+  if (soma <= 1) {
+    return `${soma} copo de água`
+  }else{
+    return `${soma} copos de água`
+  }
 }
 
 module.exports = {
