931cfdec453b03b7766782627f616bd9f028d65b
Flávia Labanca <flavialabanca@gmail.com>
2021-08-07 22:26:21 -0300

    requisito 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -2,33 +2,33 @@
 function techList(techs, name) {
   let techList = [];
   techs.sort();
-  for (let index = 0; index < techs.length; index += 1){
+  for (let index = 0; index < techs.length; index += 1) {
     let objeto = {
       tech: techs[index],
-      name: name
+      name,
     };
     techList.push(objeto);
   }
-  if (techs.length <= 1){
+  if (techs.length <= 1) {
     return 'Vazio!';
   }
-return techList;
+  return techList;
 }
 
 // valida tamanho
-function tamanhoInvalido(phoneNumber){
+function tamanhoInvalido(phoneNumber) {
   let validacao = true;
-  if (phoneNumber.length !== 11){
+  if (phoneNumber.length !== 11) {
     validacao = false;
   }
   return validacao;
 }
 
 // valida < 0 ; > 9
-function validaNumeros(phoneNumber){
+function validaNumeros(phoneNumber) {
   let validacao = true;
-  for (let index = 0; index < phoneNumber.length; index++){
-    if ((phoneNumber[index] < 0) || (phoneNumber[index] > 9)){
+  for (let index = 0; index < phoneNumber.length; index += 1) {
+    if ((phoneNumber[index] < 0) || (phoneNumber[index] > 9)) {
       validacao = false;
     }
   }
@@ -36,20 +36,20 @@ function validaNumeros(phoneNumber){
 }
 
 // valida repeticao de 3 seguidos
-function validaRepeticao(phoneNumber){
+function validaRepeticao(phoneNumber) {
   let validacao = true;
   let guardaNumero = 0;
   let contador = 0;
 
-  for (let indexUm = 0; indexUm < phoneNumber.length; indexUm += 1){
+  for (let indexUm = 0; indexUm < phoneNumber.length; indexUm += 1) {
     guardaNumero = phoneNumber[indexUm];
     contador = 0;
-    for (let indexDois = 0; indexDois < phoneNumber.length; indexDois += 1){
-      if (guardaNumero === phoneNumber[indexDois]){
-      contador += 1;
+    for (let indexDois = 0; indexDois < phoneNumber.length; indexDois += 1) {
+      if (guardaNumero === phoneNumber[indexDois]) {
+        contador += 1;
       }
-      if (contador > 2){
-      validacao = false;
+      if (contador > 2) {
+        validacao = false;
       }
     }
   }
@@ -57,24 +57,24 @@ function validaRepeticao(phoneNumber){
 }
 
 // monta o numero
-function montaNumero(phoneNumber){
+function montaNumero(phoneNumber) {
   let numero = [];
   let numeroFinal = '';
   numero.push('(');
   for (let index = 0; index < phoneNumber.length; index += 1) {
     if (index === 1) {
-      numero.push(phoneNumber[index] + ') ');
-    } else if (index === 6) { 
-      numero.push(phoneNumber[index] + '-');
+      numero.push(`${phoneNumber[index]}) `);
+    } else if (index === 6) {
+      numero.push(`${phoneNumber[index]}-`);
     } else {
-      numero.push(phoneNumber[index])
+      numero.push(phoneNumber[index]);
     }
   }
-numeroFinal = numero.join('');
-return numeroFinal;
-}  
+  numeroFinal = numero.join('');
+  return numeroFinal;
+}
 
-//Desafio 11
+// Desafio 11
 function generatePhoneNumber(phoneNumber) {
   if (!tamanhoInvalido(phoneNumber)) {
     return 'Array com tamanho incorreto.';
@@ -88,98 +88,75 @@ function generatePhoneNumber(phoneNumber) {
   return montaNumero(phoneNumber);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
 // valida lado A
-function validaLadoA(lineA, lineB, lineC){
+function validaLadoA(lineA, lineB, lineC) {
   let validacao = false;
-  if (lineA < (lineB + lineC)){
-    if (lineA > Math.abs(lineB - lineC)){
+  if (lineA < (lineB + lineC)) {
+    if (lineA > Math.abs(lineB - lineC)) {
       validacao = true;
     }
   }
   return validacao;
 }
-//console.log(validaLadoA(10, 14, 8)) // retorno: true
 
-//valida lado B
-function validaLadoB(lineA, lineB, lineC){
+// valida lado B
+function validaLadoB(lineA, lineB, lineC) {
   let validacao = false;
-  if (lineB < (lineC + lineA)){
-    if (lineB > Math.abs(lineC - lineA)){
+  if (lineB < (lineC + lineA)) {
+    if (lineB > Math.abs(lineC - lineA)) {
       validacao = true;
     }
   }
   return validacao;
 }
-//console.log(validaLadoA(10, 14, 8)) // retorno: true
 
-//valida lado C
-function validaLadoC(lineA, lineB, lineC){
+// valida lado C
+function validaLadoC(lineA, lineB, lineC) {
   let validacao = false;
-  if (lineC < (lineA + lineB)){
-    if (lineC > Math.abs(lineA - lineB)){
+  if (lineC < (lineA + lineB)) {
+    if (lineC > Math.abs(lineA - lineB)) {
       validacao = true;
     }
   }
   return validacao;
 }
-//console.log(validaLadoA(10, 14, 8)) // retorno: true
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
-  if (!validaLadoA(lineA, lineB, lineC)){
+  if (!validaLadoA(lineA, lineB, lineC)) {
     return false;
   }
-  if (!validaLadoB(lineA, lineB, lineC)){
+  if (!validaLadoB(lineA, lineB, lineC)) {
     return false;
   }
-  if (!validaLadoC(lineA, lineB, lineC)){
+  if (!validaLadoC(lineA, lineB, lineC)) {
     return false;
   }
   return true;
 }
 
-console.log(triangleCheck(10, 14, 8)) // retorno: true
-
-
-
-// recebe: três linhas lineA, lineB, lineC
-// retorna: boleano; pode (ou nao) criar um triângulo.
-
-// regra: a medida de qualqer um dos lados seja MENOR que a soma dos outros dois 
-//        e maior que o valor absoluto da diferenca entre eles
-// regra: (lineA < lineB + lineC) && (lineA > (lineB - lineC)
-// funcao Maths.abs pra descobrir valor absoluto
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(comanda) {
+  let comandaSaida ='';
+  for (let index = 0; index < comanda.length; index += 1) {
+    comandaSaida = comanda.replace(/\D/g, '');
+  }
+
+  let totalAgua = 0;
+
+  for (let index = 0; index < comandaSaida.length; index += 1) {
+    totalAgua += parseInt(comandaSaida[index])
+  }
+  
+  if (totalAgua === 1){
+    return '1 copo de água'
+  } else {
+    return totalAgua + ' copos de água'
+  }
+
 }
+
+console.log(hydrate('1 vinho'))
 
 module.exports = {
   generatePhoneNumber,
