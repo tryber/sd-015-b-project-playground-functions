412eb860ec41f109392335569b9e4c9e64eb4c41
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-08-07 21:47:29 -0300

    fix: Tirando o log do 11.

--- src/challenges2.js
+++ src/challenges2.js
@@ -69,7 +69,7 @@ function generatePhoneNumber(phone) {
 
   return numero;
 }
-generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]);
+
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
