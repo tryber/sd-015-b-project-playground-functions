04f414feb540488ebb45b004c7259d8ba2f06cfb
NicolasDimitri <nicolasdodimitri@gmail.com>
2021-08-08 12:52:31 -0300

    desafio 9 completo

--- src/challenges.js
+++ src/challenges.js
@@ -71,19 +71,66 @@ function catAndMouse(mouse, cat1, cat2) {
   }
   return resultado
 }
-catAndMouse(10,16,22)
 
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(numeros) {
   // seu código aqui
+  let resultado = [];
+  for (let index = 0; index < numeros.length; index += 1) {
+    if (numeros[index] % 3 === 0 && numeros[index] % 5 === 0) {
+      resultado.push("fizzBuzz")
+    } else if (numeros[index] % 3 === 0) {
+      resultado.push("fizz")
+    } else if (numeros[index] % 5 === 0) {
+      resultado.push("buzz") 
+    } else {
+      resultado.push("bug!")
+    }
+  }
+  return resultado
 }
 
 // Desafio 9
-function encode() {
+function encode(frase) {
   // seu código aqui
+  let fraseCodificada = "";
+  for (let index = 0; index < frase.length; index += 1) {
+    if (frase[index] === "a") {
+      fraseCodificada += "1"
+    } else if (frase[index] === "e") {
+      fraseCodificada += "2"
+    } else if (frase[index] === "i") {
+      fraseCodificada += "3"
+    } else if (frase[index] === "o") {
+      fraseCodificada += "4"
+    } else if (frase[index] === "u") {
+      fraseCodificada += "5"
+    } else { 
+      fraseCodificada += (frase[index])
+    }
+  }
+  return fraseCodificada;
 }
-function decode() {
+
+function decode(frase) {
   // seu código aqui
+  let fraseDecodificada = "";
+  for (let index = 0; index < frase.length; index += 1) {
+    if (frase[index] === "1") {
+      fraseDecodificada += "a"
+    } else if (frase[index] === "2") {
+      fraseDecodificada += "e"
+    } else if (frase[index] === "3") {
+      fraseDecodificada += "i"
+    } else if (frase[index] === "4") {
+      fraseDecodificada += "o"
+    } else if (frase[index] === "5") {
+      fraseDecodificada += "u"
+    } else { 
+      fraseDecodificada += (frase[index])
+  }
+} 
+return fraseDecodificada;
 }
 
 module.exports = {
