fef3baf18373de583c1b98466779d3aaf9c3e8c4
Richard Leal <rwsleal@gmail.com>
2021-08-08 16:57:16 -0300

    Requisitos 10 e 11 finalizados

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,56 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techs, name) {
+  if (techs.length === 0) {
+    return "Vazio!"
+  } else {
+    techs.sort()
+  }
+  let techObjects = [];
+  for (index = 0; index < techs.length; index += 1) {
+    techObjects.push({
+      tech: techs[index],
+      name: name,
+    });
+  }return techObjects;
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(phoneNumber) {
+  function countRepeat(phoneNumber){
+    for (index = 0; index < phoneNumber.length; index += 1) {
+      let count = 0;
+      for (i = index; i < phoneNumber.length; i += 1) {
+        if (phoneNumber[index] === phoneNumber[i]) {
+          count += 1;
+          if (count >= 3) {
+            return false;
+          }
+        }
+      }
+    }
+  }
+  let correctNumber = "(";
+  if (phoneNumber.length !== 11) {
+    return "Array com tamanho incorreto.";
+  } else if (countRepeat(phoneNumber) === false) {
+      return "não é possível gerar um número de telefone com esses valores";
+  } else {
+    for (index = 0; index < phoneNumber.length; index += 1) {
+      if (phoneNumber[index] > 9 || phoneNumber[index] < 0) {
+        return "não é possível gerar um número de telefone com esses valores";
+      } else {
+        if ([index] <= 1) {
+          correctNumber += String(phoneNumber[index]);
+        }else if ([index] == 2) {
+          correctNumber = correctNumber + ") " + String(phoneNumber[index]);
+        }else if ([index] == 7) {
+          correctNumber = correctNumber + "-" + String(phoneNumber[index]);
+        }else if (3 <= [index] <= 6 || [index] >= 8) {
+          correctNumber += String(phoneNumber[index]);
+        }
+      }
+    }
+  } return correctNumber;
 }
 
 // Desafio 12
