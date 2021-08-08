c545b902aea3bbad747a83dcdd1432562e11e032
iuryroque <iury.roque@hotmail.com>
2021-08-08 12:42:12 -0300

    Desafio 11 generatePhoneNumber - concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,22 +1,79 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+
+function techList(telefone) {
+  
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+
+function generatePhoneNumber(telefone) {
+  
+  if (telefone.length !== 11){
+    return "Array com tamanho incorreto."
+  }
+
+  for (let index = 0; index < telefone.length; index++) {
+    if (telefone[index] < 0 || telefone[index] > 9){
+      return "não é possível gerar um número de telefone com esses valores";
+    } 
+  }
+  let comparador = "";
+  let soma = 0;
+  for (let index = 0; index < telefone.length; index++) {
+    comparador = telefone[index];
+    soma = 0;
+    for (let secondIndex = 0; secondIndex < telefone.length; secondIndex++) {
+      if (comparador === telefone[secondIndex]) {
+        soma += 1;
+        if (soma >= 3) {
+          return "não é possível gerar um número de telefone com esses valores"
+        }
+      }
+    }   
+  }
+  
+  let ddd = "";
+  for (let index = 0; index < 2; index++) {
+    ddd += telefone[index]; 
+  }
+  let prefixo = "";
+  for (let index = 2; index < 7; index++) {
+    prefixo += telefone[index];
+  }
+  let numero = "";
+  for (let index = 7; index < 11; index++) {
+    numero += telefone[index];
+  }
+  
+  return `(${ddd}) ${prefixo}-${numero}`;
+  
+  //  return `(${telefone[0]}${telefone[1]}) ${telefone[2]}${telefone[3]}${telefone[4]}${telefone[5]}${telefone[6]}-${telefone[7]}${telefone[8]}${telefone[9]}${telefone[10]}`
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA,lineB,lineC) {
+  if (lineA + lineB > lineC && lineB + lineC > lineA && lineC + lineA > lineB){
+    return true;
+  }else{
+    return false;
+  }
+
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(frase) {
+  let vetorNumeros = frase.match(/\d+/g);
+  let soma = 0;
+  for (let index = 0; index < vetorNumeros.length; index++) {
+    soma += parseInt(vetorNumeros [index]);
+  }
+  if (soma <= 1) {
+    return `${soma} copo de água`
+  }else{
+    return `${soma} copos de água`
+  }
 }
+
 
 module.exports = {
   generatePhoneNumber,
@@ -24,3 +81,4 @@ module.exports = {
   hydrate,
   triangleCheck,
 };
+
