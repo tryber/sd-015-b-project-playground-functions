4a1f21006628186eecb6622ac434b01ed17ee8d5
Jesse Benevides <jessebenevides@gmail.com>
2021-08-08 16:19:10 -0300

    Adiciona requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,8 +19,42 @@ function techList(techs, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+
+function generatePhoneNumber(numbers) {
+  if (!isLengthValid(numbers)) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  if (!isArrayValid(numbers)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+
+  let phoneNumber = '';
+
+  for (let i in numbers) {
+    switch (i) {
+      case '0':
+        phoneNumber += '(';
+        phoneNumber += numbers[i];
+        break;
+
+      case '1':
+        phoneNumber += numbers[i];
+        phoneNumber += ') ';
+        break;
+
+      case '7':
+        phoneNumber += '-';
+        phoneNumber += numbers[i];
+        break;
+
+      default:
+        phoneNumber += numbers[i];
+        break;
+    }
+  }
+
+  return phoneNumber;
 }
 
 // Desafio 12
@@ -39,3 +73,51 @@ module.exports = {
   hydrate,
   triangleCheck,
 };
+
+//Verifica se o tamanho do array é válido
+//Retorna TRUE se o tamanho for igual a 11
+function isLengthValid(array) {
+  if (array.length != 11) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+//Verifica se os valores dentro do array são válidos
+//Retorna TRUE se os valores estiverem entre 0 e 9
+function isValueValid(numbers) {
+  for (let number of numbers) {
+    if (number < 0 || number > 9) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//Verifica se a sequencia de números do array é válida
+//Retorna TRUE se um número não se repetir 3x ou mais
+function isSequenceValid(numbers) {
+  for (let i in numbers) {
+    let cont = 0;
+    for (let j in numbers) {
+      if (numbers[i] === numbers[j]) {
+        cont += 1;
+      }
+    }
+    if (cont === 3) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+//Verifica se o array é válido
+//Retorna TRUE se o array passar nas verificaçõs de isSequenceValid e isValueValid
+function isArrayValid(array) {
+  let values = isValueValid(array);
+  let repetitions = isSequenceValid(array);
+
+  return values && repetitions;
+}
