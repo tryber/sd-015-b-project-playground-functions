474594c9221a239e873cd4b62cab9c751cf43d37
assisMeneghetti <assismfabri@hotmail.com>
2021-08-08 11:40:33 -0300

    adiciona desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,20 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, name) {
+  let sortTechnologies = tech.sort();
+  let result = [];
+  if (tech.length === 0) {
+    return 'Vazio!';
+  }
+
+  for (let index = 0; index < sortTechnologies.length; index += 1) {
+    let technologiesByName = {
+      tech: sortTechnologies[index],
+      name: name
+    }
+    result.push(technologiesByName);
+    // return result;
+  }
+  return result;
 }
 
 // Desafio 11
