13c7905ec2c2e3c6719aa54724956682c4a02e00
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2021-08-08 21:28:30 -0300

    adiciona desafio 13..;...

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,9 +1,9 @@
 // Desafio 10
-function techList (techList, name) {
+function techList (techList, name){
 techList.sort()
   let technologies = []
 var technologiesAndName = technologies.sort()
-if (techList.length === 0) {
+if (techList.length === 0){
   return 'Vazio!'
 } else {
 for(let i = 0; i < techList.length; i += 1){
@@ -37,16 +37,17 @@ for(let i of input){
 }
 // console.log(generatePhoneNumber([1, 1, 3 ,3 ,3, 3 ,4 ,4 ,5,6,7]))
   // Desafio 12
-function triangleCheck (lineA, lineB,lineC) {
+function triangleCheck (lineA, lineB,lineC){
   if(lineA + lineB > lineC && lineC > Math.abs(lineA - lineB) || lineC + lineB > lineA && lineA > Math.abs(lineC - lineB) || lineA + lineC > lineB && lineB > Math.abs(lineA - lineC)){
     return true;
   } else {
     return false;
   }
 }
+console.log(triangleCheck(123, 12, 11))
 
 // Desafio 13
-function hydrate (input) {
+function hydrate (input){
 const rawArray = input.match(/\d+/g);
 var numArray = [];
 var waterCup = 0;
@@ -55,11 +56,9 @@ for (let i = 0; i < rawArray.length; i += 1) {
   waterCup = waterCup + numArray[i];
 } 
 if (waterCup === 1){
-  return waterCup + ' copo de água'
+  return waterCup + ' copo de água';
 } else { return waterCup + ' copos de água' }
 }
-
-console.log(hydrate('1 coisa'))
 
 
 module.exports = {
