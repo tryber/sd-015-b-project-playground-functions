61c0e86ed1752991504df9db21ffe29825ebe823
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2021-08-09 11:19:39 -0300

    exercicio 11 iniciado

--- src/challenges2.js
+++ src/challenges2.js
@@ -9,7 +9,7 @@ function techList(tech, name) {
   for (let index = 0; index < techSort.length; index += 1) {
     let objList = {
       tech: techSort[index], 
-      name,
+      name: name
     };
     
     newArray.push(objList);
@@ -20,14 +20,44 @@ function techList(tech, name) {
 }
 //console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 
-
+//let numeberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(array) {
+  
+  if (array.length !== 11 ){
+    return "Array com tamanho incorreto."
+  }
+  
+  for (let index = 0; index < array.length; index +=1) {
+    if(array[index] < 0 || array[index] > 9 ) {
+    return 'não é possível gerar um número de telefone com esses valores' 
+    }
+  
+    let count = 0;
+  
+    for (let index2 = index ; index2 < array.length; index2 +=1) {
+      if (array[index] === array[index2]){
+        count = count +1;
+      }
+    }
+  
+    if (count === 3) {
+      return 'não é possível gerar um número de telefone com esses valores'
+    }
+  
+    //(12) 34567-8901
+  
+  }
+  
+let phone = `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}
+  ${numberArray[6]}-${numberArray[7]}${array[8]}${array[9]}${array[10]}`
+  return phone
   // seu código aqui
 }
+//console.log(generatePhoneNumber([1, 2, 3, 1, 5, 6, 7, 8, 9, 0, 9 ]))
 
 // Desafio 12
-function triangleCheck() {
+function triangleCheck() { 
   // seu código aqui
 }
 // Desafio 13
--- src/teste.js
+++ src/teste.js
@@ -1,16 +1,42 @@
-// Desafio 10
-function techList(tech, name) {
-  let newArray = [];
-  let techSort = tech.sort();
+// Desafio 11
+ let numeberArray = [1, 2, 3, 4, 1, 6, 7, 8, 9, 0, 1,1]; 
 
-  for (let index = 0; index < techSort.length; index += 1) {
-    let objList = {
-      tech: techSort[index], 
-      name,
-    };
-    
-    newArray.push(objList);
+function generatePhoneNumber(array) {
+console.log(array);
+
+if (array.length !== 11 ){
+  return "Array com tamanho incorreto."
 }
-  return newArray;
+
+for (let index = 0; index < array.length; index =+1) {
+  if(array[index] < 0 || array[index] > 9 ) {
+  return 'não é possível gerar um número de telefone com esses valores' 
+  }
+
+  let count = 1;
+
+  for (let index2 = index+1; index2 < array.length; index2 =+1) {
+    if (array[index] === array[index2]){
+      count = count +1;
+    }
+  }
+
+  if (count >= 3) {
+    return 'não é possível gerar um número de telefone com esses valores'
+  }
+
+  //(12) 34567-8901
+
+ return 'oi'  
+
 }
-  console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
+
+let phone = `(${numeberArray[0]}${numeberArray[1]}) ${numeberArray[2]}${numeberArray[3]}${numeberArray[4]}${numeberArray[5]}
+${numeberArray[6]}-${numeberArray[7]}${numeberArray[8]}${numeberArray[9]}${numeberArray[10]}`
+return phone;
+
+  // seu código aqui
+}
+
+
+console.log(generatePhoneNumber(numeberArray));
