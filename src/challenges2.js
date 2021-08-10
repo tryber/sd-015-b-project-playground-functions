ac871e4bb75ecebe22f2d62f2d48609a74cc8b52
LeonardoMonti <contatoleomonti@gmail.com>
2021-08-10 13:44:12 -0300

    Adicionando desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -35,7 +35,7 @@ function generatePhoneNumber(array) {
     }
   }
 
-  let resultNumber = '(' + array[0] + array[1] + ') ' + array[2] + array[3] + array[4] + array[5]  + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
+  let resultNumber = '(' + array[0] + array[1] + ') ' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
   return resultNumber;
 }
 
