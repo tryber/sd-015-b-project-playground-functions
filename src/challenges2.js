2e71448131ea98a601bed8908c9698a8547087a1
jpcp0614 <jpcp0614@gmail.com>
2021-08-10 11:42:16 -0300

    fix: function hydrate

--- src/challenges2.js
+++ src/challenges2.js
@@ -10,12 +10,22 @@ function generatePhoneNumber() {
 
 // Desafio 12
 function triangleCheck() {
-  // seu código aqui
+
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(phrase) {
+  let count = 0;
+  if (phrase.length > 0) {
+    let arrayNumbers = phrase.match(/\d+/g);
+    for (let i = 0; i < arrayNumbers.length; i += 1) {
+      count += parseInt(arrayNumbers[i], 10);
+    }
+  }
+  if (count === 1) {
+    return '1 copo de água';
+  }
+  return `${count} copos de água`;
 }
 
 module.exports = {
