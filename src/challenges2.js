7c35996f4cdbfa9f8ee8e9db5965fb1079ae9715
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-08-10 09:53:56 -0300

    Tentando desafio 11 novamente

--- src/challenges2.js
+++ src/challenges2.js
@@ -44,26 +44,26 @@ function techList(tech, nameParam) {
 function generatePhoneNumber(numbers) {
   for (let unityNumber of numbers) {
 
-      let min = 0
+      let min = 0;
 
       if (numbers.length !== 11) {
-          return "Array com tamanho incorreto."
+          return "Array com tamanho incorreto.";
       }
 
       if (unityNumber > 9 || unityNumber < 0) {
-          return "não é possível gerar um número de telefone com esses valores"
+          return "não é possível gerar um número de telefone com esses valores";
       }
 
       for (let compare of numbers) {
           if (compare === unityNumber) {
-              min += 1
+              min += 1;
           }
       }
       if (min >= 3) {
-          return "não é possível gerar um número de telefone com esses valores"
+          return "não é possível gerar um número de telefone com esses valores";
       }
   }
-      return "(" + numbers[0] + numbers[1] + ")" + numbers[2] + numbers[3] + numbers[4] + numbers[5] + "-" + numbers[6] + numbers[7] + numbers[8] + numbers[9]
+      return "(" + numbers[0] + numbers[1] + ")" + " " + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + "-" + numbers[7] + numbers[8] + numbers[9] + numbers[10];
 }
 
 // Desafio 12
