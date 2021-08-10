f9820a17475b032898811fb783cd0eb2d8473255
Kdulima <carlosedu151@gmail.com>
2021-08-10 13:13:51 -0300

    Correção de chamadas das funções

--- src/challenges.js
+++ src/challenges.js
@@ -8,17 +8,11 @@ compareTrue(true, true);
 function calcArea(base, height) {
   return base * height / 2;
 }
-calcArea(10, 50);
-calcArea(5, 2);
-calcArea(51, 1);
 
 // Desafio 3
 function splitSentence(string) {
   return string.split(' ');
 }
-splitSentence('go Trybe');
-splitSentence('vamo que vamo');
-splitSentence('foguete');
 
 // Desafio 4
 function concatName(name) {
@@ -26,17 +20,11 @@ function concatName(name) {
   let last = name.length - 1;
   return name [last] + ', ' + name[first];
 }
-concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']);
-concatName(['foguete', 'não', 'tem', 'ré']);
-concatName(['captain', 'my', 'captain']);
 
 // Desafio 5
 function footballPoints(wins, ties) {
   return (wins * 3) + (ties * 1);
 }
-footballPoints(14, 8);
-footballPoints(1, 2);
-footballPoints(0, 0);
 
 // Desafio 6
 function highestCount (array) {
@@ -54,9 +42,6 @@ function highestCount (array) {
   }
   return count;
 }
-highestCount([9, 1, 2, 3, 9, 5, 7]);
-highestCount([0, 4, 4, 4, 9, 2, 1]);
-highestCount([0, 0, 0]);
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
@@ -74,9 +59,6 @@ function catAndMouse(mouse, cat1, cat2) {
       return 'cat2';
   }
 }
-catAndMouse(0, 3, -2);
-catAndMouse(0, 6, -12);
-catAndMouse(0, 3, -3);
 
 // Desafio 8
 function fizzBuzz (arrayNumeros) {
@@ -99,9 +81,6 @@ function fizzBuzz (arrayNumeros) {
   }
  return string;
 }
-fizzBuzz([2, 15, 7, 9, 45]);
-fizzBuzz([7, 9]);
-fizzBuzz([9, 25]);
 
 // Desafio 9
 function encode (string) {
--- src/challenges2.js
+++ src/challenges2.js
@@ -17,8 +17,6 @@ function techList (tech, name) {
   return 'Vazio!';
 }
 
-techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas");
-
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
