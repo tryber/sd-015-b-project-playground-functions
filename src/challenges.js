ace3072963fa82639ffd8df6f85d33916fd0d36e
aklimak <alexandre.skk@gmail.com>
2021-08-10 00:50:28 -0300

    [Alexandre Klimak] Adiciona teste 9

--- src/challenges.js
+++ src/challenges.js
@@ -96,12 +96,24 @@ function fizzBuzz(initialArray) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(initialString) {
+let result = initialString.replace(/a/g, 1);
+    result = result.replace(/e/g, 2);
+    result = result.replace(/i/g, 3);
+    result = result.replace(/o/g, 4);
+    result = result.replace(/u/g, 5);
+return result;
 }
-function decode() {
-  // seu código aqui
+
+function decode(initialString) {
+let result = initialString.replace(/1/g, 'a');
+    result = result.replace(/2/g, 'e');
+    result = result.replace(/3/g, 'i');
+    result = result.replace(/4/g, 'o');
+    result = result.replace(/5/g, 'u');
+return result;
 }
+
 
 module.exports = {
   calcArea,
