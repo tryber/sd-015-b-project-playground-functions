5240777b246557660fbb051b919ab18da58ca096
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-08-10 13:54:31 -0300

    Tentando desafio 10 novamente (3)

--- src/challenges2.js
+++ src/challenges2.js
@@ -2,16 +2,21 @@
 function techList(tech, nameParam) {
   let techListing = []
 
-  for (let index = 0; index < tech.length; index += 1) {
-
+  if (tech.length > 1) {
+    for (let index = 0; index < tech.length; index += 1) {
       let addList = tech[index]
-      techListing.push({
-          tech: addList,
-          name: nameParam
-      })
-  }
 
-  return techListing;
+      techListing.push({
+        tech: addList,
+        name: nameParam
+      })
+    }
+    return techListing;
+  }
+  
+  else {
+    return "Vazio!"
+  }
 }
 
 // Desafio 11
