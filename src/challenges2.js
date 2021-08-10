22ee3496cad3c0a3de095e45f0d7fc71e6c494cd
Rafael <rafael.gomesbarretomoura@gmail.com>
2021-08-09 22:58:49 -0300

    adiciona function generatePhoneNumber

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,8 +16,28 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(array) {
   // seu código aqui
+  let format = "(xx) xxxxx-xxxx";
+  if (array.length != 11) {
+    return "Array com tamanho incorreto.";
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    let count = 0;
+    if (array[i] < 0 || array[i] > 9) {
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+    for (let i2 = 0; i2 < array.length; i2 += 1) {
+      if (array[i] === array[i2]) {
+        count += 1;
+      }
+      if (count > 2) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+    format = format.replace("x", array[i]);
+  }
+  return format;
 }
 
 // Desafio 12
