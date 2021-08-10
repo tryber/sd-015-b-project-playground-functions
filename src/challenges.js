24409d3058f86b2c3383fae63f8d4cefdf0aee1b
João Victor <joao.silveira@estudante.ufla.br>
2021-08-09 23:15:27 -0300

    desafio 5

--- src/challenges.js
+++ src/challenges.js
@@ -28,8 +28,9 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let sumPoints = (wins * 3) + ties;
+  return sumPoints; 
 }
 
 // Desafio 6
