ee9a178e3dd2627566a5c69ace00db98df470704
jonatan-teixeira <jonatan.teixeira@live.com>
2021-08-10 12:49:30 -0300

    Adicionando requisito 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,17 +1,19 @@
 // Desafio 10
-function techName(tech, name) {
+function techList() {
 
 }
-techName()
+techList()
 
 // Desafio 11
 function generatePhoneNumber(array) {
   if (array.length == 11) {
-      return '('+array[0]+array[1]+')'+array[2]+array[3]+array[4]+array[5]+array[6]+'-'+array[7]+array[8]+array[9]+array[10];
+      return '('+array[0]+array[1]+') '+array[2]+array[3]+array[4]+array[5]+array[6]+'-'+array[7]+array[8]+array[9]+array[10];
   } else if (array.length != 11){
       return 'Array com tamanho incorreto.'
-  } else if (array < 0 && array > 9) {
+  } else if (array[i] < 0) {
       return "não é possível gerar um número de telefone com esses valores"
+  } else if (array[i] > 9) {
+    return "não é possível gerar um número de telefone com esses valores"
   }
 }
 
