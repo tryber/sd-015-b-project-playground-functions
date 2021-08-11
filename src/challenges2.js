b19741d15cbbe9540689a751a663f5cbb2a09f8a
NicolasDimitri <nicolasdodimitri@gmail.com>
2021-08-11 11:07:33 -0300

    consertando erros

--- src/challenges2.js
+++ src/challenges2.js
@@ -42,7 +42,7 @@ function generatePhoneNumber(numero) {
   numero.splice(9, 0, "-");
   return numero.join("")
 }
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
+
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
