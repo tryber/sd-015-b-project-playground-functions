9cd639638fe9aa81aee871c2f908483567a3efb3
Davi Freitas <davivarejao@gmail.com>
2021-08-11 01:56:20 -0300

    Cria a funçao 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,14 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(a, b) {
+  if (a == true && b == true) {
+    return(true)
+  } else {
+    return(false)
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(altura, base) {
 }
 
 // Desafio 3
