6aef46ffd279a1319cce5b0393884b2b1a6d9395
NicolasDimitri <nicolasdodimitri@gmail.com>
2021-08-11 08:11:31 -0300

    desafio 10 concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,26 @@
 // Desafio 10
-function techList() {
+function techList(tech, name) {
   // seu código aqui
+  let obejtos = [];
+  
+  tech.sort();
+
+  for (index = 0; index < tech.length; index += 1) {
+    obejtos.push({
+      name: name,
+      tech: tech[index],
+    })
+  }
+  if (tech.length === 0) {
+    return "Vazio!"
+  }
+  return obejtos;
 }
 
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
+  
 }
 
 // Desafio 12
