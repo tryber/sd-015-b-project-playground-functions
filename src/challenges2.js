41a7d7772661422a951febce08e85275d318e856
NicolasDimitri <nicolasdodimitri@gmail.com>
2021-08-11 09:36:25 -0300

    Desafio 13 concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -42,8 +42,21 @@ function triangleCheck(lineA, lineB, lineC) {
   }
 
 // Desafio 13
-function hydrate() {
+function hydrate(bebidas) {
   // seu código aqui
+  let quantidadeDeBebidas = 0;
+  let umCopo = "1 copo de água"
+  let maisDeUmCopo = " copos de água"
+  for (index = 0; index < bebidas.length; index += 1) {
+    if (parseFloat(bebidas[index]) < 10 && parseFloat(bebidas[index]) >= 1) {
+      quantidadeDeBebidas += parseFloat(bebidas[index])
+    }
+  }
+  if (quantidadeDeBebidas === 1) {
+    return umCopo;
+  } else {
+    return quantidadeDeBebidas + maisDeUmCopo;
+  }
 }
 
 module.exports = {
