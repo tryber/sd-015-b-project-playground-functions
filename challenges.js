ba89f975a1a98b1a182fced897f2d91778c76b5a
Rafael de Alvarenga Reis <rafa_alvarenga@msn.com>
2021-08-24 10:32:12 -0300

    Atualizaçao do requisito 1

---
+++ challenges.js
@@ -0,0 +1,11 @@
+let trybeOne = true;
+let trybeTwo = true;
+let compareTrue;
+
+if (trybeOne === true && trybeTwo === true) {
+   compareTrue = true;
+} else {
+    compareTrue = false;
+}
+
+console.log(compareTrue);
